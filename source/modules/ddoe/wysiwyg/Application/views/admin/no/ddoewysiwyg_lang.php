d34b2330bdedfbe483b0659624d444abc1af2320
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 21:49:41 +0200

    New translations ddoewysiwyg_lang.php (Norwegian)

---
+++ source/modules/ddoe/wysiwyg/Application/views/admin/no/ddoewysiwyg_lang.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of OXID eSales WYSIWYG module.
+ *
+ * OXID eSales WYSIWYG module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales WYSIWYG module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales WYSIWYG module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2017
+ * @version   OXID eSales WYSIWYG
+ */
+
+$sLangName = 'English';
+
+$aLang = array(
+    // Charsets
+    'charset' => '',
+    // 'charset' => 'ISO-8859-15',
+
+    // Lang-Strings
+
+    // Base Module
+
+    'DD_CLOSE'          => '',
+    'DD_CANCEL'         => '',
+    'DD_APPLY'          => '',
+    'DD_OK'             => '',
+    'DD_CONFIRM_DIALOG' => '',
+    'DD_CONFIRM'        => '',
+
+    'DD_MEDIA_DIALOG'               => '',
+    'DD_MEDIA_LIST'                 => '',
+    'DD_MEDIA_UPLOAD'               => '',
+    'DD_MEDIA_REMOVE'               => '',
+    'DD_MEDIA_REMOVE_CONFIRM'       => '',
+    'DD_MEDIA_FALLBACK_INFO'        => '',
+    'DD_MEDIA_UPLOAD_INFO'          => '',
+    'DD_MEDIA_DRAG_INFO'            => '',
+    'DD_MEDIA_EMPTY_LIST'           => '',
+    'DD_MEDIA_FILETYPE_NOT_ALLOWED' => '',
+    'DD_MEDIA_FILES_FOUND'          => '',
+    'DD_MEDIA_MULTIPLE_INFO'        => '',
+
+    'DD_MEDIA_EXCEPTION_INVALID_FILEEXT'                   => '',
+
+);
