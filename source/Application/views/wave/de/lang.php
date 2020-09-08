be428f395631bcd6032d6d5c2fbe4840656444e0
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 22:20:33 +0200

    New translations lang.php (German)

--- source/Application/views/wave/de/lang.php
+++ source/Application/views/wave/de/lang.php
@@ -19,128 +19,128 @@
  * @copyright (C) OXID eSales AG 2003-2016
  */
 
-$sLangName = "English";
+$sLangName = "Deutsch";
 
 $aLang = array(
     'charset'                                               => 'UTF-8',
 
     // Global
-    'DD_SORT_DESC'                                          => '',
-    'DD_SORT_ASC'                                           => '',
-    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_SORT_DESC'                                          => 'absteigend',
+    'DD_SORT_ASC'                                           => 'aufsteigend',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Admin-Tool starten',
     'DD_DELETE'                                             => 'Löschen',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
-    'DD_FORM_VALIDATION_NUMBER'                             => '',
-    'DD_FORM_VALIDATION_INTEGER'                            => '',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_REQUIRED'                           => '',
-    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Bitte geben Sie eine gültige E-Mail-Adresse ein.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Die Passwörter stimmen nicht überein.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Bitte geben Sie eine Zahl ein.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Es sind keine Nachkommastellen erlaubt.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Bitte geben Sie eine positive Zahl ein.',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Bitte geben Sie eine negative Zahl ein.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Bitte Wert angeben.',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Bitte wählen Sie mindestens eine Option.',
 
     // Header
-    'SEARCH_TITLE'                                          => '',
-    'SEARCH_SUBMIT'                                         => 'Suche',
+    'SEARCH_TITLE'                                          => 'Suchbegriff eingeben...',
+    'SEARCH_SUBMIT'                                         => 'Suchen',
     'NAV_MORE'                                              => 'Mehr',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => '',
+    'FOOTER_NEWSLETTER_INFO'                                => 'Die neuesten Produkte und die besten Angebote per E-Mail, damit Ihr nichts mehr verpasst.',
 
     // Home page
-    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
-    'START_BARGAIN_HEADER'                                  => '',
-    'START_NEWEST_HEADER'                                   => '',
-    'START_TOP_PRODUCTS_HEADER'                             => '',
-    'START_BARGAIN_SUBHEADER'                               => '',
-    'START_NEWEST_SUBHEADER'                                => '',
-    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
+    'MANUFACTURERSLIDER_SUBHEAD'                            => 'Wir präsentieren Ihnen hier unsere sorgsam ausgewählten Marken, deren Produkte Sie in unserem Shop finden.',
+    'START_BARGAIN_HEADER'                                  => 'Angebote der Woche',
+    'START_NEWEST_HEADER'                                   => 'Frisch eingetroffen',
+    'START_TOP_PRODUCTS_HEADER'                             => 'Topseller',
+    'START_BARGAIN_SUBHEADER'                               => 'Sparen Sie bares Geld mit unseren aktuellen Schnäppchen!',
+    'START_NEWEST_SUBHEADER'                                => 'Frischer geht es nicht. Gerade noch in der Kiste und nun schon im Shop.',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Nur %s Produkte, dafür aber die besten, die wir Euch bieten können.',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => '',
-    'DD_CONTACT_FORM_HEADING'                               => 'Kontakt',
+    'DD_CONTACT_PAGE_HEADING'                               => 'Kontaktieren Sie uns!',
+    'DD_CONTACT_FORM_HEADING'                               => 'Kontaktformular',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => '',
+    'DD_LINKS_NO_ENTRIES'                                   => 'Es sind leider noch keine Links vorhanden.',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => '',
-    'DD_ERR_404_START_BUTTON'                               => '',
-    'DD_ERR_404_CONTACT_TEXT'                               => '',
-    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+    'DD_ERR_404_START_TEXT'                                 => 'Vielleicht finden Sie die von Ihnen gewünschten Informationen über unsere Startseite:',
+    'DD_ERR_404_START_BUTTON'                               => 'Startseite aufrufen',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Dürfen wir Ihnen direkt behilflich sein?<br>Gerne können Sie uns anrufen oder eine E-Mail schreiben:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'zur Kontaktseite',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'Durch Ihre Anmeldung in unserem Shop, werden Sie in der Lage sein schneller durch den Bestellvorgang geführt zu werden. Des Weiteren können Sie mehrere Versandadressen speichern und Bestellungen in Ihrem Konto verfolgen.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'Bundesland',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
-    'DD_DELETE_SHIPPING_ADDRESS'                            => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'auswählen',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'neue Adresse hinzufügen',
+    'DD_DELETE_SHIPPING_ADDRESS'                            => 'Lieferadresse löschen',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
-    'DD_LIST_SHOW_MORE'                                     => '',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filter',
+    'DD_LIST_SHOW_MORE'                                     => 'Produkte ansehen',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'zurück zur Übersicht',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
-    'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'herunterladen',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Dateigröße',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'Uhr',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Dateiname',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => '',
-    'OF'                                                    => 'VON',
-    'DD_RATING_CUSTOMERRATINGS'                             => '',
-    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
-    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'BACK_TO_OVERVIEW'                                      => 'Zur Übersicht',
+    'OF'                                                    => 'von',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Kundenmeinungen',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Kunden die sich diesen Artikel gekauft haben, kauften auch folgende Artikel.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'Folgende Artikel passen gut zu diesem Artikel.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Schauen Sie sich doch auch unsere ähnlichen Artikel an.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Kunden die sich diesen Artikel angesehen haben, haben sich auch folgende Artikel angesehen.',
+    'DETAILS_VPE_MESSAGE_1'                                 => "Dieser Artikel kann nur in Verpackungseinheiten zu je",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
-    'TO_CART_NOVARIANT'                                     => '',
+    'TO_CART_NOVARIANT'                                     => 'Variante wählen',
 
     // Modal basket
-    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Gesamtbetrag',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Gesamtsumme',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'weiter einkaufen',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => '',
+    'DD_BASKET_BACK_TO_SHOP'                                => 'zurück zum Shop',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => '',
-    'DD_FORGOT_PASSWORD_HEADING'                            => '',
-    'DD_INVITE_HEADING'                                     => '',
+    'DD_FOOTER_FOLLOW_US'                                   => 'Folgen Sie uns',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Passwort vergessen',
+    'DD_INVITE_HEADING'                                     => 'Artikel-Empfehlung',
     'DD_INVITE_LINK'                                        => 'Link',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
-    'DD_ORDERSHIPPED_HEADING'                               => '',
-    'DD_PRICEALARM_HEADING'                                 => 'Kundenpreis',
-    'DD_REGISTER_HEADING'                                   => '',
-    'DD_DOWNLOADLINKS_HEADING'                              => '',
-    'DD_WISHLIST_HEADING'                                   => '',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Ihre Newsletter-Anmeldung',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Versandbestätigung - Bestellung',
+    'DD_PRICEALARM_HEADING'                                 => 'Wunschpreis',
+    'DD_REGISTER_HEADING'                                   => 'Ihre Registrierung',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Ihre Downloadlinks - Bestellung',
+    'DD_WISHLIST_HEADING'                                   => 'Wunschzettel',
 
     'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menü',
 
-    'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
-    'DD_DELETE_MY_ACCOUNT'                                  => '',
-    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => '',
-    'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'Beenden',
-    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => '',
-    'DD_DELETE_MY_ACCOUNT_ERROR'                            => '',
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => 'Dieser Vorgang kann nicht rückgängig gemacht werden. Alle persönlichen Daten werden dauerhaft gelöscht.',
+    'DD_DELETE_MY_ACCOUNT'                                  => 'Konto löschen',
+    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => 'Sind Sie sicher, dass Sie Ihr Konto löschen wollen?',
+    'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'Abbrechen',
+    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => 'Ihr Konto wurde gelöscht',
+    'DD_DELETE_MY_ACCOUNT_ERROR'                            => 'Das Konto konnte nicht gelöscht werden',
 
     // Account -> My product reviews
-    'DD_DELETE_REVIEW_AND_RATING'                           => '',
-    'DD_REVIEWS_NOT_AVAILABLE'                              => '',
-    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => '',
+    'DD_DELETE_REVIEW_AND_RATING'                           => 'Bewertung und Sterne-Rating löschen',
+    'DD_REVIEWS_NOT_AVAILABLE'                              => 'Es liegen keine Bewertungen vor',
+    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => 'Sind Sie sicher, dass Sie die Bewertung löschen wollen?',
 
     // Contact page
-    'DD_SELECT_SALUTATION'                                  => 'Bitte wählen',
+    'DD_SELECT_SALUTATION'                                  => 'Bitte auswählen',
 
-    'DD_CATEGORY_RESET_BUTTON'                              => 'Reset',
+    'DD_CATEGORY_RESET_BUTTON'                              => 'Zurücksetzen',
 );
 
 /*
