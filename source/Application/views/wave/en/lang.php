bcb9e54d29ebb2dfc0894107fec8f966d186b26b
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 21:42:30 +0200

    New translations lang.php (English, United States)

--- source/Application/views/wave/en/lang.php
+++ source/Application/views/wave/en/lang.php
@@ -22,120 +22,120 @@
 $sLangName = "English";
 
 $aLang = array(
-    'charset'                                               => 'UTF-8',
+    'charset'                                               => '',
 
     // Global
-    'DD_SORT_DESC'                                          => 'descending',
-    'DD_SORT_ASC'                                           => 'ascending',
-    'DD_DEMO_ADMIN_TOOL'                                    => 'Open admin interface',
+    'DD_SORT_DESC'                                          => '',
+    'DD_SORT_ASC'                                           => '',
+    'DD_DEMO_ADMIN_TOOL'                                    => '',
     'DD_DELETE'                                             => 'Delete',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Please enter a valid email address.',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Passwords do not match.',
-    'DD_FORM_VALIDATION_NUMBER'                             => 'Please enter a number.',
-    'DD_FORM_VALIDATION_INTEGER'                            => 'There are no decimal places allowed.',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Please enter a positive number.',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Please enter a negative number.',
-    'DD_FORM_VALIDATION_REQUIRED'                           => 'Please specify a value for this required field.',
-    'DD_FORM_VALIDATION_CHECKONE'                           => 'Please select at least one option.',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
+    'DD_FORM_VALIDATION_NUMBER'                             => '',
+    'DD_FORM_VALIDATION_INTEGER'                            => '',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
+    'DD_FORM_VALIDATION_REQUIRED'                           => '',
+    'DD_FORM_VALIDATION_CHECKONE'                           => '',
 
     // Header
-    'SEARCH_TITLE'                                          => 'Enter a search term...',
+    'SEARCH_TITLE'                                          => '',
     'SEARCH_SUBMIT'                                         => 'Search',
     'NAV_MORE'                                              => 'More',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => 'Get informed about the latest products and offers per email.',
+    'FOOTER_NEWSLETTER_INFO'                                => '',
 
     // Home page
-    'MANUFACTURERSLIDER_SUBHEAD'                            => 'We present our carefully selected brands, whose products can be found in our shop.',
-    'START_BARGAIN_HEADER'                                  => 'Week\'s Special',
-    'START_NEWEST_HEADER'                                   => 'Just arrived',
-    'START_TOP_PRODUCTS_HEADER'                             => 'Top seller',
-    'START_BARGAIN_SUBHEADER'                               => 'Save money with our current bargains!',
-    'START_NEWEST_SUBHEADER'                                => 'Fresh as it gets. One minute in the box and now already in store.',
-    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Only %s products, but the best we can offer you.',
+    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
+    'START_BARGAIN_HEADER'                                  => '',
+    'START_NEWEST_HEADER'                                   => '',
+    'START_TOP_PRODUCTS_HEADER'                             => '',
+    'START_BARGAIN_SUBHEADER'                               => '',
+    'START_NEWEST_SUBHEADER'                                => '',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => 'Contact us!',
+    'DD_CONTACT_PAGE_HEADING'                               => '',
     'DD_CONTACT_FORM_HEADING'                               => 'Contact',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => 'Unfortunately, there are no links available.',
+    'DD_LINKS_NO_ENTRIES'                                   => '',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => 'You may find the information you want from our home page:',
-    'DD_ERR_404_START_BUTTON'                               => 'Go to home page',
-    'DD_ERR_404_CONTACT_TEXT'                               => 'May we assist you?<br>Feel free to call us or write an email:',
-    'DD_ERR_404_CONTACT_BUTTON'                             => 'to the contact page',
+    'DD_ERR_404_START_TEXT'                                 => '',
+    'DD_ERR_404_START_BUTTON'                               => '',
+    'DD_ERR_404_CONTACT_TEXT'                               => '',
+    'DD_ERR_404_CONTACT_BUTTON'                             => '',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'By creating an account with our store, you will be guided through the checkout process faster. In addition, you can store multiple shipping addresses and track orders in your account.',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'State',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'select',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'add delivery address',
-    'DD_DELETE_SHIPPING_ADDRESS'                            => 'Delete shipping address',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_DELETE_SHIPPING_ADDRESS'                            => '',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filter',
-    'DD_LIST_SHOW_MORE'                                     => 'View products',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
+    'DD_LIST_SHOW_MORE'                                     => '',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'back to overview',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'downloaded',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'File size:',
-    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'o\'clock',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'File name:',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => 'to overview',
-    'OF'                                                    => 'of',
-    'DD_RATING_CUSTOMERRATINGS'                             => 'Customer reviews',
-    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Customers who bought this item also bought one of the following products.',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'The following products fit well to this product.',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Take a look at our similar products.',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Customers who viewed this product also viewed the following products.',
-    'DETAILS_VPE_MESSAGE_1'                                 => "This product can only be ordered in packaging units of ",
+    'BACK_TO_OVERVIEW'                                      => '',
+    'OF'                                                    => 'OF',
+    'DD_RATING_CUSTOMERRATINGS'                             => '',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
+    'DETAILS_VPE_MESSAGE_1'                                 => "",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
-    'TO_CART_NOVARIANT'                                     => 'Choose a variant',
+    'TO_CART_NOVARIANT'                                     => '',
 
     // Modal basket
     'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Total',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'continue shopping',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => 'back to shop',
+    'DD_BASKET_BACK_TO_SHOP'                                => '',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => 'Follow us:',
-    'DD_FORGOT_PASSWORD_HEADING'                            => 'Forgot password',
-    'DD_INVITE_HEADING'                                     => 'Article recommendation',
+    'DD_FOOTER_FOLLOW_US'                                   => '',
+    'DD_FORGOT_PASSWORD_HEADING'                            => '',
+    'DD_INVITE_HEADING'                                     => '',
     'DD_INVITE_LINK'                                        => 'Link',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Your newsletter subscription',
-    'DD_ORDERSHIPPED_HEADING'                               => 'Delivery confirmation - Order',
-    'DD_PRICEALARM_HEADING'                                 => 'Wished price',
-    'DD_REGISTER_HEADING'                                   => 'Your registration',
-    'DD_DOWNLOADLINKS_HEADING'                              => 'Your download links - Order',
-    'DD_WISHLIST_HEADING'                                   => 'Wishlist',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
+    'DD_ORDERSHIPPED_HEADING'                               => '',
+    'DD_PRICEALARM_HEADING'                                 => 'Cust. Price',
+    'DD_REGISTER_HEADING'                                   => '',
+    'DD_DOWNLOADLINKS_HEADING'                              => '',
+    'DD_WISHLIST_HEADING'                                   => '',
 
     'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menu',
 
-    'DD_DELETE_MY_ACCOUNT_WARNING'                          => 'This action cannot be undone. This will permanently delete your personal data.',
-    'DD_DELETE_MY_ACCOUNT'                                  => 'Delete account',
-    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => 'Are you sure you want to delete your account?',
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
+    'DD_DELETE_MY_ACCOUNT'                                  => '',
+    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => '',
     'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'Cancel',
-    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => 'The account has been deleted',
-    'DD_DELETE_MY_ACCOUNT_ERROR'                            => 'The account could not have been deleted',
+    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => '',
+    'DD_DELETE_MY_ACCOUNT_ERROR'                            => '',
 
     // Account -> My product reviews
-    'DD_DELETE_REVIEW_AND_RATING'                           => 'Delete review and star rating',
-    'DD_REVIEWS_NOT_AVAILABLE'                              => 'No reviews available',
-    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => 'Are you sure you want to delete the review?',
+    'DD_DELETE_REVIEW_AND_RATING'                           => '',
+    'DD_REVIEWS_NOT_AVAILABLE'                              => '',
+    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => '',
 
     // Contact page
     'DD_SELECT_SALUTATION'                                  => 'Please choose',
