a752d36d1a1eaa937b27c38d32a9437c216011b8
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 21:46:14 +0200

    New translations oepaypal_lang.php (Japanese)

---
+++ source/modules/oe/oepaypal/translations/ja/oepaypal_lang.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of OXID eSales PayPal module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales PayPal module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2018
+ */
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset'                                    => '',
+    'OEPAYPAL_LOCALE'                            => '',
+    'OEPAYPAL_PARTNERBOX_LINK'                   => '',
+    'OEPAYPAL_ERROR_USER_ADDRESS'                => '',
+    'OEPAYPAL_LOGO_IMG'                          => '',
+    'OEPAYPAL_PAYMENT_HELP_LINK'                 => '',
+    'OEPAYPAL_PAYMENT_HELP_LINK_TEXT'            => '',
+    'OEPAYPAL_PAYMENT_NOSHIPPINGFOUND'           => '',
+    'OEPAYPAL_PAYMENT_NOT_VALID'                 => '',
+    'OEPAYPAL_ORDER_SUBJECT'                     => '',
+    'OEPAYPAL_ORDER_CONF_SUBJECT'                => '',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL'          => '',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL_HELPTEXT' => '',
+    'OEPAYPAL_PRICE'                             => '価格',
+    'OEPAYPAL_REQUIRED_FIELDS_EMPTY'             => '',
+    'OEPAYPAL_ORDER_ERROR'                       => '',
+    'OEPAYPAL_RESPONSE_FROM_PAYPAL'              => '',
+    'OEPAYPAL_ORDER_TOTAL_HAS_CHANGED'           => '',
+    'OEPAYPAL_SURCHARGE'                         => '割増',
+    'OEPAYPAL_TYPE_OF_PAYMENT'                   => '',
+    'OEPAYPAL_GIFTWRAPPER'                       => '',
+    'OEPAYPAL_GREETING_CARD'                     => 'グリーテイングカード',
+    'OEPAYPAL_GRAND_TOTAL'                       => '',
+    'OEPAYPAL_INFORMATION'                       => '',
+    'OEPAYPAL_SAME_ITEM_QUESTION'                => '',
+    'OEPAYPAL_SAME_ITEMS_QUESTION'               => '',
+    'OEPAYPAL_BUTTON_DO_NOT_ADD_ITEM'            => '',
+    'OEPAYPAL_BUTTON_ADD_ITEM'                   => '',
+    'OEPAYPAL_MINIBASKET_DISPLAY_BASKET'         => '',
+    'OEPAYPAL_CANCEL'                            => '',
+    'OEPAYPAL_SELECT_ANOTHER_SHIPMENT'           => '',
+    'OEPAYPAL_OR'                                => 'または',
+    'OEPAYPAL_BACK'                              => '',
+    'OEPAYPAL_PLUS_VAT'                          => '',
+    'OEPAYPAL_PAYMENT_INFORMATION'               => "",
+);
