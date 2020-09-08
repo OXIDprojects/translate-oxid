fde6d1aeb9e4dd1df7f9cdef03d34546f731ef1a
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 21:51:17 +0200

    New translations tcklarna_lang.php (German, informal)

--- source/modules/tc/tcklarna/translations/de/tcklarna_lang.php
+++ source/modules/tc/tcklarna/translations/de/tcklarna_lang.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$sLangName = "Deutsch";
+$sLangName = "English";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
