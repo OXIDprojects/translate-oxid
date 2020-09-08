1b5ef91c0cfc7adb623fb4ea6f9c0dfee4dbd973
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 21:50:39 +0200

    New translations tcklarna_lang.php (Norwegian)

---
+++ source/modules/tc/tcklarna/translations/no/tcklarna_lang.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Copyright 2018 Klarna AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+$sLangName = "English";
+// -------------------------------
+// RESOURCE IDENTITFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset' => '',
+
+    'TCKLARNA_EXCEPTION_OUT_OF_STOCK' => '',
+    'TCKLARNA_CHECKOUT'               => '',
+
+    'TCKLARNA_DISCOUNT_TITLE'                     => '',
+    'TCKLARNA_VOUCHER_DISCOUNT'                   => '',
+    'TCKLARNA_GIFT_WRAPPING_TITLE'                => '',
+    'TCKLARNA_GIFT_CARD_TITLE'                    => '',
+    'TCKLARNA_PAYMENT_FEE_TITLE'                  => '',
+    'TCKLARNA_TRUSTED_SHOPS_EXCELLENCE_FEE_TITLE' => '',
+
+    'TCKLARNA_PASSWORD'                          => '',
+    'TCKLARNA_TRUSTED_SHOP_BUYER_PROTECTION'     => '',
+    'TCKLARNA_ALREADY_A_CUSTOMER'                => 'jeg er allerede kunde',
+    'TCKLARNA_LAW_NOTICE'                        => '',
+    'TCKLARNA_OUTSIDE_VOUCHER'                   => '',
+    'TCKLARNA_GO_TO_CHECKOUT'                    => '',
+    'TCKLARNA_BUY_NOW'                           => '',
+    'TCKLARNA_USE_AS_DELIVERY_ADDRESS'           => '',
+    'TCKLARNA_CHOOSE_DELIVERY_ADDRESS'           => '',
+    'TCKLARNA_CREATE_USER_ACCOUNT'               => '',
+    'TCKLARNA_SUBSCRIBE_TO_NEWSLETTER'           => '',
+    'TCKLARNA_CREATE_USER_ACCOUNT_AND_SUBSCRIBE' => '',
+    'TCKLARNA_NO_CHECKBOX'                       => '',
+    'TCKLARNA_ALLOW_SEPARATE_SHIPPING_ADDRESS'   => '',
+    'TCKLARNA_PHONE_NUMBER_MANDATORY'            => '',
+    'TCKLARNA_DATE_OF_BIRTH_MANDATORY'           => '',
+    'TCKLARNA_CHOOSE_YOUR_SHIPPING_COUNTRY'      => '',
+    'TCKLARNA_CHOOSE_YOUR_NOT_SUPPORTED_COUNTRY' => '',
+    'TCKLARNA_MORE_COUNTRIES'                    => '',
+    'TCKLARNA_MY_COUNTRY_IS_NOT_LISTED'          => '',
+    'TCKLARNA_OTHER_COUNTRY'                     => '',
+    'TCKLARNA_RESET_COUNTRY'                     => '',
+    'TCKLARNA_CHANGE_COUNTRY'                    => '',
+    'TCKLARNA_LOGIN_INTO_AMAZON'                 => '',
+    'KLARNA_ORDER_NOT_IN_SYNC'             => '',
+    'KLARNA_ORDER_IS_CANCELLED'            => '',
+    'KLARNA_SEE_ORDER_IN_PORTAL'           => '',
+
+    'KLARNA_WENT_WRONG_TRY_AGAIN' => '',
+    'KLARNA_WRONG_URLS_CONFIG'    => '',
+
+    'TCKLARNA_KP_CURRENCY_DONT_MATCH'      => '',
+    'TCKLARNA_KP_MATCH_ERROR'              => '',
+    'TCKLARNA_KP_INVALID_TOKEN'            => '',
+    'TCKLARNA_KP_ORDER_DATA_CHANGED'       => '',
+    'KP_NOT_AVAILABLE_FOR_COMPANIES'  => '',
+    'KP_AVAILABLE_FOR_PRIVATE_ONLY'   => '',
+    'KP_AVAILABLE_FOR_COMPANIES_ONLY' => '',
+    'TCKLARNA_KP_NOT_KLARNA_CORE_COUNTRY'  => '',
+
+    'TCKLARNA_PLEASE_AGREE_TO_TERMS'            => '',
+    'TCKLARNA_ERROR_NOT_ENOUGH_IN_STOCK'        => '',
+    'TCKLARNA_ERROR_NO_SHIPPING_METHODS_SET_UP' => '',
+
+    'TCKLARNA_PAY_LATER_SUBTITLE'    => '',
+    'TCKLARNA_SLICE_IT_SUBTITLE'     => '',
+    'TCKLARNA_PAY_NOW_SUBTITLE'      => '',
+    'TCKLARNA_ORDER_AMOUNT_TOO_HIGH' => '',
+
+    'TCKLARNA_ANONYMIZED_PRODUCT'                  => '',
+
+    'TCKLARNA_IS_ERROR_DEFAULT' => '',
+);
+
