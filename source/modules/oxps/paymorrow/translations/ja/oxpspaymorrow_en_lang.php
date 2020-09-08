15ad13395e0ca940d4d4535cd301d4139ce099fd
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 21:45:50 +0200

    New translations oxpspaymorrow_en_lang.php (Japanese)

---
+++ source/modules/oxps/paymorrow/translations/ja/oxpspaymorrow_en_lang.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of the OXID module for Paymorrow payment.
+ *
+ * The OXID module for Paymorrow payment is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The OXID eShop module for Paymorrow payment is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms and
+ * conditions of the license of that module. An independent module is a module
+ * which is not derived from or based on this library. If you modify this library,
+ * you may extend this exception to your version of the library, but you are not
+ * obliged to do so. If you do not wish to do so, delete this exception statement
+ * from your version.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * the OXID module for Paymorrow payment. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$sLangName = "English";
+
+$aLang = array(
+    "charset"                                        => "",
+
+    'PAYMORROW_PAYMENT_METHOD_NAME_INVOICE'          => '請求',
+    'PAYMORROW_PAYMENT_METHOD_NAME_DIRECT_DEBIT'     => '',
+
+    'PAYMORROW_PAYMENT_NO_JAVASCRIPT'                => '',
+
+    'PAYMORROW_GENERAL_ERROR'                        => '',
+    'PAYMORROW_ACCEPT_CONDITIONS_ERROR'              => '',
+    'PAYMORROW_SELECT_GENDER_ERROR'                  => '',
+    'PAYMORROW_DATE_OF_BIRTH_ERROR'                  => '',
+    'PAYMORROW_MOBILE_NUMBER_ERROR'                  => '',
+
+    // Custom
+    'PAYMORROW_ORDER_DATA_COLLECTION_FAILED'         => '',
+    'PAYMORROW_ORDER_SAVING_TEMPORARY_ORDER_FAILED'  => '',
+
+    // Email
+    'EMAIL_ORDER_CUST_HTML_PAYMENTMETHOD'            => '',
+    'PAYMORROW_EMAIL_ORDER_CUST_HTML_BANK'           => '',
+    'PAYMORROW_EMAIL_ORDER_CUST_HTML_IBAN'           => '',
+    'PAYMORROW_EMAIL_ORDER_CUST_HTML_BIC'            => '',
+    'PAYMORROW_EMAIL_ORDER_CUST_HTML_REFERENCE_LINE' => '',
+    'PAYMORROW_EMAIL_ORDER_CUST_HTML_ORDER_ID'       => '',
+    'PAYMORROW_EMAIL_ORDER_CUST_HTML_CUSTOMER_NR'    => '',
+);
