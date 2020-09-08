1cd6bb51f7521e3201b919b6832085067b556aa1
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-08 21:51:15 +0200

    New translations oxpspaymorrow_admin_en_lang.php (German, informal)

--- source/modules/oxps/paymorrow/views/admin/de/oxpspaymorrow_admin_en_lang.php
+++ source/modules/oxps/paymorrow/views/admin/de/oxpspaymorrow_admin_en_lang.php
@@ -30,7 +30,7 @@
  * the OXID module for Paymorrow payment. If not, see <http://www.gnu.org/licenses/>.
  */
 
-$sLangName = "Deutsch";
+$sLangName = "English";
 
 $aLang = array(
     "charset"                                      => "UTF-8",
