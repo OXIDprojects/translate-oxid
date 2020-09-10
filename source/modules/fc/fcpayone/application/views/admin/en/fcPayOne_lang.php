8c9c47ce97e70011263c27fa1a2b99ee985743ff
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 13:55:12 +0200

    New translations fcPayOne_lang.php (English, Australia)

--- source/modules/fc/fcpayone/application/views/admin/en/fcPayOne_lang.php
+++ source/modules/fc/fcpayone/application/views/admin/en/fcPayOne_lang.php
@@ -384,7 +384,7 @@ $aLang = array(
 'ORDER_OVERVIEW_FCPO_ELV_BLZ'               => "",
 'ORDER_OVERVIEW_FCPO_ELV_KTONR'             => "",
 'ORDER_OVERVIEW_FCPO_ELV_IBAN'              => "IBAN:",
-'ORDER_OVERVIEW_FCPO_ELV_BIC'               => "BIC:",
+'ORDER_OVERVIEW_FCPO_ELV_BIC'               => "Swift Code:",
 
 'FCPO_CC_CONFIG'                            => '',
 'FCPO_CC_STANDARD_STYLE'                    => '',
