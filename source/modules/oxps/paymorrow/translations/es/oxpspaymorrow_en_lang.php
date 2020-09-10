a8542ebc1e754120cdea0a9c6c89816609617f51
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 20:39:13 +0200

    New translations oxpspaymorrow_en_lang.php (Spanish)

--- source/modules/oxps/paymorrow/translations/es/oxpspaymorrow_en_lang.php
+++ source/modules/oxps/paymorrow/translations/es/oxpspaymorrow_en_lang.php
@@ -30,7 +30,7 @@
  * the OXID module for Paymorrow payment. If not, see <http://www.gnu.org/licenses/>.
  */
 
-$sLangName = "English";
+$sLangName = "Español";
 
 $aLang = array(
     "charset"                                        => "",
