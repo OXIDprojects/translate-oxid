916b81d978d0af027bd3d8c26aca64ba9e03a66e
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 22:40:59 +0200

    New translations theme_options.php (Slovak)

--- source/Application/views/wave/sk/theme_options.php
+++ source/Application/views/wave/sk/theme_options.php
@@ -52,17 +52,17 @@ $aLang = array(
 
     'SHOP_THEME_sZoomImageSize'                     => '', //SHOP_CONFIG_ZOOMIMAGESIZE
     'SHOP_THEME_sCatThumbnailsize'                  => '', //SHOP_CONFIG_CATEGORYTHUMBNAILSIZE
-    'HELP_SHOP_THEME_sCatThumbnailsize'             => '', //SHOP_CONFIG_CATEGORYTHUMBNAILSIZE
+    'HELP_SHOP_THEME_sCatThumbnailsize'             => 'V zobrazení kategórie sa zobrazuje obrázok vybranej kategórie v definovanej veľkosti.', //SHOP_CONFIG_CATEGORYTHUMBNAILSIZE
     'SHOP_THEME_aDetailImageSizes'                  => '', //SHOP_CONFIG_DETAILIMAGESIZE
 
     'SHOP_THEME_sManufacturerIconsize'              => '', // Check if this is really manufacturer or if it is more like "brand"
     'HELP_SHOP_THEME_sManufacturerIconsize'         => '',
 
     'SHOP_THEME_sCatIconsize'                       => '',
-    'HELP_SHOP_THEME_sCatIconsize'                  => '',
+    'HELP_SHOP_THEME_sCatIconsize'                  => 'V zobrazení kategórie sa zobrazujú kategórie obrázkov podkategórií v definovanej veľkosti.',
 
     'SHOP_THEME_sCatPromotionsize'                  => '',
-    'HELP_SHOP_THEME_sCatPromotionsize'             => '',
+    'HELP_SHOP_THEME_sCatPromotionsize'             => 'Propagácia kategórie na úvodnej stránke potrebuje špeciálnu veľkosť obrázkov kategórie. Tu definujte veľkosť týchto obrázkov.',
 
     'SHOP_THEME_blFooterShowHelp'                   => '',
     'SHOP_THEME_blFooterShowLinks'                  => '',
@@ -79,14 +79,14 @@ $aLang = array(
     'SHOP_THEME_iTopNaviCatCount'                   => '', //SHOP_CONFIG_TOPNAVICATCOUNT
     'SHOP_THEME_blShowFinalStep'                    => '', //SHOP_SYSTEM_SHOWFINALSTEP
     'SHOP_THEME_iNewBasketItemMessage'              => '', //SHOP_SYSTEM_SHOWNEWBASKETITEMMESSAGE
-    'HELP_SHOP_THEME_iNewBasketItemMessage'         => '', //SHOP_SYSTEM_SHOWNEWBASKETITEMMESSAGE
+    'HELP_SHOP_THEME_iNewBasketItemMessage'         => 'Keď zákazník pridá produkty do košíka, OXID eShop sa môže správať inak. Nastavte, čo sa stane, aby zákazník poskytol správnu spätnú väzbu.', //SHOP_SYSTEM_SHOWNEWBASKETITEMMESSAGE
     'SHOP_THEME_iNewBasketItemMessage_0'            => 'nič',
     'SHOP_THEME_iNewBasketItemMessage_1'            => '',
     'SHOP_THEME_iNewBasketItemMessage_2'            => '',
     'SHOP_THEME_iNewBasketItemMessage_3'            => '',
 
     'SHOP_THEME_blShowListDisplayType'              => '',
-    'HELP_SHOP_THEME_blShowListDisplayType'         => '',
+    'HELP_SHOP_THEME_blShowListDisplayType'         => 'Rozhodnite sa, či návštevník vášho obchodu môže vybrať typ zoznamu produktov v prednej časti obchodu. Ak tieto možnosti nie sú aktivované, vaši návštevníci uvidia zobrazené zoznamy, ako ste si upravili do rámčeka "Predvolený typ zoznamu produktov".',
     'SHOP_THEME_sDefaultListDisplayType'            => '',
     'SHOP_THEME_sDefaultListDisplayType_grid'       => 'Mriežka',
     'SHOP_THEME_sDefaultListDisplayType_line'       => '',
@@ -100,26 +100,26 @@ $aLang = array(
     'SHOP_THEME_aNrofCatArticles'                   => '',
 
     'SHOP_THEME_sFacebookUrl'                       => '',
-    'HELP_SHOP_THEME_sFacebookUrl'                  => '',
+    'HELP_SHOP_THEME_sFacebookUrl'                  => 'Napríklad: https://www.facebook.com/oxidesales',
     'SHOP_THEME_sTwitterUrl'                        => '',
-    'HELP_SHOP_THEME_sTwitterUrl'                   => '',
+    'HELP_SHOP_THEME_sTwitterUrl'                   => 'Napríklad: https://twitter.com/OXID_eSales',
     'SHOP_THEME_sBlogUrl'                           => '',
-    'HELP_SHOP_THEME_sBlogUrl'                      => '',
+    'HELP_SHOP_THEME_sBlogUrl'                      => 'Napríklad: http://blog.oxid-esales.com',
     'SHOP_THEME_sYouTubeUrl'                        => '',
-    'HELP_SHOP_THEME_sYouTubeUrl'                   => '',
+    'HELP_SHOP_THEME_sYouTubeUrl'                   => 'Napríklad: https://www.youtube.com/user/oxidesales',
     'SHOP_THEME_sGooglePlusUrl'                     => '',
-    'HELP_SHOP_THEME_sGooglePlusUrl'                => '',
+    'HELP_SHOP_THEME_sGooglePlusUrl'                => 'Napríklad: https://plus.google.com/114133952363540971062',
 
     'SHOP_THEME_sGoogleMapsAddr'                    => '',
-    'HELP_SHOP_THEME_sGoogleMapsAddr'               => '',
+    'HELP_SHOP_THEME_sGoogleMapsAddr'               => 'Napríklad: OXID eSales AG, Bertoldstraße 48, 79098 Freiburg',
 
     'SHOP_THEME_bl_showManufacturerSlider'          => '',
 
     'SHOP_THEME_blUseGAPageTracker'                 => '',
     'SHOP_THEME_blUseGAEcommerceTracking'           => '',
-    'HELP_SHOP_THEME_blUseGAEcommerceTracking'      => '',
+    'HELP_SHOP_THEME_blUseGAEcommerceTracking'      => 'Nezabudnite, že táto možnosť musí byť povolená aj vo vašom účte Google Analytics. Manuál:<a href="https://support.google.com/analytics/answer/1009612?hl=en&ref_topic=1037061" target="_blank">Link</a>',
     'SHOP_THEME_sGATrackingId'                      => '',
-    'HELP_SHOP_THEME_sGATrackingId'                 => '',
+    'HELP_SHOP_THEME_sGATrackingId'                 => 'Napríklad: UA-XXXXXXXX-1',
     'SHOP_THEME_blGAAnonymizeIPs'                   => '',
 
     'SHOP_THEME_sLogoFile'                          => '',
@@ -131,10 +131,10 @@ $aLang = array(
     'HELP_SHOP_THEME_sLogoHeight'                   => '',
 
     'SHOP_THEME_sEmailLogo'                         => '',
-    'HELP_SHOP_THEME_sEmailLogo'                    => '',
+    'HELP_SHOP_THEME_sEmailLogo'                    => 'Toto je relatívne k ceste <code>/out/flow/img/</code>.',
 
     'SHOP_THEME_sFaviconFile'                       => '',
-    'HELP_SHOP_THEME_sFaviconFile'                  => '',
+    'HELP_SHOP_THEME_sFaviconFile'                  => 'Táto favikón sa má zobrazovať na karte prehliadača. <br> Vo všeobecnosti ide o súbor ico. <br> Je to relatívne k ceste <code>/out/flow/img/favicons/</code>.',
     'SHOP_THEME_sFavicon16File'                     => '',
     'SHOP_THEME_sFavicon32File'                     => '',
     'SHOP_THEME_sFavicon48File'                     => '',
@@ -142,13 +142,13 @@ $aLang = array(
     'SHOP_THEME_sFavicon128File'                    => '',
     'SHOP_THEME_sFavicon512File'                    => '',
     'SHOP_THEME_sFaviconMSTileColor'                => '',
-    'HELP_SHOP_THEME_sFaviconMSTileColor'           => '',
+    'HELP_SHOP_THEME_sFaviconMSTileColor'           => 'Určené ako hexadecimálna hodnota.<br>Napríklad: #D83434',
 
     'SHOP_THEME_blUseBackground'                    => '',
     'SHOP_THEME_sBackgroundColor'                   => '',
-    'HELP_SHOP_THEME_sBackgroundColor'              => '',
+    'HELP_SHOP_THEME_sBackgroundColor'              => 'Určené ako hexadecimálna hodnota.<br>Napríklad: #D83434',
     'SHOP_THEME_sBackgroundPath'                    => '',
-    'HELP_SHOP_THEME_sBackgroundPath'               => '',
+    'HELP_SHOP_THEME_sBackgroundPath'               => 'Toto je relatívne k ceste <code>/out/flow/img/backgrounds/</code>.',
     'SHOP_THEME_sBackgroundRepeat'                  => '',
     'SHOP_THEME_sBackgroundRepeat_no-repeat'        => 'Nie',
     'SHOP_THEME_sBackgroundRepeat_repeat-x'         => '',
@@ -172,26 +172,26 @@ $aLang = array(
 
     'SHOP_THEME_blUseGoogleTS'                      => '',
     'SHOP_THEME_sGoogleVendorId'                    => '',
-    'HELP_SHOP_THEME_sGoogleVendorId'               => '',
+    'HELP_SHOP_THEME_sGoogleVendorId'               => 'Toto ID nájdete v informačnom paneli Dôveryhodných obchodov - Trusted Stores Merchant Dashboard.',
     'SHOP_THEME_sGoogleShoppingAccountId'           => '',
-    'HELP_SHOP_THEME_sGoogleShoppingAccountId'      => '',
+    'HELP_SHOP_THEME_sGoogleShoppingAccountId'      => 'Zadajte toto pole len vtedy, ak odovzdáte informačné kanály pre službu Nákupy Google. <br> <br> ID účtu Google Merchant Center. Táto hodnota by mala zodpovedať ID účtu, ktorý používate na odoslanie <a href="https://support.google.com/merchants/answer/188494#US" target="_blank">product data feed to Google Shopping</a> Prostredníctvom služby Google Merchant Center. Ak máte účet MCA, použite ID podúčtu priradeného k danému informačnému kanálu produktu.',
     'SHOP_THEME_sPageLanguage'                      => '',
     'HELP_SHOP_THEME_sPageLanguage'                 => '',
     'SHOP_THEME_sShoppingCountry'                   => '',
     'HELP_SHOP_THEME_sShoppingCountry'              => '',
     'SHOP_THEME_sShoppingLanguage'                  => '',
     'HELP_SHOP_THEME_sShoppingLanguage'             => '',
-    'SHOP_THEME_sShippingDaysOnStock'               => '',
-    'HELP_SHOP_THEME_sShippingDaysOnStock'          => '',
-    'SHOP_THEME_sShippingDaysNotOnStock'            => '',
-    'HELP_SHOP_THEME_sShippingDaysNotOnStock'       => '',
+    'SHOP_THEME_sShippingDaysOnStock'               => 'Počet dní do odoslania objednávky s produktami, ktoré sú všetky skladom. (povinné)',
+    'HELP_SHOP_THEME_sShippingDaysOnStock'          => 'Tu sa jedná o približný dátum odoslania objednávky; nejedná sa o predpokladaný dátum dodania.<br><br>Pokiaľ objednávka obsahuje viac produktov, zvoľte to najvzdialenejší predpokladaný dátum odoslania.<br><br>Pokiaľ udávate zákazníkovi časový interval, zadajte posledný deň tohoto úseku. Pokiaľ napr. zasielate v rozmedzí od 5 do 7 dní, zadajte 7 dní.<br><br>Tento tu zadaný termín bude zákazníkovi oznámený e-mailom.<br><br>Po predpokladanom dátume odoslania bude zákazníkovi odoslaný dotazník ohľadom nákupu.',
+    'SHOP_THEME_sShippingDaysNotOnStock'            => 'Počet dní do odoslania objednávky, v prípade že aspoň 1 produkt nie je skladom.(povinné)',
+    'HELP_SHOP_THEME_sShippingDaysNotOnStock'       => 'Predpokladaný termín, pri ktorom bude odoslaná objednávka; To sa líši od predpokladaného dátumu dodania. <br> V prípade, že objednávka bude obsahovať viac položiek. vyberte posledný približný dátum odoslania. <br> Ak je hlásenie zákazníkom v rozsahu dní. uveďte koniec rozsahu. (Napríklad 7 dní, ak váš rozsah pre prepravu je 5-7 dní). <br> Dátum tu uvedený bude poskytovaný zákazníkovi prostredníctvom e-mailu. <br> Zákazník dostane prehľad potvrdenia, že lehota nákupu približný dátum odoslania vypršala.',
     'SHOP_THEME_sDeliveryDaysOnStock'               => '',
-    'HELP_SHOP_THEME_sDeliveryDaysOnStock'          => '',
+    'HELP_SHOP_THEME_sDeliveryDaysOnStock'          => 'Odhadovaný dátum, od ktorého očakávate doručenie objednávky zákazníkovi. Ak objednávka obsahuje viac položiek, vyberte najnovší dátum doručenia. Ak oznámete niekoľko dní, uveďte prosím neskorší koniec rozsahu. (Napr. 7 dní, ak je váš rozsah 5-7 dní).',
     'SHOP_THEME_sDeliveryDaysNotOnStock'            => '',
-    'HELP_SHOP_THEME_sDeliveryDaysNotOnStock'       => '',
+    'HELP_SHOP_THEME_sDeliveryDaysNotOnStock'       => 'Odhadovaný dátum, od ktorého očakávate doručenie objednávky zákazníkovi. Ak objednávka obsahuje viac položiek, vyberte najnovší dátum doručenia. Ak oznámete niekoľko dní, uveďte prosím neskorší koniec rozsahu. (Napr. 7 dní, ak je váš rozsah 5-7 dní).',
 
     'SHOP_THEME_blSliderShowImageCaption'           => '',
-    'HELP_SHOP_THEME_blSliderShowImageCaption'      => '',
+    'HELP_SHOP_THEME_blSliderShowImageCaption'      => 'Titulok je viditeľný, ak má aktívny snímok priradený produkt.',
 
     'SHOP_THEME_blEcondaRecommendationsStart'       => '',
     'SHOP_THEME_sEcondaWidgetIdStart'               => '',
