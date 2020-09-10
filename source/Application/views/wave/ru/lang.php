d2ed360e853aaf29b6276c55cad877c6de9a2c4c
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 22:41:56 +0200

    New translations lang.php (Russian)

--- source/Application/views/wave/ru/lang.php
+++ source/Application/views/wave/ru/lang.php
@@ -25,103 +25,103 @@ $aLang = array(
     'charset'                                               => '',
 
     // Global
-    'DD_SORT_DESC'                                          => '',
-    'DD_SORT_ASC'                                           => '',
-    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_SORT_DESC'                                          => 'уменьш.',
+    'DD_SORT_ASC'                                           => 'увелич.',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Открыть панель администратора',
     'DD_DELETE'                                             => 'Удалить',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
-    'DD_FORM_VALIDATION_NUMBER'                             => '',
-    'DD_FORM_VALIDATION_INTEGER'                            => '',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_REQUIRED'                           => '',
-    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Укажите существующий e-mail.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Пароли не совпаюдают.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Укажите число.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Не допускаются дробные значения.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Укажите положительную величину.',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Укажите отрицательную величину.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Заполните обязательное поле',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Выберите вариант',
 
     // Header
-    'SEARCH_TITLE'                                          => '',
-    'SEARCH_SUBMIT'                                         => 'Поиск',
+    'SEARCH_TITLE'                                          => 'поиск ...',
+    'SEARCH_SUBMIT'                                         => 'Искать',
     'NAV_MORE'                                              => 'Больше',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => '',
+    'FOOTER_NEWSLETTER_INFO'                                => 'Получайте информацию о новинках и акциях на e-mail.',
 
     // Home page
-    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
-    'START_BARGAIN_HEADER'                                  => '',
-    'START_NEWEST_HEADER'                                   => '',
-    'START_TOP_PRODUCTS_HEADER'                             => '',
-    'START_BARGAIN_SUBHEADER'                               => '',
-    'START_NEWEST_SUBHEADER'                                => '',
-    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
+    'MANUFACTURERSLIDER_SUBHEAD'                            => 'Бренды, представленные в каталоге товаров',
+    'START_BARGAIN_HEADER'                                  => 'Товары недели',
+    'START_NEWEST_HEADER'                                   => 'Новинки',
+    'START_TOP_PRODUCTS_HEADER'                             => 'Лидеры продаж',
+    'START_BARGAIN_SUBHEADER'                               => 'Успейте сэкономить!',
+    'START_NEWEST_SUBHEADER'                                => 'Свежее поступление',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Только 2 товара, но это наше лучшее предложение',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => '',
-    'DD_CONTACT_FORM_HEADING'                               => 'Контакт',
+    'DD_CONTACT_PAGE_HEADING'                               => 'Обратная связь',
+    'DD_CONTACT_FORM_HEADING'                               => 'Контакты',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => '',
+    'DD_LINKS_NO_ENTRIES'                                   => 'К сожалению, ссылки недоступны.',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => '',
-    'DD_ERR_404_START_BUTTON'                               => '',
-    'DD_ERR_404_CONTACT_TEXT'                               => '',
-    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+    'DD_ERR_404_START_TEXT'                                 => 'Вы можете найти информацию на главной странице:',
+    'DD_ERR_404_START_BUTTON'                               => 'Перейти на главную',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Нужна помощь?<br>Позвоните или напишите нам:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'связаться с нами',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'После регистрации вы сможете оформлять заказы быстрее, а также вам будут доступны варианты адресов доставки и функция отслеживания заказов.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'Штат',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'выберите',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'добавить адрес',
     'DD_DELETE_SHIPPING_ADDRESS'                            => '',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
-    'DD_LIST_SHOW_MORE'                                     => '',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Фильтр',
+    'DD_LIST_SHOW_MORE'                                     => 'Смотреть товары ...',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'назад к списку',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
-    'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'скачать',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Размер файла:',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'часов',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Имя файла:',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => '',
-    'OF'                                                    => 'ОТ',
-    'DD_RATING_CUSTOMERRATINGS'                             => '',
-    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
-    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'BACK_TO_OVERVIEW'                                      => 'в меню',
+    'OF'                                                    => 'из',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Отзывы покупателей',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Клиенты, купившие этот товар, также приобрели:',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'К этому товару хорошо подойдёт',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Взгляните на похожие товары',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Клиенты, что просматривали этот товар также смотрели',
+    'DETAILS_VPE_MESSAGE_1'                                 => "Этот товара можно заказать только с",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
     'TO_CART_NOVARIANT'                                     => '',
 
     // Modal basket
-    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Итого',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Итог',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'назад в магазин',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => '',
+    'DD_BASKET_BACK_TO_SHOP'                                => 'вернуться в магазин',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => '',
-    'DD_FORGOT_PASSWORD_HEADING'                            => '',
-    'DD_INVITE_HEADING'                                     => '',
+    'DD_FOOTER_FOLLOW_US'                                   => 'Присоединяйтесь:',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Забыли пароль?',
+    'DD_INVITE_HEADING'                                     => 'Рекомендовать товар',
     'DD_INVITE_LINK'                                        => 'Ссылка',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
-    'DD_ORDERSHIPPED_HEADING'                               => '',
-    'DD_PRICEALARM_HEADING'                                 => 'Цена клиента',
-    'DD_REGISTER_HEADING'                                   => '',
-    'DD_DOWNLOADLINKS_HEADING'                              => '',
-    'DD_WISHLIST_HEADING'                                   => '',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Подписка на новости',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Подтверждение доставки - Заказ',
+    'DD_PRICEALARM_HEADING'                                 => 'Уведомление о скидке',
+    'DD_REGISTER_HEADING'                                   => 'Регистрация',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Ваши ссылки для скачивания - заказы',
+    'DD_WISHLIST_HEADING'                                   => 'Список желаний',
 
     'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Меню',
 
