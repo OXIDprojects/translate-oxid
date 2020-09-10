03e2d4a4a7c870f6d4a3f8372c8cfaa7b69bcda9
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 13:51:33 +0200

    New translations bestitamazonpay4oxid_lang.php (Arabic)

---
+++ source/modules/bestit/amazonpay4oxid/application/views/admin/ar/bestitamazonpay4oxid_lang.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * Backend language entries for EN
+ *
+ * @author best it GmbH & Co. KG <info@bestit-online.de>
+ */
+
+$sLangName  = "English";
+
+$aLang = array(
+    'charset'                                               => 'UTF-8',
+
+    'SHOP_MODULE_GROUP_bestitAmazonPay4OxidSettings'       => '',
+    'SHOP_MODULE_blAmazonSandboxActive'                     => '',
+    'SHOP_MODULE_sAmazonSellerId'                           => '',
+    'SHOP_MODULE_sAmazonAWSAccessKeyId'                     => '',
+    'SHOP_MODULE_sAmazonSignature'                          => '',
+    'SHOP_MODULE_blAmazonLogging'                           => '',
+    'SHOP_MODULE_blAmazonLoggingLevel'                     => '',
+    'SHOP_MODULE_blAmazonLoggingLevel_debug'               => '',
+    'SHOP_MODULE_blAmazonLoggingLevel_error'               => 'خطأ',
+
+    'SHOP_MODULE_GROUP_bestitAmazonPay4OxidLoginSettings'        => '',
+    'SHOP_MODULE_blAmazonLoginActive'                             => '',
+    'SHOP_MODULE_sAmazonLoginClientId'                            => '',
+    'SHOP_MODULE_sAmazonLoginButtonStyle'                         => '',
+    'SHOP_MODULE_sAmazonPayButtonStyle'                           => '',
+    'SHOP_MODULE_sAmazonLoginButtonStyle_LwA-LightGray'           => '',
+    'SHOP_MODULE_sAmazonLoginButtonStyle_LwA-Gold'                => '',
+    'SHOP_MODULE_sAmazonLoginButtonStyle_LwA-DarkGray'            => '',
+    'SHOP_MODULE_sAmazonLoginButtonStyle_Login-LightGray'         => '',
+    'SHOP_MODULE_sAmazonLoginButtonStyle_Login-Gold'              => '',
+    'SHOP_MODULE_sAmazonLoginButtonStyle_Login-DarkGray'          => '',
+    'SHOP_MODULE_sAmazonPayButtonStyle_PwA-LightGray'             => '',
+    'SHOP_MODULE_sAmazonPayButtonStyle_PwA-Gold'                  => '',
+    'SHOP_MODULE_sAmazonPayButtonStyle_PwA-DarkGray'              => '',
+    'SHOP_MODULE_sAmazonPayButtonStyle_Pay-LightGray'             => '',
+    'SHOP_MODULE_sAmazonPayButtonStyle_Pay-Gold'                  => '',
+    'SHOP_MODULE_sAmazonPayButtonStyle_Pay-DarkGray'              => '',
+
+    'SHOP_MODULE_GROUP_bestitAmazonPay4OxidLocalization'   => '',
+    'SHOP_MODULE_sAmazonLocale'                             => '',
+    'SHOP_MODULE_sAmazonLocale_DE'                          => '',
+    'SHOP_MODULE_sAmazonLocale_UK'                          => '',
+    'SHOP_MODULE_sAmazonLocale_US'                          => '',
+
+    'SHOP_MODULE_GROUP_bestitAmazonPay4OxidConfiguration'   => '',
+    'SHOP_MODULE_sAmazonMode'                               => '',
+    'SHOP_MODULE_sAmazonMode_BASIC_FLOW'                    => '',
+    'SHOP_MODULE_sAmazonMode_OPTIMIZED_FLOW'                => '',
+    'SHOP_MODULE_sAmazonAuthorize'                          => '',
+    'SHOP_MODULE_sAmazonCapture'                            => '',
+    'SHOP_MODULE_blAmazonERP'                               => '',
+    'SHOP_MODULE_sAmazonERPModeStatus'                      => '',
+    'SHOP_MODULE_sAmazonAuthorize_IPN'                      => '',
+    'SHOP_MODULE_sAmazonAuthorize_CRON'                     => '',
+    'SHOP_MODULE_sAmazonCapture_DIRECT'                     => '',
+    'SHOP_MODULE_sAmazonCapture_SHIPPED'                    => '',
+
+    'SHOP_MODULE_sSandboxSimulation'                                                 => '',
+    'SHOP_MODULE_sSandboxSimulation_'                                                => '',
+    'SHOP_MODULE_sSandboxSimulation_SetOrderReferenceDetailsPaymentMethodNotAllowed' => '',
+    'SHOP_MODULE_sSandboxSimulation_CloseOrderReferenceAmazonClosed'                 => '',
+    'SHOP_MODULE_sSandboxSimulation_AuthorizeInvalidPaymentMethod'                   => '',
+    'SHOP_MODULE_sSandboxSimulation_AuthorizeAmazonRejected'                         => '',
+    'SHOP_MODULE_sSandboxSimulation_AuthorizeTransactionTimedOut'                    => '',
+    'SHOP_MODULE_sSandboxSimulation_AuthorizeExpiredUnused'                          => '',
+    'SHOP_MODULE_sSandboxSimulation_AuthorizeAmazonClosed'                           => '',
+    'SHOP_MODULE_sSandboxSimulation_CapturePending'                                  => '',
+    'SHOP_MODULE_sSandboxSimulation_CaptureAmazonRejected'                           => '',
+    'SHOP_MODULE_sSandboxSimulation_CaptureAmazonClosed'                             => '',
+    'SHOP_MODULE_sSandboxSimulation_RefundAmazonRejected'                            => '',
+    'SHOP_MODULE_blShowAmazonPayButtonInBasketFlyout'                                => '',
+    'SHOP_MODULE_blBestitAmazonPay4OxidEnableMultiCurrency'                          => '',
+    'SHOP_MODULE_blShowAmazonPayButtonAtDetails'                                     => '',
+    'SHOP_MODULE_blShowAmazonPayButtonAtCartPopup'                                   => '',
+    'SHOP_MODULE_aAmazonReverseOrderCountries'                                       => '',
+
+    'SHOP_MODULE_GROUP_bestitAmazonPay4OxidLanguages'  => '',
+    'SHOP_MODULE_aAmazonLanguages'                      => "",
+
+    'tbclorder_bestitamazonpay'                          => '',
+    'BESTIT_AMAZON_SELECTED_PAYMENT_NOT_AMAZON'               => '',
+    'BESTIT_AMAZON_SELECT_ACTION'                             => '',
+    'BESTIT_AMAZONOPAYMENTSTATUS'                             => '',
+    'BESTIT_AMAZON_REFUNDS'                                   => '',
+    'BESTIT_AMAZON_REFUND_AMOUNT'                             => '',
+    'BESTIT_AMAZON_CONFIRM_REFUND'                            => '',
+    'BESTIT_AMAZON_PROCESS_REFUND'                            => '',
+    'BESTIT_AMAZONORDERREFERENCEID'                           => '',
+    'BESTIT_AMAZONAUTHORIZATIONID'                            => '',
+    'BESTIT_AMAZONCAPTUREID'                                  => '',
+    'BESTIT_AMAZONREFUNDID'                                   => '',
+    'BESTIT_AMAZON_REFUND_STATUS'                             => '',
+
+    'BESTIT_AMAZON_REFRESH_REFUND_STATUS'                     => '',
+    'BESTITAMAZONPAY_ORDER_NO'                           => '',
+    'BESTIT_AMAZON_SANDBOX_SIMULATION_ACTIONS'                => '',
+
+    'BESTITAMAZONPAY_PLEASE_CHECK_REFUND_CHECKBOX'       => '',
+    'BESTITAMAZONPAY_INVALID_REFUND_AMOUNT'              => '',
+
+    'BESTIT_AMAZON_QUICK_CONFIG' => ''
+);
