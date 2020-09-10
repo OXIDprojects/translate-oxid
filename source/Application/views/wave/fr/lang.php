cb203c78b23d2bdcda1a72d797cfca13a265d013
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 21:42:02 +0200

    New translations lang.php (French)

--- source/Application/views/wave/fr/lang.php
+++ source/Application/views/wave/fr/lang.php
@@ -25,103 +25,103 @@ $aLang = array(
     'charset'                                               => '',
 
     // Global
-    'DD_SORT_DESC'                                          => '',
-    'DD_SORT_ASC'                                           => '',
-    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_SORT_DESC'                                          => 'descendant',
+    'DD_SORT_ASC'                                           => 'Ascendant',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Ouvrir l\'interface d\'administration',
     'DD_DELETE'                                             => 'Supprimer',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
-    'DD_FORM_VALIDATION_NUMBER'                             => '',
-    'DD_FORM_VALIDATION_INTEGER'                            => '',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_REQUIRED'                           => '',
-    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'S\'il vous plaît, mettez une adresse email valide.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Les mots de passe ne correspondent pas.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'S\'il vous plaît entrer un nombre.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Il n\'y a pas de décimales autorisées.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Veuillez entrer un nombre positif',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'S\'il vous plaît entrer un nombre négatif.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Veuillez spécifier une valeur pour ce champ obligatoire.',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Veuillez sélectionner au moins une Option.',
 
     // Header
-    'SEARCH_TITLE'                                          => '',
-    'SEARCH_SUBMIT'                                         => 'Recherchér',
+    'SEARCH_TITLE'                                          => 'Entrez un terme de recherche ...',
+    'SEARCH_SUBMIT'                                         => 'Rechercher',
     'NAV_MORE'                                              => 'Plus',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => '',
+    'FOOTER_NEWSLETTER_INFO'                                => 'Informez-vous sur les derniers produits et offres par email.',
 
     // Home page
-    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
-    'START_BARGAIN_HEADER'                                  => '',
-    'START_NEWEST_HEADER'                                   => '',
-    'START_TOP_PRODUCTS_HEADER'                             => '',
-    'START_BARGAIN_SUBHEADER'                               => '',
-    'START_NEWEST_SUBHEADER'                                => '',
-    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
+    'MANUFACTURERSLIDER_SUBHEAD'                            => 'Nous présentons nos marques soigneusement sélectionnées, dont les produits peuvent être trouvés dans notre boutique.',
+    'START_BARGAIN_HEADER'                                  => 'Spécial de la semaine',
+    'START_NEWEST_HEADER'                                   => 'Vient d\'arriver',
+    'START_TOP_PRODUCTS_HEADER'                             => 'Meilleur vente',
+    'START_BARGAIN_SUBHEADER'                               => 'Économisez de l\'argent avec nos bonnes affaires actuelles!',
+    'START_NEWEST_SUBHEADER'                                => 'Frais comme il obtient. Une minute dans la boîte et maintenant déjà en magasin.',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Seulement 2 produits, mais le meilleur que nous pouvons vous offrir.',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => '',
+    'DD_CONTACT_PAGE_HEADING'                               => 'Contactez-nous!',
     'DD_CONTACT_FORM_HEADING'                               => 'Contact',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => '',
+    'DD_LINKS_NO_ENTRIES'                                   => 'Malheureusement, il n\'y a pas de liens disponibles.',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => '',
-    'DD_ERR_404_START_BUTTON'                               => '',
-    'DD_ERR_404_CONTACT_TEXT'                               => '',
-    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+    'DD_ERR_404_START_TEXT'                                 => 'Vous pouvez trouver les informations demandées sur notre page d\'accueil:',
+    'DD_ERR_404_START_BUTTON'                               => 'Aller à la page d\'accueil',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Pouvons-nous vous aider?<br>N\'hésitez pas à nous appeler ou à nous écrire:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'à la page de contact',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'En créant un compte avec notre magasin, vous serez guidé plus rapidement dans le processus de commande et vous pourrez stocker plusieurs adresses de livraison et suivre les commandes dans votre compte.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'Etat',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'Sélectionner',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'ajouter une adresse de livraison',
     'DD_DELETE_SHIPPING_ADDRESS'                            => '',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
-    'DD_LIST_SHOW_MORE'                                     => '',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filtre:',
+    'DD_LIST_SHOW_MORE'                                     => 'Voir les produits...',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'Retour à la vue d\'ensemble',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
-    'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'Télécharger',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Taille du fichier:',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'Heure',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Nom de fichier:',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => '',
+    'BACK_TO_OVERVIEW'                                      => 'Retour à l\'aperçu',
     'OF'                                                    => 'sur',
-    'DD_RATING_CUSTOMERRATINGS'                             => '',
-    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
-    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Avis des clients',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Lies clients ayant commandé ce produits ont également été séduits par:',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'Les produits suivants conviennent bien à ce produit.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Jetez un œil à nos produits similaires.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Les clients ayant consulté ce produit ont également consulté les produits suivants.',
+    'DETAILS_VPE_MESSAGE_1'                                 => "Ce produit ne peut être commandé que dans des unités ou en emballage",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
     'TO_CART_NOVARIANT'                                     => '',
 
     // Modal basket
     'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Total',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'Continuer vos achats',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => '',
+    'DD_BASKET_BACK_TO_SHOP'                                => 'Retour à la Boutique',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => '',
-    'DD_FORGOT_PASSWORD_HEADING'                            => '',
-    'DD_INVITE_HEADING'                                     => '',
+    'DD_FOOTER_FOLLOW_US'                                   => 'Suivez nous:',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Mot de passe oublié',
+    'DD_INVITE_HEADING'                                     => 'Recommandation d\'article',
     'DD_INVITE_LINK'                                        => 'Lien',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
-    'DD_ORDERSHIPPED_HEADING'                               => '',
-    'DD_PRICEALARM_HEADING'                                 => 'Prix client',
-    'DD_REGISTER_HEADING'                                   => '',
-    'DD_DOWNLOADLINKS_HEADING'                              => '',
-    'DD_WISHLIST_HEADING'                                   => '',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Votre abonnement à la newsletter',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Confirmation de livraison - Commande',
+    'DD_PRICEALARM_HEADING'                                 => 'Alarme de Prix',
+    'DD_REGISTER_HEADING'                                   => 'Votre inscription',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Vos liens de téléchargement - commandes',
+    'DD_WISHLIST_HEADING'                                   => 'Liste de souhaits',
 
     'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menu',
 
