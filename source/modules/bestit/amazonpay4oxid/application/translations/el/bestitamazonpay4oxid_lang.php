67459d90e3748c0a0c00e72eabedc9f82e417fd8
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 20:37:09 +0200

    New translations bestitamazonpay4oxid_lang.php (Greek)

--- source/modules/bestit/amazonpay4oxid/application/translations/el/bestitamazonpay4oxid_lang.php
+++ source/modules/bestit/amazonpay4oxid/application/translations/el/bestitamazonpay4oxid_lang.php
@@ -6,7 +6,7 @@
  * @author best it GmbH & Co. KG <info@bestit-online.de>
  */
 
-$sLangName  = "English";
+$sLangName  = "Ελληνικά";
 
 $aLang = array(
     'charset'                                                    => '',
