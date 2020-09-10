04e1df1800006fdcdd57d1214a04bbef00f82686
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 21:42:33 +0200

    New translations lang.php (Spanish)

--- source/Application/views/wave/es/lang.php
+++ source/Application/views/wave/es/lang.php
@@ -19,34 +19,34 @@
  * @copyright (C) OXID eSales AG 2003-2016
  */
 
-$sLangName = "Español";
+$sLangName = "English";
 
 $aLang = array(
     'charset'                                               => '',
 
     // Global
-    'DD_SORT_DESC'                                          => '',
-    'DD_SORT_ASC'                                           => '',
-    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_SORT_DESC'                                          => 'descendente',
+    'DD_SORT_ASC'                                           => 'ascendiente',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Abrir en modo interfaz',
     'DD_DELETE'                                             => 'Eliminar',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
-    'DD_FORM_VALIDATION_NUMBER'                             => '',
-    'DD_FORM_VALIDATION_INTEGER'                            => '',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_REQUIRED'                           => '',
-    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Por favor, introduce una dirección de correo electrónico válida.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Las contraseñas no coinciden.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Por favor, introduzca un número.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'No hay espacio permitido para decimales.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Por favor introduce un número positivo.',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Por favor ingrese un número negativo.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Por favor, especifique un valor para este campo obligatorio.',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Por favor seleccione al menos una opción.',
 
     // Header
-    'SEARCH_TITLE'                                          => '',
-    'SEARCH_SUBMIT'                                         => 'Buscar',
+    'SEARCH_TITLE'                                          => 'Ingrese un término de búsqueda ...',
+    'SEARCH_SUBMIT'                                         => 'Busqueda',
     'NAV_MORE'                                              => 'Más',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => '',
+    'FOOTER_NEWSLETTER_INFO'                                => 'Infórmese sobre los últimos productos y ofertas por correo electrónico.',
 
     // Home page
     'MANUFACTURERSLIDER_SUBHEAD'                            => '',
@@ -58,70 +58,70 @@ $aLang = array(
     'START_TOP_PRODUCTS_SUBHEADER'                          => '',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => '',
-    'DD_CONTACT_FORM_HEADING'                               => 'Contacto',
+    'DD_CONTACT_PAGE_HEADING'                               => 'Contacta con nosotros!',
+    'DD_CONTACT_FORM_HEADING'                               => 'Contactar',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => '',
+    'DD_LINKS_NO_ENTRIES'                                   => 'Lamentablemente, no hay enlaces disponibles.',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => '',
-    'DD_ERR_404_START_BUTTON'                               => '',
-    'DD_ERR_404_CONTACT_TEXT'                               => '',
-    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+    'DD_ERR_404_START_TEXT'                                 => 'Tú puedes encontrar la información solicitada en nuestra página de inicio:',
+    'DD_ERR_404_START_BUTTON'                               => 'Ir a la página de inicio',
+    'DD_ERR_404_CONTACT_TEXT'                               => '¿Podemos ayudarte? <br>No dude en llamarnos o escribir un correo electrónico:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'A la página de contacto',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'Al crear una cuenta en nuestra tienda, se le guiará a través del proceso de pago más rápido. Además, puede almacenar múltiples direcciones de envío y supervisar pedidos en su cuenta.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'Estado',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'seleccionar',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'añadir una dirección de entrega',
     'DD_DELETE_SHIPPING_ADDRESS'                            => '',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
-    'DD_LIST_SHOW_MORE'                                     => '',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filtro:',
+    'DD_LIST_SHOW_MORE'                                     => 'Ver productos...',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'Atrás a visión general',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'descargar',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Tamaño del archivo:',
     'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Nombre del Documento:',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => '',
+    'BACK_TO_OVERVIEW'                                      => 'volver a la vista general',
     'OF'                                                    => 'de',
-    'DD_RATING_CUSTOMERRATINGS'                             => '',
-    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Valoración de los clientes',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Los clientes que compraron este artículo también compraron uno de los siguientes productos.',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
-    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'DETAILS_VPE_MESSAGE_1'                                 => "Este producto solo se puede pedir en unidades de embalaje de",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
     'TO_CART_NOVARIANT'                                     => '',
 
     // Modal basket
     'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Total',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'continuar comprando',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => '',
+    'DD_BASKET_BACK_TO_SHOP'                                => 'Volver a la Tienda',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => '',
-    'DD_FORGOT_PASSWORD_HEADING'                            => '',
-    'DD_INVITE_HEADING'                                     => '',
+    'DD_FOOTER_FOLLOW_US'                                   => 'Síguenos:',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Se te olvidó tu contraseña',
+    'DD_INVITE_HEADING'                                     => 'Recomendación del artículo',
     'DD_INVITE_LINK'                                        => 'Enlace',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
-    'DD_ORDERSHIPPED_HEADING'                               => '',
-    'DD_PRICEALARM_HEADING'                                 => 'Precio fijado por el cliente',
-    'DD_REGISTER_HEADING'                                   => '',
-    'DD_DOWNLOADLINKS_HEADING'                              => '',
-    'DD_WISHLIST_HEADING'                                   => '',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Tu suscripción al boletín informativo',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Confirmación de entrega - Pedido',
+    'DD_PRICEALARM_HEADING'                                 => 'Alarma de precio',
+    'DD_REGISTER_HEADING'                                   => 'Tu registro',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Tus enlaces de descarga - pedidos',
+    'DD_WISHLIST_HEADING'                                   => 'Lista de deseos',
 
     'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menú',
 
@@ -140,7 +140,7 @@ $aLang = array(
     // Contact page
     'DD_SELECT_SALUTATION'                                  => 'Por favor escoge',
 
-    'DD_CATEGORY_RESET_BUTTON'                              => 'Resetear',
+    'DD_CATEGORY_RESET_BUTTON'                              => 'Reset',
 );
 
 /*
