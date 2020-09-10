d3aa92c830fa7c759db9eed373b9e40ad4087b36
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 22:41:41 +0200

    New translations lang.php (Slovak)

--- source/Application/views/wave/sk/lang.php
+++ source/Application/views/wave/sk/lang.php
@@ -25,20 +25,20 @@ $aLang = array(
     'charset'                                               => '',
 
     // Global
-    'DD_SORT_DESC'                                          => '',
-    'DD_SORT_ASC'                                           => '',
-    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_SORT_DESC'                                          => 'zostupne',
+    'DD_SORT_ASC'                                           => 'vzostupne',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Otvoriť administrátorské rozhranie',
     'DD_DELETE'                                             => 'Vymazať',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
-    'DD_FORM_VALIDATION_NUMBER'                             => '',
-    'DD_FORM_VALIDATION_INTEGER'                            => '',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_REQUIRED'                           => '',
-    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Zadajte prosím platnú e-mailovou adresu.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Heslá sa nezhodujú.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Zadajte číslo.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Nie sú povolené desatinné miesta.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Zadajte kladné číslo.',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Zadajte záporné číslo.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Zadajte prosím hodnotu.',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Zvolte prosím aspoň jednu voľbu.',
 
     // Header
     'SEARCH_TITLE'                                          => '',
@@ -46,7 +46,7 @@ $aLang = array(
     'NAV_MORE'                                              => 'Viac',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => '',
+    'FOOTER_NEWSLETTER_INFO'                                => 'Buďte informovaný o nových produktoch a najlepších ponukách pomocou e-mailu!',
 
     // Home page
     'MANUFACTURERSLIDER_SUBHEAD'                            => '',
@@ -58,72 +58,72 @@ $aLang = array(
     'START_TOP_PRODUCTS_SUBHEADER'                          => '',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => '',
+    'DD_CONTACT_PAGE_HEADING'                               => 'Kontaktujte nás!',
     'DD_CONTACT_FORM_HEADING'                               => 'Kontakt',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => '',
+    'DD_LINKS_NO_ENTRIES'                                   => 'Bohužiaľ nie sú k dispozícii žiadne odkazy/linky.',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => '',
-    'DD_ERR_404_START_BUTTON'                               => '',
-    'DD_ERR_404_CONTACT_TEXT'                               => '',
-    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+    'DD_ERR_404_START_TEXT'                                 => 'Požadované informácie môžete nájsť na našej domovskej stránke:',
+    'DD_ERR_404_START_BUTTON'                               => 'Prejsť na domovskú stránku',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Môžeme Vám nejako pomôcť?<br>Bez váhania nám zavolajte, alebo napíšte e-mail:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'Ísť na kontaktnú stránku',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'Vytvorením účtu v našom obchode, budete vedený cez proces objednávky rýchlejší. Okrem toho si môžete uložiť viac dodacích adries a sledovať objednávky v účte.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'Štát',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'Vybrať',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'pridať ďalšiu adresu',
     'DD_DELETE_SHIPPING_ADDRESS'                            => '',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
-    'DD_LIST_SHOW_MORE'                                     => '',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filter:',
+    'DD_LIST_SHOW_MORE'                                     => 'Zobraziť produkty...',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'späť na prehľad',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
-    'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'Stiahnuť',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Veľkosť súboru:',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'hodiny',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Názov súboru:',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => '',
+    'BACK_TO_OVERVIEW'                                      => 'na prehľad',
     'OF'                                                    => 'OF',
-    'DD_RATING_CUSTOMERRATINGS'                             => '',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Názory zákazníkov',
     'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
-    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'DETAILS_VPE_MESSAGE_1'                                 => "Tento produkt je možné objednať len v balení po",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
     'TO_CART_NOVARIANT'                                     => '',
 
     // Modal basket
-    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Spolu',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Celkový súčet',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'Pokračovať v nákupe',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => '',
+    'DD_BASKET_BACK_TO_SHOP'                                => 'Späť do obchodu',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => '',
-    'DD_FORGOT_PASSWORD_HEADING'                            => '',
-    'DD_INVITE_HEADING'                                     => '',
+    'DD_FOOTER_FOLLOW_US'                                   => 'Sleduj nás:',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Zabudnuté heslo',
+    'DD_INVITE_HEADING'                                     => 'Doporučený produkt',
     'DD_INVITE_LINK'                                        => 'Odkaz',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
-    'DD_ORDERSHIPPED_HEADING'                               => '',
-    'DD_PRICEALARM_HEADING'                                 => 'Zákaznícka cena',
-    'DD_REGISTER_HEADING'                                   => '',
-    'DD_DOWNLOADLINKS_HEADING'                              => '',
-    'DD_WISHLIST_HEADING'                                   => '',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Prihlásenie k odberu noviniek',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Potvrdenie odoslania - objednávka',
+    'DD_PRICEALARM_HEADING'                                 => 'Cenový alarm',
+    'DD_REGISTER_HEADING'                                   => 'Vaša registrácia',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Vaše linky k stiahnutiu objednávok',
+    'DD_WISHLIST_HEADING'                                   => 'Zoznam prianí',
 
-    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menu',
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Ponuka',
 
     'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
     'DD_DELETE_MY_ACCOUNT'                                  => '',
