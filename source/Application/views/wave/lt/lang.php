dd7e2289a6f73e567695e312b42ca6eb16b6d69c
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 21:46:04 +0200

    New translations lang.php (Lithuanian)

--- source/Application/views/wave/lt/lang.php
+++ source/Application/views/wave/lt/lang.php
@@ -27,18 +27,18 @@ $aLang = array(
     // Global
     'DD_SORT_DESC'                                          => '',
     'DD_SORT_ASC'                                           => '',
-    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Atidarykite administratoriaus sąsaja',
     'DD_DELETE'                                             => 'Ištrinti',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
-    'DD_FORM_VALIDATION_NUMBER'                             => '',
-    'DD_FORM_VALIDATION_INTEGER'                            => '',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_REQUIRED'                           => '',
-    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Prašome įvesti galiojantį el. pašto adresą.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Slaptažodžiai nesutampa.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Įveskite skaičių.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Nėra skaičių po kablelio.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Įveskite teigiamą skaičių.',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Įveskite neigiamą skaičių.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Nurodykite šio būtino lauko reikšmę.',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Pasirinkite bent vieną parinktį.',
 
     // Header
     'SEARCH_TITLE'                                          => '',
@@ -58,72 +58,72 @@ $aLang = array(
     'START_TOP_PRODUCTS_SUBHEADER'                          => '',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => '',
-    'DD_CONTACT_FORM_HEADING'                               => 'Kontaktas',
+    'DD_CONTACT_PAGE_HEADING'                               => 'Kreipkitės į mus!',
+    'DD_CONTACT_FORM_HEADING'                               => 'kontaktas',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => '',
+    'DD_LINKS_NO_ENTRIES'                                   => 'Deja, nėra jokių galimų nuorodų.',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => '',
-    'DD_ERR_404_START_BUTTON'                               => '',
-    'DD_ERR_404_CONTACT_TEXT'                               => '',
-    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+    'DD_ERR_404_START_TEXT'                                 => 'Tu gali rasti reikiamą informaciją iš savo pradinio puslapio:',
+    'DD_ERR_404_START_BUTTON'                               => 'Eiti į pagrindinį puslapį',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Galim jums padėti?<br>Paskambinkite mums arba parašykite el. laišką:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'į puslapį',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'Kurdami paskyrą mūsų parduotuvėje, bus greitesnis procesas. Be to, galite išsaugoti kelis pristatymo adresus ir stebėti užsakymus.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'Valstybė',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'Pasirinkti',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'pridėti pristatymo adresas',
     'DD_DELETE_SHIPPING_ADDRESS'                            => '',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
-    'DD_LIST_SHOW_MORE'                                     => '',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filtras:',
+    'DD_LIST_SHOW_MORE'                                     => 'Peržiūrėti produktus...',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'atgal į apžvalga',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'atsisiuntimas',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Failo dydis:',
     'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Failo pavadinimas:',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => '',
+    'BACK_TO_OVERVIEW'                                      => 'į apžvalgą',
     'OF'                                                    => '',
-    'DD_RATING_CUSTOMERRATINGS'                             => '',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Klientų atsiliepimai',
     'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
     'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
-    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'DETAILS_VPE_MESSAGE_1'                                 => "This product can only be ordered in packaging units of",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
     'TO_CART_NOVARIANT'                                     => '',
 
     // Modal basket
     'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Viso',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'tęsti apsipirkimą',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => '',
+    'DD_BASKET_BACK_TO_SHOP'                                => 'grįžti į parduotuvę',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => '',
-    'DD_FORGOT_PASSWORD_HEADING'                            => '',
-    'DD_INVITE_HEADING'                                     => '',
-    'DD_INVITE_LINK'                                        => 'Nuorodos',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
-    'DD_ORDERSHIPPED_HEADING'                               => '',
-    'DD_PRICEALARM_HEADING'                                 => 'Kliento kaina',
-    'DD_REGISTER_HEADING'                                   => '',
-    'DD_DOWNLOADLINKS_HEADING'                              => '',
-    'DD_WISHLIST_HEADING'                                   => '',
+    'DD_FOOTER_FOLLOW_US'                                   => 'Sekite mus:',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Pamiršau slaptažodį',
+    'DD_INVITE_HEADING'                                     => 'Straipsnis rekomendacijos',
+    'DD_INVITE_LINK'                                        => 'Nuoroda',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Jūsų informacinio biuletenio prenumerata',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Pristatymo patvirtinimas - užsakymas',
+    'DD_PRICEALARM_HEADING'                                 => 'Kainų signalas',
+    'DD_REGISTER_HEADING'                                   => 'Jūsų registracija',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Jūsų atsisiuntimo nuorodos - užsakymai',
+    'DD_WISHLIST_HEADING'                                   => 'Pageidaujamų sąrašas',
 
-    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => '',
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Meniu',
 
     'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
     'DD_DELETE_MY_ACCOUNT'                                  => '',
