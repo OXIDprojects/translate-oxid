fdd11dadb91b912f6d439dae62bacb8d0f98d25a
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-10 22:42:56 +0200

    New translations lang.php (Ukrainian)

--- source/Application/views/wave/uk/lang.php
+++ source/Application/views/wave/uk/lang.php
@@ -25,103 +25,103 @@ $aLang = array(
     'charset'                                               => 'UTF-8',
 
     // Global
-    'DD_SORT_DESC'                                          => '',
-    'DD_SORT_ASC'                                           => '',
-    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_SORT_DESC'                                          => 'низхідний',
+    'DD_SORT_ASC'                                           => 'висхідний',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Відкрити панель адміністратора',
     'DD_DELETE'                                             => 'Видалити',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
-    'DD_FORM_VALIDATION_NUMBER'                             => '',
-    'DD_FORM_VALIDATION_INTEGER'                            => '',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
-    'DD_FORM_VALIDATION_REQUIRED'                           => '',
-    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Будь ласка, введіть дійсну адресу електронної пошти.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Паролі не співпадають.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Вкажіть число.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Не дозволяється розміщення десяткових знаків.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Введіть додатне число',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Вкажіть від\'ємну величину.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Заповніть обов\'язкове поле',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Виберіть варіант',
 
     // Header
-    'SEARCH_TITLE'                                          => '',
+    'SEARCH_TITLE'                                          => 'пошук...',
     'SEARCH_SUBMIT'                                         => 'Пошук',
     'NAV_MORE'                                              => 'Більше',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => '',
+    'FOOTER_NEWSLETTER_INFO'                                => 'Отримайте інформацію про найновіші продукти та пропозиції на електронну пошту.',
 
     // Home page
-    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
-    'START_BARGAIN_HEADER'                                  => '',
-    'START_NEWEST_HEADER'                                   => '',
-    'START_TOP_PRODUCTS_HEADER'                             => '',
-    'START_BARGAIN_SUBHEADER'                               => '',
-    'START_NEWEST_SUBHEADER'                                => '',
-    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
+    'MANUFACTURERSLIDER_SUBHEAD'                            => 'Бренди, представлені в каталозі товарів',
+    'START_BARGAIN_HEADER'                                  => 'Товари тижня',
+    'START_NEWEST_HEADER'                                   => 'Новинки',
+    'START_TOP_PRODUCTS_HEADER'                             => 'Лідери продажу',
+    'START_BARGAIN_SUBHEADER'                               => 'Устигніть заощадити!',
+    'START_NEWEST_SUBHEADER'                                => 'Свіжі надходження',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Тільки 2 товари, але це наша краща пропозиція.',
 
     // Contact form
-    'DD_CONTACT_PAGE_HEADING'                               => '',
-    'DD_CONTACT_FORM_HEADING'                               => 'Контакт',
+    'DD_CONTACT_PAGE_HEADING'                               => 'Зворотній зв\'язок',
+    'DD_CONTACT_FORM_HEADING'                               => 'Контакти',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => '',
+    'DD_LINKS_NO_ENTRIES'                                   => 'На жаль, немає доступних посилань.',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => '',
-    'DD_ERR_404_START_BUTTON'                               => '',
-    'DD_ERR_404_CONTACT_TEXT'                               => '',
-    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+    'DD_ERR_404_START_TEXT'                                 => 'Ви можете знайти потрібну інформацію на нашій домашній сторінці:',
+    'DD_ERR_404_START_BUTTON'                               => 'Перейти на головну',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Чи можемо ми допомогти вам?<br>Не соромтеся зателефонувати нам або написати електронний лист:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'зв\'язатися з нами',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'Створивши обліковий запис у нашому магазині, ви будете швидше керуватися процесом оформлення замовлення. Крім того, ви можете зберігати кілька адрес доставки та відстежувати замовлення в обліковому записі.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'Штат',
-    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'виберіть',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'додати адресу доставки',
     'DD_DELETE_SHIPPING_ADDRESS'                            => '',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
-    'DD_LIST_SHOW_MORE'                                     => '',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Фільтр:',
+    'DD_LIST_SHOW_MORE'                                     => 'Переглянути продукти ...',
 
     // Recommendation list
-    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'повернутися до списку',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
-    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
-    'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
-    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'завантажити',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Розмір файлу:',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'годин',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Ім\'я файлу:',
 
     // Details page
-    'BACK_TO_OVERVIEW'                                      => '',
-    'OF'                                                    => 'від',
-    'DD_RATING_CUSTOMERRATINGS'                             => '',
-    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
-    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'BACK_TO_OVERVIEW'                                      => 'до меню',
+    'OF'                                                    => 'з',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Відгуки покупців',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Клієнти, які купили цей товар, також придбали:',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'До цього товару добре підійде',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Погляньте на схожі товари',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Клієнти, що переглядали цей товар також цікавились',
+    'DETAILS_VPE_MESSAGE_1'                                 => "Цей продукт можна замовити тільки в упаковці",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
     'TO_CART_NOVARIANT'                                     => '',
 
     // Modal basket
-    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Разом',
-    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Всього',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'продовжити покупки',
 
     // Checkout
-    'DD_BASKET_BACK_TO_SHOP'                                => '',
+    'DD_BASKET_BACK_TO_SHOP'                                => 'повернутися до магазину',
 
     // E-Mails
-    'DD_FOOTER_FOLLOW_US'                                   => '',
-    'DD_FORGOT_PASSWORD_HEADING'                            => '',
-    'DD_INVITE_HEADING'                                     => '',
+    'DD_FOOTER_FOLLOW_US'                                   => 'Приєднуйтемь:',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Забули пароль?',
+    'DD_INVITE_HEADING'                                     => 'Рекомендувати товар',
     'DD_INVITE_LINK'                                        => 'Посилання',
-    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
-    'DD_ORDERSHIPPED_HEADING'                               => '',
-    'DD_PRICEALARM_HEADING'                                 => 'Ціна клєнта',
-    'DD_REGISTER_HEADING'                                   => '',
-    'DD_DOWNLOADLINKS_HEADING'                              => '',
-    'DD_WISHLIST_HEADING'                                   => '',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Підписка на розсилку новин',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Підтвердження доставки - замовлення',
+    'DD_PRICEALARM_HEADING'                                 => 'Повідомлення про знижку',
+    'DD_REGISTER_HEADING'                                   => 'Реєстрація',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Ваші посилання для скачування - замовлення',
+    'DD_WISHLIST_HEADING'                                   => 'Список бажань',
 
     'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Меню',
 
