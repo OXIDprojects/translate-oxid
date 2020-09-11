155954f9ff99279e7a84391dbe842d7a4a9668ab
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 14:03:45 +0200

    New translations fcPayOne_lang.php (Slovak)

--- source/modules/fc/fcpayone/application/translations/sk/fcPayOne_lang.php
+++ source/modules/fc/fcpayone/application/translations/sk/fcPayOne_lang.php
@@ -18,12 +18,12 @@
  * @version   OXID eShop CE
  */
  
-$sLangName  = "";
+$sLangName  = "Slovenčina";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-'charset'                                       => '',
+'charset'                                       => 'UTF-8',
 'FCPO_IBAN_INVALID'                             => '',
 'FCPO_BIC_INVALID'                              => '',
 'FCPO_BLZ_INVALID'                              => '',
