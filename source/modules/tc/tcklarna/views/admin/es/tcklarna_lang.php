dd10e421727bef2f35ad0d0ac0b114efbfce2cf1
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 12:57:12 +0200

    New translations tcklarna_lang.php (Spanish, Mexico)

--- source/modules/tc/tcklarna/views/admin/es/tcklarna_lang.php
+++ source/modules/tc/tcklarna/views/admin/es/tcklarna_lang.php
@@ -15,12 +15,12 @@
  * limitations under the License.
  */
 
-$sLangName = "Español";
+$sLangName = "Español de América";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-    'charset' => '',
+    'charset' => 'UTF-8',
 
     'TCKLARNA_KLARNAMAINMENU'      => '',
     'TCKLARNA_START'               => '',
