da43aaabe2f6873cade0fef5d43150c131927c2a
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:27:22 +0200

    New translations oepaypal_lang.php (Danish)

--- source/modules/oe/oepaypal/views/admin/da/oepaypal_lang.php
+++ source/modules/oe/oepaypal/views/admin/da/oepaypal_lang.php
@@ -25,7 +25,7 @@ $sLangName = 'Dansk';
 // RESOURCE IDENTIFIER = STRING
 // -------------------------------
 $aLang = array(
-    'charset'                              => '',
+    'charset'                              => 'UTF-8',
     'tbclorder_paypal'                     => '',
     'OEPAYPAL_MOBILE_DEFAULT_PAYMENT'      => '',
     'OEPAYPAL_AMOUNT'                      => 'Mængde',
