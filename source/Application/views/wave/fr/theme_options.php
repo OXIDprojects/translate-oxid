9f586624708fbb24a0621bca127e19aa1447e7ab
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:26:34 +0200

    New translations theme_options.php (French)

--- source/Application/views/wave/fr/theme_options.php
+++ source/Application/views/wave/fr/theme_options.php
@@ -22,7 +22,7 @@
 $sLangName  = 'Français';
 
 $aLang = array(
-    'charset'                                       => '',
+    'charset'                                       => 'UTF-8',
 
     'SHOP_THEME_GROUP_images'                       => 'Images',
     'SHOP_THEME_GROUP_features'                     => 'Caractéristiques',
