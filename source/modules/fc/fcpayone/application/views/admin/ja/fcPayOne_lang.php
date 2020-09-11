b3917678cd0b51a64118fcab1c1285d9949fe499
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 13:02:22 +0200

    New translations fcPayOne_lang.php (Japanese)

--- source/modules/fc/fcpayone/application/views/admin/ja/fcPayOne_lang.php
+++ source/modules/fc/fcpayone/application/views/admin/ja/fcPayOne_lang.php
@@ -19,12 +19,12 @@
  */
  
 
-$sLangName  = "English";
+$sLangName  = "日本語";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-'charset'                                   => '',
+'charset'                                   => 'UTF-8',
 'fcpo_admin_title'                          => '',
 'fcpo_main_title'                           => '',
 'fcpo_main_log'                             => '',
@@ -400,7 +400,7 @@ $aLang = array(
 'FCPO_CC_SELECT'                            => 'セレクション',
     
 'FCPO_CC_ERRORLANG_DE'                      => '',
-'FCPO_CC_ERRORLANG_EN'                      => 'English',
+'FCPO_CC_ERRORLANG_EN'                      => '日本語',
 'FCPO_CC_CUSTOM_TEMPLATE'                   => '',
 'FCPO_CC_PREVIEW'                           => 'プレビュー',
     
