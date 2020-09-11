15dcc13b4c73f3ddeef6ff30b449ed3a0b2a3f12
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 14:09:08 +0200

    New translations bestitamazonpay4oxid_lang.php (English, United Kingdom)

--- source/modules/bestit/amazonpay4oxid/application/views/admin/en/bestitamazonpay4oxid_lang.php
+++ source/modules/bestit/amazonpay4oxid/application/views/admin/en/bestitamazonpay4oxid_lang.php
@@ -6,7 +6,7 @@
  * @author best it GmbH & Co. KG <info@bestit-online.de>
  */
 
-$sLangName  = "English (United States)";
+$sLangName  = "English (United Kingdom)";
 
 $aLang = array(
     'charset'                                               => 'UTF-8',
