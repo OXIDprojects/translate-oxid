9c74bbebd9f198400ce673a6375d4d601559ac40
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 12:58:45 +0200

    New translations ddoewysiwyg_lang.php (Japanese)

--- source/modules/ddoe/wysiwyg/Application/views/admin/ja/ddoewysiwyg_lang.php
+++ source/modules/ddoe/wysiwyg/Application/views/admin/ja/ddoewysiwyg_lang.php
@@ -20,11 +20,11 @@
  * @version   OXID eSales WYSIWYG
  */
 
-$sLangName = 'English';
+$sLangName = '日本語';
 
 $aLang = array(
     // Charsets
-    'charset' => '',
+    'charset' => 'UTF-8',
     // 'charset' => 'ISO-8859-15',
 
     // Lang-Strings
