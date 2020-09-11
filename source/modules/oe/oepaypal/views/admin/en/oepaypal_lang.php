21031d67c0d95fe0c4e0b2adb2c4f80a3ec7a65b
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 20:29:47 +0200

    New translations oepaypal_lang.php (English, Australia)

--- source/modules/oe/oepaypal/views/admin/en/oepaypal_lang.php
+++ source/modules/oe/oepaypal/views/admin/en/oepaypal_lang.php
@@ -19,7 +19,7 @@
  * @copyright (C) OXID eSales AG 2003-2018
  */
 
-$sLangName = 'English (United States)';
+$sLangName = 'English (Australia)';
 
 // -------------------------------
 // RESOURCE IDENTIFIER = STRING
@@ -60,7 +60,7 @@ $aLang = array(
     'OEPAYPAL_STATUS_pending'              => '',
     'OEPAYPAL_STATUS_completed'            => '',
     'OEPAYPAL_STATUS_failed'               => '',
-    'OEPAYPAL_STATUS_canceled'             => 'Canceled',
+    'OEPAYPAL_STATUS_canceled'             => 'Cancelled',
     'OEPAYPAL_ONLY_FOR_PAYPAL_PAYMENT'     => '',
     'OEPAYPAL_ONLY_FOR_NEW_PAYPAL_PAYMENT' => '',
     'OEPAYPAL_PAYMENT_METHOD'              => 'Payment Method',
