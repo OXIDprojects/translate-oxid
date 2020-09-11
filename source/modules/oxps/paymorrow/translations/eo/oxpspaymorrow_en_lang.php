16a8268055c02a9e9b3c477b85cb52807dfd6ebd
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:29:18 +0200

    New translations oxpspaymorrow_en_lang.php (Esperanto)

--- source/modules/oxps/paymorrow/translations/eo/oxpspaymorrow_en_lang.php
+++ source/modules/oxps/paymorrow/translations/eo/oxpspaymorrow_en_lang.php
@@ -33,7 +33,7 @@
 $sLangName = "Esperanto";
 
 $aLang = array(
-    "charset"                                        => "",
+    "charset"                                        => "UTF-8",
 
     'PAYMORROW_PAYMENT_METHOD_NAME_INVOICE'          => '',
     'PAYMORROW_PAYMENT_METHOD_NAME_DIRECT_DEBIT'     => '',
