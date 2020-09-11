9b83bb97b6568f1c5278097a263750e8c84d92c5
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:25:49 +0200

    New translations lang.php (Bulgarian)

--- source/Application/views/wave/bg/lang.php
+++ source/Application/views/wave/bg/lang.php
@@ -22,7 +22,7 @@
 $sLangName = "Български";
 
 $aLang = array(
-    'charset'                                               => '',
+    'charset'                                               => 'UTF-8',
 
     // Global
     'DD_SORT_DESC'                                          => 'низходящ',
