b18ce6218ba831d601ec79e87af50ed6fee236ca
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 14:05:54 +0200

    New translations tcklarna_lang.php (Vietnamese)

--- source/modules/tc/tcklarna/views/admin/vi/tcklarna_lang.php
+++ source/modules/tc/tcklarna/views/admin/vi/tcklarna_lang.php
@@ -15,12 +15,12 @@
  * limitations under the License.
  */
 
-$sLangName = "English";
+$sLangName = "Tiếng Việt";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-    'charset' => '',
+    'charset' => 'UTF-8',
 
     'TCKLARNA_KLARNAMAINMENU'      => '',
     'TCKLARNA_START'               => '',
