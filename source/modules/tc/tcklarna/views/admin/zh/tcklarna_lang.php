b5403bde68388a19081907c271fb999f8b912bfb
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:30:42 +0200

    New translations tcklarna_lang.php (Chinese Simplified)

--- source/modules/tc/tcklarna/views/admin/zh/tcklarna_lang.php
+++ source/modules/tc/tcklarna/views/admin/zh/tcklarna_lang.php
@@ -20,7 +20,7 @@ $sLangName = "中文";
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-    'charset' => '',
+    'charset' => 'UTF-8',
 
     'TCKLARNA_KLARNAMAINMENU'      => '',
     'TCKLARNA_START'               => '',
