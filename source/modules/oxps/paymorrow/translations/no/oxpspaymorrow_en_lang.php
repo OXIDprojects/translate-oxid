4fc0d0227a8890f0f2c00a3f29edc11788ab3b78
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 13:01:06 +0200

    New translations oxpspaymorrow_en_lang.php (Norwegian)

--- source/modules/oxps/paymorrow/translations/no/oxpspaymorrow_en_lang.php
+++ source/modules/oxps/paymorrow/translations/no/oxpspaymorrow_en_lang.php
@@ -30,10 +30,10 @@
  * the OXID module for Paymorrow payment. If not, see <http://www.gnu.org/licenses/>.
  */
 
-$sLangName = "English";
+$sLangName = "Norsk Bokmål";
 
 $aLang = array(
-    "charset"                                        => "",
+    "charset"                                        => "UTF-8",
 
     'PAYMORROW_PAYMENT_METHOD_NAME_INVOICE'          => '',
     'PAYMORROW_PAYMENT_METHOD_NAME_DIRECT_DEBIT'     => '',
