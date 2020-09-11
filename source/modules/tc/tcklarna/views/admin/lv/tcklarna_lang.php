1af9613b485250eda5e74f9d33a12d11b08d8397
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 12:56:11 +0200

    New translations tcklarna_lang.php (Latvian)

--- source/modules/tc/tcklarna/views/admin/lv/tcklarna_lang.php
+++ source/modules/tc/tcklarna/views/admin/lv/tcklarna_lang.php
@@ -15,12 +15,12 @@
  * limitations under the License.
  */
 
-$sLangName = "English";
+$sLangName = "Latviešu";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-    'charset' => '',
+    'charset' => 'UTF-8',
 
     'TCKLARNA_KLARNAMAINMENU'      => '',
     'TCKLARNA_START'               => '',
