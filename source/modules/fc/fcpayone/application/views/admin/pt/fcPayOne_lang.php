1c2b2a3fd92f8785ff043c6f8a1cdc820608e7bd
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 12:56:44 +0200

    New translations fcPayOne_lang.php (Portuguese, Brazilian)

--- source/modules/fc/fcpayone/application/views/admin/pt/fcPayOne_lang.php
+++ source/modules/fc/fcpayone/application/views/admin/pt/fcPayOne_lang.php
@@ -19,12 +19,12 @@
  */
  
 
-$sLangName  = "English";
+$sLangName  = "Português do Brasil";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-'charset'                                   => '',
+'charset'                                   => 'UTF-8',
 'fcpo_admin_title'                          => '',
 'fcpo_main_title'                           => '',
 'fcpo_main_log'                             => '',
@@ -400,7 +400,7 @@ $aLang = array(
 'FCPO_CC_SELECT'                            => '',
     
 'FCPO_CC_ERRORLANG_DE'                      => '',
-'FCPO_CC_ERRORLANG_EN'                      => 'English',
+'FCPO_CC_ERRORLANG_EN'                      => 'Português do Brasil',
 'FCPO_CC_CUSTOM_TEMPLATE'                   => '',
 'FCPO_CC_PREVIEW'                           => 'Visualização',
     
