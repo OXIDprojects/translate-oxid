85215e04a459c3d34eeb3e657c5122eae4b6a9a4
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:25:25 +0200

    New translations lang.php (Czech)

--- source/Application/views/admin/cs/lang.php
+++ source/Application/views/admin/cs/lang.php
@@ -4,11 +4,6 @@
  * See LICENSE file for license details.
  */
 
-/*
- * Capitalisation in this document:
- * First letter is always capitalized
- * All nouns are capitalized
- */
 $sLangName = 'Česky';
 
 // -------------------------------
@@ -16,7 +11,7 @@ $sLangName = 'Česky';
 // -------------------------------
 $aLang = [
 
-    'charset'          => '',
+    'charset'          => 'UTF-8',
     'fullDateFormat'   => 'd.m.Y H:i:s',
     'simpleDateFormat' => 'd.m.Y',
 
