ccf91c9f2094ca5d18f9d9913578a6a6cba9aa9e
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 14:11:30 +0200

    New translations fcPayOne_lang.php (English, United States)

--- source/modules/fc/fcpayone/application/translations/en/fcPayOne_lang.php
+++ source/modules/fc/fcpayone/application/translations/en/fcPayOne_lang.php
@@ -18,12 +18,12 @@
  * @version   OXID eShop CE
  */
  
-$sLangName  = "English (Australia)";
+$sLangName  = "English (United States)";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-'charset'                                       => '',
+'charset'                                       => 'UTF-8',
 'FCPO_IBAN_INVALID'                             => '',
 'FCPO_BIC_INVALID'                              => '',
 'FCPO_BLZ_INVALID'                              => '',
@@ -70,7 +70,7 @@ $aLang = array(
 'FCPO_CREDITCARD'                               => "",
 'FCPO_CREDITCARD_CHOOSE' => "",
 'FCPO_CARD_VISA'                                => "Visa",
-'FCPO_CARD_MASTERCARD'                          => "MasterCard",
+'FCPO_CARD_MASTERCARD'                          => "Mastercard",
 'FCPO_NUMBER'                                   => "",
 'FCPO_FIRSTNAME'                                => "",
 'FCPO_LASTNAME'                                 => "",
