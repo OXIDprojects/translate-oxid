2b15159c872523be948af1d3fb17e8090ef385c7
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 13:02:33 +0200

    New translations ddoewysiwyg_lang.php (Norwegian)

--- source/modules/ddoe/wysiwyg/Application/views/admin/no/ddoewysiwyg_lang.php
+++ source/modules/ddoe/wysiwyg/Application/views/admin/no/ddoewysiwyg_lang.php
@@ -20,11 +20,11 @@
  * @version   OXID eSales WYSIWYG
  */
 
-$sLangName = 'English';
+$sLangName = 'Norsk Bokmål';
 
 $aLang = array(
     // Charsets
-    'charset' => '',
+    'charset' => 'UTF-8',
     // 'charset' => 'ISO-8859-15',
 
     // Lang-Strings
