b978fd90043983ef090d11a74dc24e7a9e0e4066
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 12:59:05 +0200

    New translations oxpspaymorrow_en_lang.php (Italian)

--- source/modules/oxps/paymorrow/translations/it/oxpspaymorrow_en_lang.php
+++ source/modules/oxps/paymorrow/translations/it/oxpspaymorrow_en_lang.php
@@ -30,10 +30,10 @@
  * the OXID module for Paymorrow payment. If not, see <http://www.gnu.org/licenses/>.
  */
 
-$sLangName = "English";
+$sLangName = "Italiano";
 
 $aLang = array(
-    "charset"                                        => "",
+    "charset"                                        => "UTF-8",
 
     'PAYMORROW_PAYMENT_METHOD_NAME_INVOICE'          => 'Fattura',
     'PAYMORROW_PAYMENT_METHOD_NAME_DIRECT_DEBIT'     => '',
