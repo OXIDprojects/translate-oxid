8856bad713bfc903419e98a223695bd837e0afa1
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 14:01:04 +0200

    New translations tcklarna_lang.php (Arabic)

--- source/modules/tc/tcklarna/views/admin/ar/tcklarna_lang.php
+++ source/modules/tc/tcklarna/views/admin/ar/tcklarna_lang.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$sLangName = "English";
+$sLangName = "العربية";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
