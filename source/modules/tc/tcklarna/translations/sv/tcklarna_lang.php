b66f3d1173514e780519ae52a13878fe839e2bd1
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:24:15 +0200

    New translations tcklarna_lang.php (Swedish)

--- source/modules/tc/tcklarna/translations/sv/tcklarna_lang.php
+++ source/modules/tc/tcklarna/translations/sv/tcklarna_lang.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$sLangName = "Schwedisch";
+$sLangName = "Svenska";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
