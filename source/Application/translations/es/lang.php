9bc3c79b3fe5b4d438d514c96f92285033a5c9cf
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 20:32:03 +0200

    New translations lang.php (Spanish, Mexico)

--- source/Application/translations/es/lang.php
+++ source/Application/translations/es/lang.php
@@ -5,7 +5,7 @@
  * See LICENSE file for license details.
  */
 
-$sLangName  = "Español";
+$sLangName  = "Español de América";
 
 // -------------------------------
 // RESOURCE IDENTIFIER = STRING
@@ -63,12 +63,12 @@ $aLang = [
 
 // @deprecated since v6.5.1 (2019-02-07); credit card payment method will be no longer supported
 'CARD_MASTERCARD'                                             => 'Mastercard',
-'CARD_SECURITY_CODE'                                          => 'CVV2 o CVC2 código de seguridad:',
-'CARD_SECURITY_CODE_DESCRIPTION'                              => 'El valor de tres dígitos de CVV2/CVC2 está impreso al lado del panel de la firma en la parte de atrás de tu tarjeta.<br>Usualmente es continuación de tu número de tarjeta.',
-'CARD_VISA'                                                   => 'visa',
+'CARD_SECURITY_CODE'                                          => 'Código de seguridad CVV2 o CVC2',
+'CARD_SECURITY_CODE_DESCRIPTION'                              => 'Este dígito de revisión está impreso en itálicas en el dorso de tu tarjeta de crédito próximo al cajón  de la firma.',
+'CARD_VISA'                                                   => 'Visa',
 // END deprecated
 /** @deprecated since v6.5.4 (2020-04-06); Suggest feature will be removed completely */
-'CARD_TO'                                                     => 'Tarjeta para:',
+'CARD_TO'                                                     => 'Tarjeta de',
 /** end deprecated */
 'CART'                                                        => 'Carrito',
 'CATEGORIES'                                                  => 'Categorías',
@@ -82,7 +82,7 @@ $aLang = [
 'CHANGE_PASSWORD'                                             => 'Cambiar tu clave',
 'CHARGES'                                                     => 'Cargos',
 /** @deprecated since v6.5.4 (2020-04-06); Suggest feature will be removed completely */
-'CHECK'                                                       => 'Comprobar',
+'CHECK'                                                       => 'Revisar',
 /** end deprecated */
 'CHECKOUT'                                                    => 'Pagar',
 'CHECK_YOUR_ORDER_HISTORY'                                    => 'revisar tu historial de órdenes',
@@ -103,7 +103,7 @@ $aLang = [
 'COUPON_NOT_ACCEPTED'                                         => 'Tu cupón "%s" no pudo ser aceptado.',
 'CREATE_PASSWORD'                                             => 'Crear clave',
 // @deprecated since v6.5.1 (2019-02-07); credit card payment method will be no longer supported
-'CREDITCARD'                                                  => 'Tarjeta',
+'CREDITCARD'                                                  => 'Tarjeta de crédito',
 // END deprecated
 'CURRENT_PRODUCT'                                             => 'Producto actual',
 'CUSTOMERS_ALSO_BOUGHT'                                       => 'Los clientes que compraron este producto también compraron',
@@ -239,8 +239,8 @@ $aLang = [
 'LAST_NAME'                                                   => 'Apellido',
 'LAST_SEEN_PRODUCTS'                                          => 'Ultimos productos vistos',
 // @deprecated 6.5.6 "News" feature will be removed completely
-'LATEST_NEWS_AND_UPDATES_AT' => 'Últimas noticias y actualizaciones en',
-'LATEST_NEWS_NOACTIVENEWS'   => 'En estos momentos no hay noticias',
+'LATEST_NEWS_AND_UPDATES_AT' => 'Ultimas noticias y actualizaciones en',
+'LATEST_NEWS_NOACTIVENEWS'   => 'No hay noticias',
 // END deprecated
 'LINKS'                                                       => 'Links',
 'LISTMANIA'                                                   => 'Listmania',
@@ -276,7 +276,7 @@ $aLang = [
 'MESSAGE_DENIED_BY_SHOP_RULES'                                => 'Denegado por reglas de la tienda.',
 'MESSAGE_EMAIL_ALREADY_IN_USE'                                => 'Este email ya existe!',
 /** @deprecated since v6.5.4 (2020-04-06); Suggest feature will be removed completely */
-'MESSAGE_ENTER_YOUR_ADDRESS_AND_MESSAGE'                      => 'Introduce tus datos de dirección y mensaje personal.',
+'MESSAGE_ENTER_YOUR_ADDRESS_AND_MESSAGE'                      => 'Escribe tu dirección y un mensaje personal!',
 /** end deprecated */
 'MESSAGE_FROM'                                                => 'Mensaje de',
 'MESSAGE_GET_BONUS_POINTS'                                    => 'Recibe puntos por tu compra ahora!',
@@ -310,7 +310,7 @@ $aLang = [
 'MESSAGE_RATE_THIS_ARTICLE'                                   => 'Evalua este producto!',
 'MESSAGE_READ_DETAILS'                                        => 'Lee nuestro',
 /** @deprecated since v6.5.4 (2020-04-06); Suggest feature will be removed completely */
-'MESSAGE_RECOMMEND_CLICK_ON_SEND'                             => 'Clickea en "Enviar recomendación" para enviar el e-mail.',
+'MESSAGE_RECOMMEND_CLICK_ON_SEND'                             => 'Haz click en "Enviar recomendación" para enviar el email!',
 /** end deprecated */
 'MESSAGE_SELECT_AT_LEAST_ONE_PRODUCT'                         => 'Por favor selecciona al menos un producto!',
 'MESSAGE_SELECT_MORE_PRODUCTS'                                => 'Por favor selecciona varios productos para la comparación!',
@@ -322,7 +322,7 @@ $aLang = [
 'MESSAGE_THANKYOU_FOR_SUBSCRIBING_NEWSLETTERS'                => 'Gracias por suscribirte a nuestra lista de mensajes.',
 'MESSAGE_UNAVAILABLE_SHIPPING_METHOD'                         => 'El método de envío que seleccionaste no esta disponible. Por favor escoge otro!',
 'MESSAGE_VERIFY_YOUR_EMAIL'                                   => 'Por favor verifica tu email!',
-'MESSAGE_WELCOME_REGISTERED_USER'                             => 'Te damos la bienvenida como usuario registrado!',
+'MESSAGE_WELCOME_REGISTERED_USER'                             => 'Bienvenido como usuario registrado!',
 'MESSAGE_WE_WILL_INFORM_YOU'                                  => 'Te informaremos de inmediato si un artículo no se puede enviar.',
 'MESSAGE_WRONG_VERIFICATION_CODE'                             => 'El código de verificación no es correcto. Por favor trate de nuevo.!',
 'MESSAGE_YOU_RECEIVED_ORDER_CONFIRM'                          => 'Ya has recibido un email con el código de confirmación.',
@@ -427,7 +427,7 @@ $aLang = [
 'PRODUCT_IMAGES'                                              => 'Imágenes del producto',
 'PRODUCT_NO'                                                  => 'Artículo #',
 /** @deprecated since v6.5.4 (2020-04-06); Suggest feature will be removed completely */
-'PRODUCT_POST_CARD_FROM'                                      => 'Una postal de producto desde',
+'PRODUCT_POST_CARD_FROM'                                      => 'Una tarjeta de producto de',
 /** end deprecated */
 'PRODUCT_REVIEW'                                              => 'Evaluación de Producto',
 'PUBLIC_GIFT_REGISTRIES'                                      => 'Registros de Regalo Públicos',
@@ -448,7 +448,7 @@ $aLang = [
 'RECIPIENT_NAME'                                              => 'Nombre del recipiente',
 'RECOMMEND'                                                   => 'Recomendar',
 /** @deprecated since v6.5.4 (2020-04-06); Suggest feature will be removed completely */
-'RECOMMENDED_PRODUCTS'                                        => 'Mi artículo recomendación',
+'RECOMMENDED_PRODUCTS'                                        => 'Producto Recomendado',
 /** end deprecated */
 'RECOMMEND_PRODUCT'                                           => 'Recomendar Producto',
 'REDEEM_COUPON'                                               => 'Aplicar Cupón',
