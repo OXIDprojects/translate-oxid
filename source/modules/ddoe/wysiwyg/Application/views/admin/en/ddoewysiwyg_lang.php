8864c81e60482c492502365349ba89801ad43584
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 14:09:20 +0200

    New translations ddoewysiwyg_lang.php (English, United Kingdom)

--- source/modules/ddoe/wysiwyg/Application/views/admin/en/ddoewysiwyg_lang.php
+++ source/modules/ddoe/wysiwyg/Application/views/admin/en/ddoewysiwyg_lang.php
@@ -20,7 +20,7 @@
  * @version   OXID eSales WYSIWYG
  */
 
-$sLangName = 'English (United States)';
+$sLangName = 'English (United Kingdom)';
 
 $aLang = array(
     // Charsets
@@ -33,8 +33,8 @@ $aLang = array(
 
     'DD_CLOSE'          => 'Close',
     'DD_CANCEL'         => 'Cancel',
-    'DD_APPLY'          => '',
-    'DD_OK'             => 'OK',
+    'DD_APPLY'          => 'Apply',
+    'DD_OK'             => 'okay',
     'DD_CONFIRM_DIALOG' => '',
     'DD_CONFIRM'        => '',
 
