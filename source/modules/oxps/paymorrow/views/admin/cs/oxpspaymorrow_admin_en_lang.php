7324799c3d83e42cbeefdc75b2669106411cceca
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:27:42 +0200

    New translations oxpspaymorrow_admin_en_lang.php (Czech)

--- source/modules/oxps/paymorrow/views/admin/cs/oxpspaymorrow_admin_en_lang.php
+++ source/modules/oxps/paymorrow/views/admin/cs/oxpspaymorrow_admin_en_lang.php
@@ -33,7 +33,7 @@
 $sLangName = "Česky";
 
 $aLang = array(
-    "charset"                                      => "",
+    "charset"                                      => "UTF-8",
     "oxpspaymorrow"                                => "",
 
     'NAVIGATION_PAYMORROW'                         => '',
