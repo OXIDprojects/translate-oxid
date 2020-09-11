0eaed65ec10f426f389d25173157c37add92ca2e
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 17:29:22 +0200

    New translations tcklarna_lang.php (Esperanto)

--- source/modules/tc/tcklarna/views/admin/eo/tcklarna_lang.php
+++ source/modules/tc/tcklarna/views/admin/eo/tcklarna_lang.php
@@ -20,7 +20,7 @@ $sLangName = "Esperanto";
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
-    'charset' => '',
+    'charset' => 'UTF-8',
 
     'TCKLARNA_KLARNAMAINMENU'      => '',
     'TCKLARNA_START'               => '',
