abaedbdb055d09167a3ce59a059c891246d7fa57
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 14:05:01 +0200

    New translations tcklarna_lang.php (Chinese Traditional)

--- source/modules/tc/tcklarna/views/admin/zh/tcklarna_lang.php
+++ source/modules/tc/tcklarna/views/admin/zh/tcklarna_lang.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$sLangName = "English";
+$sLangName = "繁體中文";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
