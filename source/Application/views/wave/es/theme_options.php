4c41451d4e2660ac24614e82ee37019d71f3f1e9
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 12:57:08 +0200

    New translations theme_options.php (Spanish, Mexico)

--- source/Application/views/wave/es/theme_options.php
+++ source/Application/views/wave/es/theme_options.php
@@ -22,7 +22,7 @@
 $sLangName  = 'English';
 
 $aLang = array(
-    'charset'                                       => '',
+    'charset'                                       => 'UTF-8',
 
     'SHOP_THEME_GROUP_images'                       => 'Imagenes',
     'SHOP_THEME_GROUP_features'                     => 'Caracteristicas',
