6c38b782d7b5ddc8ea655ef6cd3894f38b2d6690
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-11 12:53:11 +0200

    New translations fcPayOne_lang.php (Ukrainian)

--- source/modules/fc/fcpayone/application/views/admin/uk/fcPayOne_lang.php
+++ source/modules/fc/fcpayone/application/views/admin/uk/fcPayOne_lang.php
@@ -19,7 +19,7 @@
  */
  
 
-$sLangName  = "English";
+$sLangName  = "Українська";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
@@ -400,7 +400,7 @@ $aLang = array(
 'FCPO_CC_SELECT'                            => 'Виборка',
     
 'FCPO_CC_ERRORLANG_DE'                      => '',
-'FCPO_CC_ERRORLANG_EN'                      => 'English',
+'FCPO_CC_ERRORLANG_EN'                      => 'Українська',
 'FCPO_CC_CUSTOM_TEMPLATE'                   => '',
 'FCPO_CC_PREVIEW'                           => 'Попередній перегляд',
     
