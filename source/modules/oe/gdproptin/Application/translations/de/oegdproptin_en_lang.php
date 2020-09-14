de2e4e8ad4cf9ee4a15b3eb667c0c6c9e00266af
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 07:21:01 +0200

    New translations oegdproptin_en_lang.php (German, informal)

--- source/modules/oe/gdproptin/Application/translations/de/oegdproptin_en_lang.php
+++ source/modules/oe/gdproptin/Application/translations/de/oegdproptin_en_lang.php
@@ -19,19 +19,19 @@
  * @copyright (C) OXID eSales AG 2003-2018
  */
 
-$sLangName = 'Deutsch informell';
+$sLangName = 'Deutsch';
 
 $aLang = [
     'charset' => 'UTF-8',
     'OEGDPROPTIN_STORE_DELIVERY_ADDRESS' => "Ich bin damit einverstanden, dass meine Lieferadresse dauerhaft gespeichert wird. Unter 'Mein Konto' kann ich die Lieferadresse endgültig löschen.",
-    'OEGDPROPTIN_CONFIRM_STORE_DELIVERY_ADDRESS' => 'Bitte stimmen Sie dem Speichern der Lieferadresse zu!',
-    'OEGDPROPTIN_CONTACT_FORM_ERROR_MESSAGE' => "Bitte stimmen Sie den Bedingungen der Verarbeitung Ihrer Daten zu!",
+    'OEGDPROPTIN_CONFIRM_STORE_DELIVERY_ADDRESS' => 'Bitte stimme dem Speichern der Lieferadresse zu!',
+    'OEGDPROPTIN_CONTACT_FORM_ERROR_MESSAGE' => "Bitte stimme den Bedingungen der Verarbeitung Deiner Daten zu!",
     'OEGDPROPTIN_CONTACT_FORM_MESSAGE_DELETION' => "Ihre Kontaktdaten werden nach Bearbeitung der Anfrage gelöscht.",
     'OEGDPROPTIN_CONTACT_FORM_MESSAGE_STATISTICAL' => "Ich stimme der Verwendung meiner Daten zur Beantwortung dieser Anfrage und für statistische Zwecke zu.",
     'OEGDPROPTIN_USER_REGISTRATION_OPTIN' => "Ich bin damit einverstanden, dass meine Daten dauerhaft für das Kundenkonto verwendet werden. Ein Widerruf meiner Einwilligung ist jederzeit mit Wirkung für die Zukunft möglich.",
-    'OEGDPROPTIN_CONFIRM_USER_REGISTRATION_OPTIN' => 'Bitte stimmen Sie dem Speichern der Daten zu!',
+    'OEGDPROPTIN_CONFIRM_USER_REGISTRATION_OPTIN' => 'Bitte stimme dem Speichern der Daten zu!',
     'OEGDPROPTIN_REVIEW_FORM_MESSAGE' => "Ich stimme zu, dass meine Bewertung und mein Name auf dieser Seite veröffentlicht werden. Diese Einwilligung kann ich jederzeit mit Wirkung für die Zukunft widerrufen.",
-    'OEGDPROPTIN_REVIEW_FORM_ERROR_MESSAGE' => 'Bitte stimmen Sie der Veröffentlichung Ihrer Bewertung und Ihres Namens zu!',
+    'OEGDPROPTIN_REVIEW_FORM_ERROR_MESSAGE' => 'Bitte stimme der Veröffentlichung Deiner Bewertung und Deines Namens zu!',
     'OEGDPROPTIN_STORE_INVOICE_ADDRESS' => "Ich bin damit einverstanden, dass meine Rechnungsadresse dauerhaft gespeichert wird.",
-    'OEGDPROPTIN_CONFIRM_STORE_INVOICE_ADDRESS' => 'Bitte stimmen Sie dem Speichern der Rechnungsadresse zu!',
+    'OEGDPROPTIN_CONFIRM_STORE_INVOICE_ADDRESS' => 'Bitte stimme dem Speichern der Rechnungsadresse zu!',
 ];
