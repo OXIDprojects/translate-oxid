52bf0ad05c7ac859b2221a0dda940addb7c6cbb8
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 18:35:04 +0200

    New translations lang.php (English, United States)

--- source/Application/views/wave/en/lang.php
+++ source/Application/views/wave/en/lang.php
@@ -19,7 +19,7 @@
  * @copyright (C) OXID eSales AG 2003-2016
  */
 
-$sLangName = "English";
+$sLangName = "English (United States)";
 
 $aLang = array(
     'charset'                                               => 'UTF-8',
@@ -31,13 +31,13 @@ $aLang = array(
     'DD_DELETE'                                             => 'Delete',
 
     // Form validation
-    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Please enter a valid email address.',
-    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Passwords do not match.',
-    'DD_FORM_VALIDATION_NUMBER'                             => 'Please enter a number.',
-    'DD_FORM_VALIDATION_INTEGER'                            => 'There are no decimal places allowed.',
-    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Please enter a positive number.',
-    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Please enter a negative number.',
-    'DD_FORM_VALIDATION_REQUIRED'                           => 'Please specify a value for this required field.',
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Please enter a valid e-mail address',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Passwords don\'t match',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Please enter a valid number',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Decimal places are not allowed',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Please enter a positive number',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Please enter a negative number',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Please specify a value for this field',
     'DD_FORM_VALIDATION_CHECKONE'                           => 'Please select at least one option.',
 
     // Header
@@ -46,48 +46,48 @@ $aLang = array(
     'NAV_MORE'                                              => 'More',
 
     // Footer
-    'FOOTER_NEWSLETTER_INFO'                                => 'Get informed about the latest products and offers per email.',
+    'FOOTER_NEWSLETTER_INFO'                                => 'Get informed about the latest products and offers via e-mail.',
 
     // Home page
-    'MANUFACTURERSLIDER_SUBHEAD'                            => 'We present our carefully selected brands, whose products can be found in our shop.',
-    'START_BARGAIN_HEADER'                                  => 'Week\'s Special',
+    'MANUFACTURERSLIDER_SUBHEAD'                            => 'We present our carefully selected brands, whose products can be found in our store.',
+    'START_BARGAIN_HEADER'                                  => 'This week\'s special',
     'START_NEWEST_HEADER'                                   => 'Just arrived',
-    'START_TOP_PRODUCTS_HEADER'                             => 'Top seller',
-    'START_BARGAIN_SUBHEADER'                               => 'Save money with our current bargains!',
-    'START_NEWEST_SUBHEADER'                                => 'Fresh as it gets. One minute in the box and now already in store.',
-    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Only %s products, but the best we can offer you.',
+    'START_TOP_PRODUCTS_HEADER'                             => 'Top sellers',
+    'START_BARGAIN_SUBHEADER'                               => 'Save money with our current deals!',
+    'START_NEWEST_SUBHEADER'                                => 'Can\'t get it more fresh! Just arrived.',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Just two products - the best we\'d like to offer!',
 
     // Contact form
     'DD_CONTACT_PAGE_HEADING'                               => 'Contact us!',
     'DD_CONTACT_FORM_HEADING'                               => 'Contact',
 
     // Link list
-    'DD_LINKS_NO_ENTRIES'                                   => 'Unfortunately, there are no links available.',
+    'DD_LINKS_NO_ENTRIES'                                   => 'Unfortunately, there are no links available',
 
     // 404 page
-    'DD_ERR_404_START_TEXT'                                 => 'You may find the information you want from our home page:',
+    'DD_ERR_404_START_TEXT'                                 => 'You may find the requested information from our home page:',
     'DD_ERR_404_START_BUTTON'                               => 'Go to home page',
     'DD_ERR_404_CONTACT_TEXT'                               => 'May we assist you?<br>Feel free to call us or write an email:',
-    'DD_ERR_404_CONTACT_BUTTON'                             => 'to the contact page',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'to contact page',
 
     // Login
-    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'By creating an account with our store, you will be guided through the checkout process faster. In addition, you can store multiple shipping addresses and track orders in your account.',
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'By creating an account, you\'ll be guided faster through the checkout process. Additionally, you can save multiple shipping addresses and track orders.',
 
     // Billing address
     'DD_USER_LABEL_STATE'                                    => 'State',
     'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'select',
-    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'add delivery address',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'add shipping address',
     'DD_DELETE_SHIPPING_ADDRESS'                            => 'Delete shipping address',
 
     // List views
-    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filter',
-    'DD_LIST_SHOW_MORE'                                     => 'View products',
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filter:',
+    'DD_LIST_SHOW_MORE'                                     => 'View products...',
 
     // Recommendation list
     'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'back to overview',
 
     // Downloads
-    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'downloaded',
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'download',
     'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'File size:',
     'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'o\'clock',
     'DD_FILE_ATTRIBUTES_FILENAME'                           => 'File name:',
@@ -96,16 +96,16 @@ $aLang = array(
     'BACK_TO_OVERVIEW'                                      => 'to overview',
     'OF'                                                    => 'of',
     'DD_RATING_CUSTOMERRATINGS'                             => 'Customer reviews',
-    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Customers who bought this item also bought one of the following products.',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'The following products fit well to this product.',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Take a look at our similar products.',
-    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Customers who viewed this product also viewed the following products.',
-    'DETAILS_VPE_MESSAGE_1'                                 => "This product can only be ordered in packaging units of ",
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Customers who bought this item also bought one of the following products:',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'These products perfectly fit the present one:',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Have a look at similar products as well:',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Customers who viewed this product also glanced these:',
+    'DETAILS_VPE_MESSAGE_1'                                 => "This product can only be ordered in packaging units of",
     'DETAILS_VPE_MESSAGE_2'                                 => "",
     'TO_CART_NOVARIANT'                                     => 'Choose a variant',
 
     // Modal basket
-    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Total',
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Subtotal',
     'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'continue shopping',
 
     // Checkout
@@ -114,16 +114,16 @@ $aLang = array(
     // E-Mails
     'DD_FOOTER_FOLLOW_US'                                   => 'Follow us:',
     'DD_FORGOT_PASSWORD_HEADING'                            => 'Forgot password',
-    'DD_INVITE_HEADING'                                     => 'Article recommendation',
+    'DD_INVITE_HEADING'                                     => 'Product recommendation',
     'DD_INVITE_LINK'                                        => 'Link',
     'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Your newsletter subscription',
-    'DD_ORDERSHIPPED_HEADING'                               => 'Delivery confirmation - Order',
-    'DD_PRICEALARM_HEADING'                                 => 'Wished price',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Delivery confirmation - order',
+    'DD_PRICEALARM_HEADING'                                 => 'Price alert',
     'DD_REGISTER_HEADING'                                   => 'Your registration',
-    'DD_DOWNLOADLINKS_HEADING'                              => 'Your download links - Order',
-    'DD_WISHLIST_HEADING'                                   => 'Wishlist',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Your download links - orders',
+    'DD_WISHLIST_HEADING'                                   => 'Wish list',
 
-    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menu',
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Navigation',
 
     'DD_DELETE_MY_ACCOUNT_WARNING'                          => 'This action cannot be undone. This will permanently delete your personal data.',
     'DD_DELETE_MY_ACCOUNT'                                  => 'Delete account',
