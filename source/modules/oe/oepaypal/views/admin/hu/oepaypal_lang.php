5893eba90f61e06056e272ae8cddbd605b10397c
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 18:37:01 +0200

    New translations oepaypal_lang.php (Hungarian)

---
+++ source/modules/oe/oepaypal/views/admin/hu/oepaypal_lang.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This file is part of OXID eSales PayPal module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales PayPal module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2018
+ */
+
+$sLangName = 'Magyar';
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset'                              => 'ISO-8859-2',
+    'tbclorder_paypal'                     => 'PayPal',
+    'OEPAYPAL_MOBILE_DEFAULT_PAYMENT'      => 'PayPal mobil k',
+    'OEPAYPAL_AMOUNT'                      => '',
+    'OEPAYPAL_SHOP_PAYMENT_STATUS'         => 'Shop-Fizet',
+    'OEPAYPAL_ORDER_PRICE'                 => 'Megrendel',
+    'OEPAYPAL_ORDER_PRODUCTS'              => 'Megrendelt term',
+    'OEPAYPAL_CAPTURED'                    => 'Levonva',
+    'OEPAYPAL_REFUNDED'                    => 'Ki',
+    'OEPAYPAL_CAPTURED_NET'                => 'Jelenlegi fizet',
+    'OEPAYPAL_CAPTURED_AMOUNT'             => 'Levont ',
+    'OEPAYPAL_REFUNDED_AMOUNT'             => 'Ki',
+    'OEPAYPAL_VOIDED_AMOUNT'               => 'Sztorn',
+    'OEPAYPAL_MONEY_CAPTURE'               => '\'P',
+    'OEPAYPAL_MONEY_REFUND'                => 'Ki',
+    'OEPAYPAL_CAPTURE'                     => '\'Levon',
+    'OEPAYPAL_REFUND'                      => 'Ki',
+    'OEPAYPAL_DETAILS'                     => 'R',
+    'OEPAYPAL_AUTHORIZATION'               => '\'Enged',
+    'OEPAYPAL_CANCEL_AUTHORIZATION'        => 'Sztorn',
+    'OEPAYPAL_PAYMENT_HISTORY'             => 'PayPal-t',
+    'OEPAYPAL_HISTORY_DATE'                => 'D',
+    'OEPAYPAL_HISTORY_ACTION'              => 'M',
+    'OEPAYPAL_HISTORY_PAYPAL_STATUS'       => 'PayPal-',
+    'OEPAYPAL_HISTORY_PAYPAL_STATUS_HELP'  => 'PayPal visszajelzett fizet',
+    'OEPAYPAL_HISTORY_COMMENT'             => '\'Megjegyz',
+    'OEPAYPAL_HISTORY_ACTIONS'             => 'M',
+    'OEPAYPAL_HISTORY_NOTICE'              => '\'Inform',
+    'OEPAYPAL_HISTORY_NOTICE_TEXT'         => '\'Hiba eset',
+    'OEPAYPAL_MONEY_ACTION_FULL'           => 'teljes',
+    'OEPAYPAL_MONEY_ACTION_PARTIAL'        => 'r',
+    'OEPAYPAL_LIST_STATUS_ALL'             => '',
+    'OEPAYPAL_STATUS_pending'              => 'm',
+    'OEPAYPAL_STATUS_completed'            => 'Lez',
+    'OEPAYPAL_STATUS_failed'               => 'Sikertelen',
+    'OEPAYPAL_STATUS_canceled'             => 'Megszak',
+    'OEPAYPAL_ONLY_FOR_PAYPAL_PAYMENT'     => 'Ez regiszter csak azokhoz a megrendel',
+    'OEPAYPAL_ONLY_FOR_NEW_PAYPAL_PAYMENT' => 'Ez a megrendel',
+    'OEPAYPAL_PAYMENT_METHOD'              => '\'Fizet',
+    'OEPAYPAL_CLOSE'                       => '\'Lez',
+    'OEPAYPAL_COMMENT'                     => '\'Megjegyz',
+    'OEPAYPAL_RESPONSE_FROM_PAYPAL'        => 'PayPal hibajelent',
+    'OEPAYPAL_AUTHORIZATIONID'             => 'Autoriz',
+    'OEPAYPAL_TRANSACTIONID'               => 'Tranzakci',
+    'OEPAYPAL_CORRELATIONID'               => 'Korrel',
+);
