456a8440146523b129df9fcf71cf3e009bb43e79
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 15:27:42 +0200

    New translations tcklarna_lang.php (German, informal)

--- source/modules/tc/tcklarna/translations/de/tcklarna_lang.php
+++ source/modules/tc/tcklarna/translations/de/tcklarna_lang.php
@@ -15,15 +15,15 @@
  * limitations under the License.
  */
 
-$sLangName = "Deutsch";
+$sLangName = "Deutsch informell";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
 $aLang = array(
     'charset' => 'UTF-8',
 
-    'TCKLARNA_EXCEPTION_OUT_OF_STOCK' => 'Fehler bei Überprüfung der Bestellung',
-    'TCKLARNA_CHECKOUT'               => 'Kasse',
+    'TCKLARNA_EXCEPTION_OUT_OF_STOCK' => 'Validierungsfehler',
+    'TCKLARNA_CHECKOUT'               => 'Zur Kasse',
 
     'TCKLARNA_DISCOUNT_TITLE'                     => 'Rabatt',
     'TCKLARNA_VOUCHER_DISCOUNT'                   => 'Gutschein Rabatt',
@@ -34,46 +34,46 @@ $aLang = array(
 
     'TCKLARNA_PASSWORD'                          => 'Passwort',
     'TCKLARNA_TRUSTED_SHOP_BUYER_PROTECTION'     => 'Trusted Shops Käuferschutz',
-    'TCKLARNA_ALREADY_A_CUSTOMER'                => 'Bereits Kunde?',
-    'TCKLARNA_LAW_NOTICE'                        => 'Es gelten die <a href="%s" class="klarna-notification" target="_blank">Nutzungsbedingungen</a> zur Datenübertragung',
+    'TCKLARNA_ALREADY_A_CUSTOMER'                => 'Ich bin bereits Kunde',
+    'TCKLARNA_LAW_NOTICE'                        => 'Es gelten die <a href="%s" class="klarna-notification" target="_blank">Nutzungsbedingungen</a> zur Datenübertragung.',
     'TCKLARNA_OUTSIDE_VOUCHER'                   => 'Sie haben einen Gutschein?',
     'TCKLARNA_GO_TO_CHECKOUT'                    => 'Zur Kasse',
     'TCKLARNA_BUY_NOW'                           => 'Jetzt kaufen',
     'TCKLARNA_USE_AS_DELIVERY_ADDRESS'           => 'Als Lieferanschrift verwenden',
     'TCKLARNA_CHOOSE_DELIVERY_ADDRESS'           => 'Lieferadresse wählen',
-    'TCKLARNA_CREATE_USER_ACCOUNT'               => 'Kundenkonto anlegen',
-    'TCKLARNA_SUBSCRIBE_TO_NEWSLETTER'           => 'Newsletter abonnieren',
-    'TCKLARNA_CREATE_USER_ACCOUNT_AND_SUBSCRIBE' => 'Kundenkonto anlegen UND Newsletter-Anmeldung',
+    'TCKLARNA_CREATE_USER_ACCOUNT'               => 'Kundenkonto erstellen',
+    'TCKLARNA_SUBSCRIBE_TO_NEWSLETTER'           => 'Newsletter-Anmeldung',
+    'TCKLARNA_CREATE_USER_ACCOUNT_AND_SUBSCRIBE' => 'Kundenkonto erstellen und Newsletter abonnieren',
     'TCKLARNA_NO_CHECKBOX'                       => 'Keine Checkbox anzeigen',
     'TCKLARNA_ALLOW_SEPARATE_SHIPPING_ADDRESS'   => 'Lieferanschrift darf von Rechnungsanschrift abweichen',
-    'TCKLARNA_PHONE_NUMBER_MANDATORY'            => 'Telefonnummer ist Pflichtfeld im Klarna Checkout',
-    'TCKLARNA_DATE_OF_BIRTH_MANDATORY'           => 'Geburtsdatum als Pflichtfeld',
-    'TCKLARNA_CHOOSE_YOUR_SHIPPING_COUNTRY'      => 'Bitte wählen Sie Ihr Lieferland:',
-    'TCKLARNA_CHOOSE_YOUR_NOT_SUPPORTED_COUNTRY' => 'Ist Ihr Land nicht dabei?',
+    'TCKLARNA_PHONE_NUMBER_MANDATORY'            => 'Telefonnummer ist Pflichtfeld',
+    'TCKLARNA_DATE_OF_BIRTH_MANDATORY'           => 'Geburtsdatum ist Pflichtfeld',
+    'TCKLARNA_CHOOSE_YOUR_SHIPPING_COUNTRY'      => 'Bitte wähle Dein Lieferland:',
+    'TCKLARNA_CHOOSE_YOUR_NOT_SUPPORTED_COUNTRY' => 'Ist Dein Land nicht dabei?',
     'TCKLARNA_MORE_COUNTRIES'                    => 'Weitere Lieferländer',
     'TCKLARNA_MY_COUNTRY_IS_NOT_LISTED'          => 'Mein Land ist nicht in der Liste',
     'TCKLARNA_OTHER_COUNTRY'                     => 'Andere Länder',
-    'TCKLARNA_RESET_COUNTRY'                     => 'Ihr Land: <strong>%s</strong> ',
-    'TCKLARNA_CHANGE_COUNTRY'                    => 'ändern',
-    'TCKLARNA_LOGIN_INTO_AMAZON'                 => 'Bitte klicken Sie auf diesen Button, um den Login mit Amazon zu starten',
-    'KLARNA_ORDER_NOT_IN_SYNC'             => '<strong>Achtung!</strong> Die Daten dieser Bestellung weichen von den Daten ab, die bei Klarna gespeichert sind. ',
+    'TCKLARNA_RESET_COUNTRY'                     => 'Dein Land: <strong>%s</strong> ',
+    'TCKLARNA_CHANGE_COUNTRY'                    => 'Land ändern',
+    'TCKLARNA_LOGIN_INTO_AMAZON'                 => 'Bitte klicke auf diesen Button, um den Login mit Amazon zu starten',
+    'KLARNA_ORDER_NOT_IN_SYNC'             => '<strong>Achtung!</strong> Die Daten dieser Bestellung weichen von den bei Klarna gespeicherten Daten ab. ',
     'KLARNA_ORDER_IS_CANCELLED'            => 'Die Bestellung wurde storniert. ',
     'KLARNA_SEE_ORDER_IN_PORTAL'           => '<a href="%s" target="_blank" class="alert-link">Diese Bestellung im Klarna Merchant Portal aufrufen.</a>',
 
     'KLARNA_WENT_WRONG_TRY_AGAIN' => 'Ein Fehler ist aufgetreten. Bitte noch einmal versuchen.',
-    'KLARNA_WRONG_URLS_CONFIG'    => 'Fehler in der Konfiguration - prüfen Sie die URLs zu AGBs und Widerrufsrecht',
+    'KLARNA_WRONG_URLS_CONFIG'    => 'Fehler in der Konfiguration - prüfe die URLs zu AGBs und Widerrufsrecht',
 
-    'TCKLARNA_KP_CURRENCY_DONT_MATCH'      => 'Um eine Klarna Zahlungsart verwenden zu können, muss die gewählte Währung mit der offiziellen Währung Ihres Rechnungs-/Lieferland übereinstimmen.',
+    'TCKLARNA_KP_CURRENCY_DONT_MATCH'      => 'Um eine Klarna Zahlungsart verwenden zu können, muss die gewählte Währung mit der offiziellen Währung Deines Rechnungs-/Lieferland übereinstimmen.',
     'TCKLARNA_KP_MATCH_ERROR'              => 'Um diese Klarna Zahlungsart zu verwenden, müssen die Person und das Land in Rechnungs- und Lieferanschrift übereinstimmen.',
-    'TCKLARNA_KP_INVALID_TOKEN'            => 'Ungültiger Authorization Token. Bitte probieren Sie es noch einmal.',
-    'TCKLARNA_KP_ORDER_DATA_CHANGED'       => 'Bestelldaten haben sich geändert.',
+    'TCKLARNA_KP_INVALID_TOKEN'            => 'Ungültiger Authorization Token. Bitte probiere es noch einmal.',
+    'TCKLARNA_KP_ORDER_DATA_CHANGED'       => 'Bestelldaten wurden geändert. Bitte versuche es erneut.',
     'KP_NOT_AVAILABLE_FOR_COMPANIES'  => 'Bezahlen mit dieser Klarna Zahlungsmethode steht derzeit für Bestellungen von Firmen nicht zur Verfügung.',
     'KP_AVAILABLE_FOR_PRIVATE_ONLY'   => 'Bezahlen mit dieser Klarna Zahlungsmethode steht nur für Bestellungen von Privatpersonen zur Verfügung.',
     'KP_AVAILABLE_FOR_COMPANIES_ONLY' => 'Bezahlen mit dieser Klarna Zahlungsmethode steht derzeit nur für Bestellungen von Firmen zur Verfügung.',
     'TCKLARNA_KP_NOT_KLARNA_CORE_COUNTRY'  => 'Konfigurationsfehler: In diesem Land sind keine Klarna Zahlungsarten  verfügbar.',
 
-    'TCKLARNA_PLEASE_AGREE_TO_TERMS'            => 'Bitte stimmen Sie den AGB und den Widerrufsbedingungen für digitale Inhalte zu.',
-    'TCKLARNA_ERROR_NOT_ENOUGH_IN_STOCK'        => 'Nicht ausreichender Lagerbestand des Produkts %s.',
+    'TCKLARNA_PLEASE_AGREE_TO_TERMS'            => 'Bitte stimme den AGB und den Widerrufsbedingungen für digitale Inhalte zu.',
+    'TCKLARNA_ERROR_NOT_ENOUGH_IN_STOCK'        => 'Nicht genügend Artikel von Artikel %s auf Lager.',
     'TCKLARNA_ERROR_NO_SHIPPING_METHODS_SET_UP' => 'Derzeit ist keine Versandart für dieses Land definiert: %s',
 
     'TCKLARNA_PAY_LATER_SUBTITLE'    => 'Erst kaufen, dann bezahlen',
@@ -83,6 +83,6 @@ $aLang = array(
 
     'TCKLARNA_ANONYMIZED_PRODUCT'                  => 'Anonymisierter Produkttitel:',
 
-    'TCKLARNA_IS_ERROR_DEFAULT' => 'Something went wrong. Reload the page and try again.',
+    'TCKLARNA_IS_ERROR_DEFAULT' => 'Etwas ist schief gelaufen. Lade die Seite neu und versuche es erneut.',
 );
 
