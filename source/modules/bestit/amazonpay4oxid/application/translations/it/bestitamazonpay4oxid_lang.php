edea96c7ff0220c86e369c265a3948c68c8f5f2b
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 18:35:51 +0200

    New translations bestitamazonpay4oxid_lang.php (Italian)

--- source/modules/bestit/amazonpay4oxid/application/translations/it/bestitamazonpay4oxid_lang.php
+++ source/modules/bestit/amazonpay4oxid/application/translations/it/bestitamazonpay4oxid_lang.php
@@ -1,28 +1,28 @@
 <?php
 
 /**
- * Language entries for IT
+ * Language entries for EN
  *
  * @author best it GmbH & Co. KG <info@bestit-online.de>
  */
 
-$sLangName  = 'italiano';
+$sLangName  = "Italiano";
 
 $aLang = array(
     'charset'                                                    => 'UTF-8',
-    'BESTITAMAZONPAY_SWITCH_BACK_2_STANDARD'                   => 'Passare da Amazon Pay a Standard',
-    'BESTITAMAZONPAY_ERROR_AMAZON_TERMINATED'                  => 'La procedura di pagamento per Amzon é stato interrotta... La preghiamo di provare di nuovo.',
-    'BESTITAMAZONPAY_PLEASE_CLICK_ON_AMAZON_BUTTON'            => 'Per favore prema il bottone Amazon-Payment per iniziare.',
-    'BESTITAMAZONPAY_PAY_WITH_AMAZON_BUTTON'                   => 'Pagare con Amazon.\n\nCon Amazon Pay puó usare le modalitá di pagamento e spedizione memorizzate nel suo Account Amazon per effettuare acquisti in modo veloce e sicuro.\nInoltre godrá per acquisti qualificati della tutela del compratore, che Amazon offre attraverso la garanzia dalla A alla Z.',
-    'BESTITAMAZONPAY_ORDER_NO'                                 => 'Numero d‘ordine',
-    'BESTITAMAZONPAY_NO_PAYMENTS_FOR_SHIPPING_ADDRESS'         => 'La destinazione di consegna scelta non viene supportata. Per favore scelga un‘altro indirizzo.',
-    'BESTITAMAZONPAY_PAYMENT_DECLINED_OR_REJECTED'             => 'Amazon Pay ha respinto il Suo pagamento, per favore scelga un’altra modalitá di pagamento.',
-    'BESTITAMAZONPAY_PAYMENT_INVALID'                          => 'Amazon Pay ha respinto il Suo pagamento, per favore scelga un’altra modalitá di pagamento.',
-    'BESTITAMAZONPAY_CHANGE_PAYMENT'                           => 'La modalitá di pagamento scelta non viene supportata. Per favore scelga un’altra modalitá di pagamento.',
-    'BESTITAMAZONPAYLOGIN_ERROR_UNEXPECTED'                    => 'Durante il collegamento con Amazon é insorto un errore improvviso. Per favore provi di nuovo.',
-    'BESTITAMAZONPAYLOGIN_ERROR_ACCOUNT_WITH_EMAIL_EXISTS'     => 'Per questo indirizzo Email esiste giá un conto. Per favore esegua il login prima nel negozio online scelto e poi nel Suo conto Amazon.',
-    'BESTITAMAZONPAYLOGIN_ERROR_invalid_token'                 => 'La Sua sessione Amazon é scaduta. Per favore esegua di nuovo l’accesso.',
-    'BESTITAMAZONPAYLOGIN_PURCHASE_WITH_AMAZON'                => 'Acquisti con il Suo conto Amazon.',
+    'BESTITAMAZONPAY_SWITCH_BACK_2_STANDARD'                   => "Passare da Amazon Pay a Standard",
+    'BESTITAMAZONPAY_ERROR_AMAZON_TERMINATED'                  => "La procedura di pagamento per Amzon é stato interrotta... La preghiamo di provare di nuovo.",
+    'BESTITAMAZONPAY_PLEASE_CLICK_ON_AMAZON_BUTTON'            => "Per favore prema il bottone Amazon-Payment per iniziare.",
+    'BESTITAMAZONPAY_PAY_WITH_AMAZON_BUTTON'                   => "Pagare con Amazon.\\n\\nCon Amazon Pay puó usare le modalitá di pagamento e spedizione memorizzate nel suo Account Amazon per effettuare acquisti in modo veloce e sicuro.\\nInoltre godrá per acquisti qualificati della tutela del compratore, che Amazon offre attraverso la garanzia dalla A alla Z.",
+    'BESTITAMAZONPAY_ORDER_NO'                                 => "Numero d‘ordine",
+    'BESTITAMAZONPAY_NO_PAYMENTS_FOR_SHIPPING_ADDRESS'         => "La destinazione di consegna scelta non viene supportata. Per favore scelga un‘altro indirizzo.",
+    'BESTITAMAZONPAY_PAYMENT_DECLINED_OR_REJECTED'             => "Amazon Pay ha respinto il Suo pagamento, per favore scelga un’altra modalitá di pagamento.",
+    'BESTITAMAZONPAY_PAYMENT_INVALID'                          => "Amazon Pay ha respinto il Suo pagamento, per favore scelga un’altra modalitá di pagamento.",
+    'BESTITAMAZONPAY_CHANGE_PAYMENT'                           => "La modalitá di pagamento scelta non viene supportata. Per favore scelga un’altra modalitá di pagamento.",
+    'BESTITAMAZONPAYLOGIN_ERROR_UNEXPECTED'                    => "Durante il collegamento con Amazon é insorto un errore improvviso. Per favore provi di nuovo.",
+    'BESTITAMAZONPAYLOGIN_ERROR_ACCOUNT_WITH_EMAIL_EXISTS'     => "Per questo indirizzo Email esiste giá un conto. Per favore esegua il login prima nel negozio online scelto e poi nel Suo conto Amazon.",
+    'BESTITAMAZONPAYLOGIN_ERROR_invalid_token'                 => "La Sua sessione Amazon é scaduta. Per favore esegua di nuovo l’accesso.",
+    'BESTITAMAZONPAYLOGIN_PURCHASE_WITH_AMAZON'                => "Acquisti con il Suo conto Amazon.",
 
     'BESTITAMAZONPAY_EMAIL_SUBJECT_INVALID_PAYMENT'            => 'Per favore dia un nuova modalitá di pagamento per il Suo ordine Amazon Pay.',
     'BESTITAMAZONPAY_EMAIL_SUBJECT_REJECTED_PAYMENT'           => 'Purtroppo il Suo pagamento é stato rifiutato da Amazon Pay',
