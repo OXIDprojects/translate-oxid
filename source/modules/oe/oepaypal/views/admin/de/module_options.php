1bdf21ca7db5d1942cd712c4e81523d4a1c7359c
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 15:27:13 +0200

    New translations module_options.php (German)

--- source/modules/oe/oepaypal/views/admin/de/module_options.php
+++ source/modules/oe/oepaypal/views/admin/de/module_options.php
@@ -32,9 +32,9 @@ $aLang = array(
     'SHOP_MODULE_GROUP_oepaypal_development'                 => 'Einstellungen für Entwicklung',
 
     'SHOP_MODULE_sOEPayPalBrandName'                         => 'Name des Shops',
-    'HELP_SHOP_MODULE_sOEPayPalBrandName'                    => 'Trage hier den Namen Deines Shops ein, der auf der PayPal-Zahlungsseite angezeigt werden soll.',
+    'HELP_SHOP_MODULE_sOEPayPalBrandName'                    => 'Tragen Sie hier den Namen Ihres Shops ein, der auf der PayPal-Zahlungsseite angezeigt werden soll.',
     'SHOP_MODULE_sOEPayPalBorderColor'                       => 'Warenkorbumrandungsfarbe für die PayPal-Zahlungsseite',
-    'HELP_SHOP_MODULE_sOEPayPalBorderColor'                  => 'Trage hier den hexadezimalen Code der Farbe ein, die bei der Anzeige der PayPal-Zahlungsseite verwendet werden soll.',
+    'HELP_SHOP_MODULE_sOEPayPalBorderColor'                  => 'Tragen Sie hier den hexadezimalen Code der Farbe ein, die bei der Anzeige der PayPal-Zahlungsseite verwendet werden soll.',
 
     'SHOP_MODULE_blOEPayPalStandardCheckout'                 => 'PayPal Basis',
     'HELP_SHOP_MODULE_blOEPayPalStandardCheckout'            => 'PayPal wird am Ende des Bestellprozesses als Zahlungsart angeboten. Wählt der Kunde PayPal, bestätigt er auf der PayPal-Zahlungsseite den Kauf und wird anschließend in den Shop zurückgeleitet.',
@@ -49,17 +49,17 @@ $aLang = array(
     'HELP_SHOP_MODULE_blOEPayPalDefaultUserChoice'           => 'Der Kunde muss im Bestellprozess explizit bestätigen, dass der Warenkorb mit Artikelinformationen, Preisen und Versandkosten zu PayPal übertragen wird. Sie können hier die Voreinstellung aktivieren, dass der Kunde der Übermittlung der Daten standardmäßig zustimmt.',
 
     'SHOP_MODULE_sOEPayPalLogoImageOption'                   => 'Shop-Logo auf der PayPal-Zahlungsseite',
-    'HELP_SHOP_MODULE_sOEPayPalLogoImageOption'              => 'Auf der PayPal-Zahlungsseite kann ein Shop-Logo angezeigt werden. Es ist möglich, das Standard-Shop-Logo zu verwenden, welches in der Konfigurationsdatei des Shops definiert ist, oder ein spezielles Shop-Logo. Das Logo sollte nicht größer als 190px*60px (Breite*Höhe) sein. Größere Bilder werden an diese Breite und Höhe angepasst und deren Dateinamen mit einem Prefix "resized_" versehen. Für jedes verwendete Theme muss die Datei mit dem Logo im Verzeichnis /out/{theme}/img vorhanden sein. Wird das vorgesehene Logo nicht angezeigt, überprüfe bitte, ob der Dateiname korrekt angegeben wurde und die Datei im Verzeichnis existiert. Für das Standard-Shop-Logo überprüfe den Eintrag "sShopLogo" in der Datei config.inc.php. Füge diesen Eintrag hinzu, wenn dieser nicht vorhanden ist.',
+    'HELP_SHOP_MODULE_sOEPayPalLogoImageOption'              => 'Auf der PayPal-Zahlungsseite kann ein Shop-Logo angezeigt werden. Es ist möglich, das Standard-Shop-Logo zu verwenden, welches in der Konfigurationsdatei des Shops definiert ist, oder ein spezielles Shop-Logo. Das Logo sollte nicht größer als 190px*60px (Breite*Höhe) sein. Größere Bilder werden an diese Breite und Höhe angepasst und deren Dateinamen mit einem Prefix "resized_" versehen. Für jedes verwendete Theme muss die Datei mit dem Logo im Verzeichnis /out/{theme}/img vorhanden sein. Wird das vorgesehene Logo nicht angezeigt, überprüfen Sie bitte, ob der Dateiname korrekt angegeben wurde und die Datei im Verzeichnis existiert. Für das Standard-Shop-Logo überprüfen Sie den Eintrag "sShopLogo" in der Datei config.inc.php. Fügen Sie diesen Eintrag hinzu, wenn dieser nicht vorhanden ist',
 
     'SHOP_MODULE_sOEPayPalCustomShopLogoImage'               => 'Spezielles Shop-Logo für die PayPal-Zahlungsseite',
-    'HELP_SHOP_MODULE_sOEPayPalCustomShopLogoImage'          => 'Auf der PayPal-Zahlungsseite kann ein eigenes Shop-Logo angezeigt werden. Speichere das Logo im Bildverzeichnis des Shops (/out/{theme}/img) und trage den Dateinamen hier ein. Für jedes verwendete Theme muss die Datei mit dem Logo im jeweiligen Verzeichnis vorhanden sein.',
+    'HELP_SHOP_MODULE_sOEPayPalCustomShopLogoImage'          => 'Auf der PayPal-Zahlungsseite kann ein eigenes Shop-Logo angezeigt werden. Speichern Sie das Logo im Bildverzeichnis des Shops (/out/{theme}/img) und tragen Sie den Dateinamen hier ein. Für jedes verwendete Theme muss die Datei mit dem Logo im jeweiligen Verzeichnis vorhanden sein.',
 
     'SHOP_MODULE_sOEPayPalLogoImageOption_noLogo'            => 'Kein Shop-Logo senden',
     'SHOP_MODULE_sOEPayPalLogoImageOption_shopLogo'          => 'Standard-Shop-Logo senden',
     'SHOP_MODULE_sOEPayPalLogoImageOption_customLogo'        => 'Spezielles Shop-Logo senden',
 
     'SHOP_MODULE_sOEPayPalTransactionMode'                   => 'Zeitpunkt des Geldtransfers',
-    'HELP_SHOP_MODULE_sOEPayPalTransactionMode'              => 'Wähle aus, zu welchem Zeitpunkt der Geldtransfer stattfinden soll. Du hast die Möglichkeit, den Einzug des Geldes auf der PayPal-Seite sofort beim Kauf (SALE), oder erst unmittelbar vor Versand der Ware manuell durchzuführen (AUTH). Du kannst auch festlegen, dass der Zeitpunkt des Geldtransfers in Abhängigkeit vom Lagerbestand der bestellten Artikel vom Shop automatisch bestimmt wird (AUTOMATIC).',
+    'HELP_SHOP_MODULE_sOEPayPalTransactionMode'              => 'Wählen Sie aus, zu welchem Zeitpunkt der Geldtransfer stattfinden soll. Sie haben die Möglichkeit, den Einzug des Geldes auf der PayPal-Seite sofort beim Kauf (SALE), oder erst unmittelbar vor Versand der Ware manuell durchzuführen (AUTH). Sie können auch festlegen, dass der Zeitpunkt des Geldtransfers in Abhängigkeit vom Lagerbestand der bestellten Artikel vom Shop automatisch bestimmt wird (AUTOMATIC).',
 
     'SHOP_MODULE_sOEPayPalTransactionMode_Automatic'         => 'AUTOMATIC - abhängig vom Lagerbestand der bestellten Artikel',
     'SHOP_MODULE_sOEPayPalTransactionMode_Sale'              => 'SALE - sofort durchführen',
@@ -69,14 +69,14 @@ $aLang = array(
 
     'SHOP_MODULE_sOEPayPalUserEmail'                         => 'E-Mail-Adresse des PayPal-Benutzers',
     'SHOP_MODULE_sOEPayPalUsername'                          => 'API-Benutzername',
-    'HELP_SHOP_MODULE_sOEPayPalUsername'                     => 'Logge Dich in Dein <a target="_blank" href="https://www.paypal.com/de/cgi-bin/webscr?cmd=_get-api-signature&generic-flow=true">PayPal-Konto</a> ein, um Deine API-Signatur zu erhalten.',
+    'HELP_SHOP_MODULE_sOEPayPalUsername'                     => 'Loggen Sie sich in Ihr <a target="_blank" href="https://www.paypal.com/de/cgi-bin/webscr?cmd=_get-api-signature&generic-flow=true">PayPal-Konto</a> ein, um Ihre API-Signatur zu erhalten.',
     'SHOP_MODULE_sOEPayPalPassword'                          => 'API-Passwort',
     'SHOP_MODULE_sOEPayPalSignature'                         => 'Unterschrift',
 
     'SHOP_MODULE_blOEPayPalSandboxMode'                      => 'Sandbox aktivieren',
     'SHOP_MODULE_sOEPayPalSandboxUserEmail'                  => 'Sandbox: E-Mail-Adresse des PayPal-Benutzers',
     'SHOP_MODULE_sOEPayPalSandboxUsername'                   => 'Sandbox: API-Benutzername',
-    'HELP_SHOP_MODULE_sOEPayPalSandboxUsername'              => 'Logge Dich in Dein <a target="_blank" href="https://www.sandbox.paypal.com/de/cgi-bin/webscr?cmd=_get-api-signature&generic-flow=true">PayPal-Konto</a> ein, um Deine API-Signatur für die PayPal-Sandbox zu erhalten.',
+    'HELP_SHOP_MODULE_sOEPayPalSandboxUsername'              => 'Loggen Sie sich in Ihr <a target="_blank" href="https://www.sandbox.paypal.com/de/cgi-bin/webscr?cmd=_get-api-signature&generic-flow=true">PayPal-Konto</a> ein, um Ihre API-Signatur für die PayPal-Sandbox zu erhalten.',
     'SHOP_MODULE_sOEPayPalSandboxPassword'                   => 'Sandbox: API-Passwort',
     'SHOP_MODULE_sOEPayPalSandboxSignature'                  => 'Sandbox: Unterschrift',
 
