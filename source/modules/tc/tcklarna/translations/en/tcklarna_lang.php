96f40156eabdd75f1e2dccfa7c04b95b6c62042e
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 14:11:27 +0200

    New translations tcklarna_lang.php (English, United States)

--- source/modules/tc/tcklarna/translations/en/tcklarna_lang.php
+++ source/modules/tc/tcklarna/translations/en/tcklarna_lang.php
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-$sLangName = "English (United Kingdom)";
+$sLangName = "English (United States)";
 // -------------------------------
 // RESOURCE IDENTITFIER = STRING
 // -------------------------------
