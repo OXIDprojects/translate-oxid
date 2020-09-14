c65445c243fb63a95203193e4d8e4bbd76465d72
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 15:28:38 +0200

    New translations lang.php (English, United Kingdom)

--- source/Application/views/wave/en/lang.php
+++ source/Application/views/wave/en/lang.php
@@ -19,7 +19,7 @@
  * @copyright (C) OXID eSales AG 2003-2016
  */
 
-$sLangName = "English (Australia)";
+$sLangName = "English (United Kingdom)";
 
 $aLang = array(
     'charset'                                               => 'UTF-8',
