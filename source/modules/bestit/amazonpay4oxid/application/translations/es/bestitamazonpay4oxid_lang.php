a5bc7cd981e0d809b00e1ebac8275751ccd196c4
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 18:35:42 +0200

    New translations bestitamazonpay4oxid_lang.php (Spanish, Mexico)

--- source/modules/bestit/amazonpay4oxid/application/translations/es/bestitamazonpay4oxid_lang.php
+++ source/modules/bestit/amazonpay4oxid/application/translations/es/bestitamazonpay4oxid_lang.php
@@ -1,19 +1,22 @@
 <?php
 
 /**
- * Language entries for ES
+ * Language entries for EN
  *
  * @author best it GmbH & Co. KG <info@bestit-online.de>
  */
 
-$sLangName  = "español";
+$sLangName  = "Español de América";
 
 $aLang = array(
     'charset'                                                    => 'UTF-8',
     'BESTITAMAZONPAY_SWITCH_BACK_2_STANDARD'                   => "Cambiar de Amazon Pay a Standard",
     'BESTITAMAZONPAY_ERROR_AMAZON_TERMINATED'                  => "El proceso de pago con Amazon ha sido interrumpido. Por favor, inténtelo de nuevo.",
     'BESTITAMAZONPAY_PLEASE_CLICK_ON_AMAZON_BUTTON'            => "Por favor, haga clic en el botón de Amazon Pay para comenzar el proceso.",
-    'BESTITAMAZONPAY_PAY_WITH_AMAZON_BUTTON'                   => "Pagar con Amazon.\n\nCon Amazon Pay puede utilizar la información de pago y las direcciones de su cuenta de Amazon para comprar de manera segura y rápida.\nAdemás, para aquellas compras que cumplan los requisitos, disfrutará de la protección que la garantía de la A a la Z de Amazon ofrece.",
+    'BESTITAMAZONPAY_PAY_WITH_AMAZON_BUTTON'                   => "Pagar con Amazon.
+
+Con Amazon Pay puede utilizar la información de pago y las direcciones de su cuenta de Amazon para comprar de manera segura y rápida.
+Además, para aquellas compras que cumplan los requisitos, disfrutará de la protección que la garantía de la A a la Z de Amazon ofrece.",
     'BESTITAMAZONPAY_ORDER_NO'                                 => "Número de pedido",
     'BESTITAMAZONPAY_NO_PAYMENTS_FOR_SHIPPING_ADDRESS'         => "La dirección que ha seleccionado no está disponible. Por favor, seleccione otro destino.",
     'BESTITAMAZONPAY_PAYMENT_DECLINED_OR_REJECTED'             => "Amazon Pay ha rechazado su pago. Por favor, seleccione otro método de pago.",
@@ -23,7 +26,7 @@ $aLang = array(
     'BESTITAMAZONPAYLOGIN_ERROR_ACCOUNT_WITH_EMAIL_EXISTS'     => "Ya existe una cuenta asociada a esta dirección de correo electrónico. Por favor, identifíquese primero con la contraseña de su cuenta en la tienda y después con su cuenta de Amazon.",
     'BESTITAMAZONPAYLOGIN_ERROR_invalid_token'                 => "La sesión con Amazon ha caducado. Por favor, identifíquese de nuevo.",
     'BESTITAMAZONPAYLOGIN_PURCHASE_WITH_AMAZON'                => "Compre con su cuenta de Amazon.",
-    
+
     'BESTITAMAZONPAY_EMAIL_SUBJECT_INVALID_PAYMENT'            => 'Por favor, actualiza tu información de pago',
     'BESTITAMAZONPAY_EMAIL_SUBJECT_REJECTED_PAYMENT'           => 'Tu pago con Amazon Pay ha sido rechazado',
     'BESTITAMAZONPAY_EMAIL_VALUED_CUSUTOMER'                   => 'Saludos',
@@ -35,7 +38,7 @@ $aLang = array(
     'BESTITAMAZONPAY_EMAIL_DECLINED_TEXT'                      => 'Desgraciadamente, Amazon Pay ha rechazado el pago de tu pedido en nuestra tienda online',
     'BESTITAMAZONPAY_EMAIL_CONTACT_US'                         => 'Por favor, contáctanos en referencia a tu pedido',
     'BESTITAMAZONPAY_ERROR_CURRENCY_UNSUPPORTED'               => 'Amazon Pay no puede ser utilizado con la moneda seleccionada. Por favor, seleccione una moneda diferente, si es posible, en la página web del vendedor.',
-    'BESTITAMAZONPAY_BILLING_ADDRESS'                          => 'Dirección de facturación',
+    'BESTITAMAZONPAY_BILLING_ADDRESS'                          => 'Dirección de cobro',
     'BESTITAMAZONPAY_UPDATE_BILLING_ADDRESS'                   => 'Utiliza la dirección de facturación de Amazon',
     'BESTITAMAZONPAY_BILLING_ADDRESS_CURRENT'                  => 'Dirección de facturación actual',
     'BESTITAMAZONPAY_BILLING_ADDRESS_NEW'                      => 'Dirección de facturación de Amazon',
