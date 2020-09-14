e297380d8422f266d4a4ab20748a0bc489c272a0
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-14 07:20:54 +0200

    New translations oxpspaymorrow_en_lang.php (German, informal)

--- source/modules/oxps/paymorrow/translations/de/oxpspaymorrow_en_lang.php
+++ source/modules/oxps/paymorrow/translations/de/oxpspaymorrow_en_lang.php
@@ -30,21 +30,21 @@
  * the OXID module for Paymorrow payment. If not, see <http://www.gnu.org/licenses/>.
  */
 
-$sLangName = "Deutsch informell";
+$sLangName = "Deutsch";
 
 $aLang = array(
     "charset"                                        => "UTF-8",
 
-    'PAYMORROW_PAYMENT_METHOD_NAME_INVOICE'          => 'Rechnung',
+    'PAYMORROW_PAYMENT_METHOD_NAME_INVOICE'          => 'Rechnungskauf',
     'PAYMORROW_PAYMENT_METHOD_NAME_DIRECT_DEBIT'     => 'Lastschriftverfahren',
 
     'PAYMORROW_PAYMENT_NO_JAVASCRIPT'                => 'Um diese Zahlungsart zu nutzen, muss JavaScript im Browser aktiviert sein.',
 
-    'PAYMORROW_GENERAL_ERROR'                        => 'Es ist ein Fehler aufgetreten. Bitte wiederholen Sie den Vorgang.',
-    'PAYMORROW_ACCEPT_CONDITIONS_ERROR'              => 'Bitte akzeptieren Sie die Datenschutzbestimmungen der Paymorrow GmbH.',
-    'PAYMORROW_SELECT_GENDER_ERROR'                  => 'Sie haben keine Anrede ausgewählt.',
-    'PAYMORROW_DATE_OF_BIRTH_ERROR'                  => 'Sie haben kein Geburtsdatum angegeben.',
-    'PAYMORROW_MOBILE_NUMBER_ERROR'                  => 'Sie haben keine Festnetz- oder Mobilnummer eingegeben.',
+    'PAYMORROW_GENERAL_ERROR'                        => 'Es ist ein Fehler aufgetreten. Bitte wiederhole den Vorgang.',
+    'PAYMORROW_ACCEPT_CONDITIONS_ERROR'              => 'Bitte akzeptiere die Datenschutzbestimmungen der Paymorrow GmbH.',
+    'PAYMORROW_SELECT_GENDER_ERROR'                  => 'Du hast keine Anrede ausgewählt.',
+    'PAYMORROW_DATE_OF_BIRTH_ERROR'                  => 'Du hast kein Geburtsdatum angegeben.',
+    'PAYMORROW_MOBILE_NUMBER_ERROR'                  => 'Du hast keine Festnetz- oder Mobilnummer eingegeben.',
 
     // Custom
     'PAYMORROW_ORDER_DATA_COLLECTION_FAILED'         => 'Die Bestelldatenerfassung ist gescheitert',
