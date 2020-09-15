91e36d56ec74038392fc36aa7e13faea36d0d7fc
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-15 16:16:01 +0200

    New translations lang.php (Polish)

---
+++ source/Application/views/wave/pl-PL/lang.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This file is part of OXID eSales Wave theme.
+ *
+ * OXID eSales Wave theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Wave theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Wave theme.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+$sLangName = "Polski";
+
+$aLang = array(
+    'charset'                                               => 'UTF-8',
+
+    // Global
+    'DD_SORT_DESC'                                          => '',
+    'DD_SORT_ASC'                                           => '',
+    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_DELETE'                                             => 'Usuń',
+
+    // Form validation
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => '',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
+    'DD_FORM_VALIDATION_NUMBER'                             => '',
+    'DD_FORM_VALIDATION_INTEGER'                            => '',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => '',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => '',
+    'DD_FORM_VALIDATION_REQUIRED'                           => '',
+    'DD_FORM_VALIDATION_CHECKONE'                           => '',
+
+    // Header
+    'SEARCH_TITLE'                                          => '',
+    'SEARCH_SUBMIT'                                         => 'Szukaj',
+    'NAV_MORE'                                              => 'Więcej',
+
+    // Footer
+    'FOOTER_NEWSLETTER_INFO'                                => '',
+
+    // Home page
+    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
+    'START_BARGAIN_HEADER'                                  => '',
+    'START_NEWEST_HEADER'                                   => '',
+    'START_TOP_PRODUCTS_HEADER'                             => '',
+    'START_BARGAIN_SUBHEADER'                               => '',
+    'START_NEWEST_SUBHEADER'                                => '',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
+
+    // Contact form
+    'DD_CONTACT_PAGE_HEADING'                               => '',
+    'DD_CONTACT_FORM_HEADING'                               => 'Kontakt',
+
+    // Link list
+    'DD_LINKS_NO_ENTRIES'                                   => '',
+
+    // 404 page
+    'DD_ERR_404_START_TEXT'                                 => '',
+    'DD_ERR_404_START_BUTTON'                               => '',
+    'DD_ERR_404_CONTACT_TEXT'                               => '',
+    'DD_ERR_404_CONTACT_BUTTON'                             => '',
+
+    // Login
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+
+    // Billing address
+    'DD_USER_LABEL_STATE'                                    => 'Stan',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => '',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => '',
+    'DD_DELETE_SHIPPING_ADDRESS'                            => '',
+
+    // List views
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => '',
+    'DD_LIST_SHOW_MORE'                                     => '',
+
+    // Recommendation list
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => '',
+
+    // Downloads
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => '',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => '',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => '',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => '',
+
+    // Details page
+    'BACK_TO_OVERVIEW'                                      => '',
+    'OF'                                                    => 'z',
+    'DD_RATING_CUSTOMERRATINGS'                             => '',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
+    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'DETAILS_VPE_MESSAGE_2'                                 => "",
+    'TO_CART_NOVARIANT'                                     => '',
+
+    // Modal basket
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Razem',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => '',
+
+    // Checkout
+    'DD_BASKET_BACK_TO_SHOP'                                => '',
+
+    // E-Mails
+    'DD_FOOTER_FOLLOW_US'                                   => '',
+    'DD_FORGOT_PASSWORD_HEADING'                            => '',
+    'DD_INVITE_HEADING'                                     => '',
+    'DD_INVITE_LINK'                                        => 'łącze',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
+    'DD_ORDERSHIPPED_HEADING'                               => '',
+    'DD_PRICEALARM_HEADING'                                 => 'Cena klienta',
+    'DD_REGISTER_HEADING'                                   => '',
+    'DD_DOWNLOADLINKS_HEADING'                              => '',
+    'DD_WISHLIST_HEADING'                                   => '',
+
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menu',
+
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
+    'DD_DELETE_MY_ACCOUNT'                                  => '',
+    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => '',
+    'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'Auluj',
+    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => '',
+    'DD_DELETE_MY_ACCOUNT_ERROR'                            => '',
+
+    // Account -> My product reviews
+    'DD_DELETE_REVIEW_AND_RATING'                           => '',
+    'DD_REVIEWS_NOT_AVAILABLE'                              => '',
+    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => '',
+
+    // Contact page
+    'DD_SELECT_SALUTATION'                                  => 'Proszę wybrać',
+
+    'DD_CATEGORY_RESET_BUTTON'                              => 'Reset',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE"}]
+*/
