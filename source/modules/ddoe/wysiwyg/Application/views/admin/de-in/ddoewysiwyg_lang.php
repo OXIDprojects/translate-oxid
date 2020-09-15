78ef9bae9d41ed08ea19fb0b7927689dbf3e6cd0
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-15 13:16:42 +0200

    New translations ddoewysiwyg_lang.php (German, informal)

---
+++ source/modules/ddoe/wysiwyg/Application/views/admin/de-in/ddoewysiwyg_lang.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * This file is part of OXID eSales WYSIWYG module.
+ *
+ * OXID eSales WYSIWYG module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales WYSIWYG module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales WYSIWYG module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2017
+ * @version   OXID eSales WYSIWYG
+ */
+
+$sLangName = 'Deutsch informell';
+
+$aLang = array(
+    // Charsets
+    'charset' => 'UTF-8',
+    // 'charset' => 'ISO-8859-15',
+
+    // Lang-Strings
+
+    // Base Module
+
+    'DD_CLOSE'          => 'Schließen',
+    'DD_CANCEL'         => 'Beenden',
+    'DD_APPLY'          => 'Übernehmen',
+    'DD_OK'             => 'OK',
+    'DD_CONFIRM_DIALOG' => 'Bestätigung erforderlich!',
+    'DD_CONFIRM'        => 'Bestätigen',
+
+    'DD_MEDIA_DIALOG'               => 'Mediathek',
+    'DD_MEDIA_LIST'                 => 'Medienliste',
+    'DD_MEDIA_UPLOAD'               => 'Hochladen',
+    'DD_MEDIA_REMOVE'               => '<i class="fa fa-times-circle"></i> Daten unwiderruflich löschen',
+    'DD_MEDIA_REMOVE_CONFIRM'       => 'Möchtest Du diese Datei wirklich löschen?',
+    'DD_MEDIA_FALLBACK_INFO'        => 'Dateien für\'s <strong>Hochladen</strong> auswählen',
+    'DD_MEDIA_UPLOAD_INFO'          => 'Datei zum <strong>Hochladen</strong> hierhin ziehen <br><small>oder hier klicken</small>',
+    'DD_MEDIA_DRAG_INFO'            => 'Datei <strong>zum Hochladen</strong> hierhin ziehen',
+    'DD_MEDIA_EMPTY_LIST'           => 'Keine Medien vorhanden',
+    'DD_MEDIA_FILETYPE_NOT_ALLOWED' => 'Dateiformat nicht erlaubt!',
+    'DD_MEDIA_FILES_FOUND'          => 'Dateien in der Mediathek',
+    'DD_MEDIA_MULTIPLE_INFO'        => 'STRG + Mausklick für Mehrfachauswahl',
+
+    'DD_MEDIA_EXCEPTION_INVALID_FILEEXT'                   => 'Der Upload von Dateien mit dieser Dateiendung ist nicht erlaubt!',
+
+);
