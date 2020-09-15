d974c7bee57dfa65eed95ededb7a44c653558cd3
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-15 14:06:05 +0200

    New translations lang.php (Swedish)

---
+++ source/Application/views/wave/sv-SE/lang.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This file is part of OXID eSales Wave theme.
+ *
+ * OXID eSales Wave theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Wave theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Wave theme.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+$sLangName = "Svenska";
+
+$aLang = array(
+    'charset'                                               => 'UTF-8',
+
+    // Global
+    'DD_SORT_DESC'                                          => 'uppstigande',
+    'DD_SORT_ASC'                                           => 'nerstigande',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Starta administratorverktyg',
+    'DD_DELETE'                                             => 'ta bort',
+
+    // Form validation
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Var snäll ange en giltig mejladress.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Lösenorden stämmer inte överense.',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Var snäll och fyll i en siffra.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Siffror efter kommat är ej tillåtna.',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Var snäll ange en positiv siffra.',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Var snäll ange en negativ siffra.',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Var snäll och ange ett värde.',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Var snäll och välj minst en option.',
+
+    // Header
+    'SEARCH_TITLE'                                          => 'Ange sökord...',
+    'SEARCH_SUBMIT'                                         => 'sök',
+    'NAV_MORE'                                              => 'mer',
+
+    // Footer
+    'FOOTER_NEWSLETTER_INFO'                                => 'Nyaste produkter och bästa erbjudande via mejl att du inte missa någonting!',
+
+    // Home page
+    'MANUFACTURERSLIDER_SUBHEAD'                            => 'Här presenterar vi våra utvalda varumärken, deras produkter finns i vår webbutik.',
+    'START_BARGAIN_HEADER'                                  => 'Veckans deal',
+    'START_NEWEST_HEADER'                                   => 'Precis inkommit',
+    'START_TOP_PRODUCTS_HEADER'                             => 'Bästsäljare',
+    'START_BARGAIN_SUBHEADER'                               => 'Gör ett klipp med våra erbjudanden.',
+    'START_NEWEST_SUBHEADER'                                => 'Färskare blir det inte. ut ur paketet direkt till butiken.',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => 'Bara 2 produkter men dem bästa som vi kan erbjuda.',
+
+    // Contact form
+    'DD_CONTACT_PAGE_HEADING'                               => 'Kontakta oss!',
+    'DD_CONTACT_FORM_HEADING'                               => 'kontaktformulär',
+
+    // Link list
+    'DD_LINKS_NO_ENTRIES'                                   => 'Det finns tyvärr inga länkar än.',
+
+    // 404 page
+    'DD_ERR_404_START_TEXT'                                 => 'Kanske hittar du de önskade informationerna genom vår startsida:',
+    'DD_ERR_404_START_BUTTON'                               => 'Gå till startsidan',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Kan vi hjälpa dig?<br>Du kan gärna ringa oss eller skriva ett mejl:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'till kontaktformuläret',
+
+    // Login
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'Genom din registrering i vår web-butik är det smidigare att beställa. Dessutom kan du spara flera leveransadresser och du kan följa dina beställningar.',
+
+    // Billing address
+    'DD_USER_LABEL_STATE'                                    => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'välja',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'neue Adresse hinzufügen',
+    'DD_DELETE_SHIPPING_ADDRESS'                            => '',
+
+    // List views
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filter',
+    'DD_LIST_SHOW_MORE'                                     => 'titta på produkter...',
+
+    // Recommendation list
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'tillbaka till översikten',
+
+    // Downloads
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'ladda ner',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Filstorlek:',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'Klocka',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Filnamn:',
+
+    // Details page
+    'BACK_TO_OVERVIEW'                                      => 'till översikt',
+    'OF'                                                    => 'från',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Kundrecensioner',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'Andra kunder som köpte denna artikel, köpte även följande artiklar.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'Följande artiklar passar bra till den artikel.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'Kolla artiklar som är likvärdiga.',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'Kunder som kollade den artikel, kollade även dessa artiklar.',
+    'DETAILS_VPE_MESSAGE_1'                                 => "Dieser Artikel kann nur in Verpackungseinheiten zu je",
+    'DETAILS_VPE_MESSAGE_2'                                 => "",
+    'TO_CART_NOVARIANT'                                     => '',
+
+    // Modal basket
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Totalsumma',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'fortsätt handla',
+
+    // Checkout
+    'DD_BASKET_BACK_TO_SHOP'                                => 'tillbaka till butiken',
+
+    // E-Mails
+    'DD_FOOTER_FOLLOW_US'                                   => 'DD_FOOTER_FÖLJER_OSS',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'glömt lösenord',
+    'DD_INVITE_HEADING'                                     => 'Artikelrekommendation',
+    'DD_INVITE_LINK'                                        => 'länk',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Din nyhetsbrev-prenumeration',
+    'DD_ORDERSHIPPED_HEADING'                               => 'DD_LEVERANSSKICKAT_HEADING',
+    'DD_PRICEALARM_HEADING'                                 => 'Prislarm',
+    'DD_REGISTER_HEADING'                                   => 'Din registrering',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Din registrering',
+    'DD_WISHLIST_HEADING'                                   => 'Önskelista',
+
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Meny',
+
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
+    'DD_DELETE_MY_ACCOUNT'                                  => '',
+    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => '',
+    'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'avbryta',
+    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => '',
+    'DD_DELETE_MY_ACCOUNT_ERROR'                            => '',
+
+    // Account -> My product reviews
+    'DD_DELETE_REVIEW_AND_RATING'                           => '',
+    'DD_REVIEWS_NOT_AVAILABLE'                              => '',
+    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => '',
+
+    // Contact page
+    'DD_SELECT_SALUTATION'                                  => 'Var snäll och välj',
+
+    'DD_CATEGORY_RESET_BUTTON'                              => '',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE"}]
+*/
