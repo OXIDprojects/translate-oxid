01454e9163b5936ca31f7e7d46d148344b7d1221
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-15 14:10:42 +0200

    New translations lang.php (Romanian)

---
+++ source/Application/views/wave/ro-RO/lang.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This file is part of OXID eSales Wave theme.
+ *
+ * OXID eSales Wave theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Wave theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Wave theme.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+$sLangName = "Română";
+
+$aLang = array(
+    'charset'                                               => 'UTF-8',
+
+    // Global
+    'DD_SORT_DESC'                                          => 'descrescatoare',
+    'DD_SORT_ASC'                                           => 'crescator',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'Deschide interfata de admin',
+    'DD_DELETE'                                             => 'Sterge',
+
+    // Form validation
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Va rugam inserati o adresa de email valida.',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'Parola nu se potrivește .',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Va rugam inserati un numar.',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'Zecimalele nu sunt permise',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Va rugam inserati un numar pozitiv.',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Va rugam introduceti un numar negativ',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Va rugam specificati o valoare pt campul specificat',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Va rugam selectați măcar o opțiune .',
+
+    // Header
+    'SEARCH_TITLE'                                          => '',
+    'SEARCH_SUBMIT'                                         => '',
+    'NAV_MORE'                                              => '',
+
+    // Footer
+    'FOOTER_NEWSLETTER_INFO'                                => '',
+
+    // Home page
+    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
+    'START_BARGAIN_HEADER'                                  => '',
+    'START_NEWEST_HEADER'                                   => '',
+    'START_TOP_PRODUCTS_HEADER'                             => '',
+    'START_BARGAIN_SUBHEADER'                               => '',
+    'START_NEWEST_SUBHEADER'                                => '',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
+
+    // Contact form
+    'DD_CONTACT_PAGE_HEADING'                               => 'Contactaține !',
+    'DD_CONTACT_FORM_HEADING'                               => 'Contact',
+
+    // Link list
+    'DD_LINKS_NO_ENTRIES'                                   => 'Din pacate, nu sunt legaturi disponibile.',
+
+    // 404 page
+    'DD_ERR_404_START_TEXT'                                 => 'Puteti  sa gasiti informatii  de pe pagina noastra de acasa :',
+    'DD_ERR_404_START_BUTTON'                               => 'Mergi la pagina de start',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Va putem ajuta?<br>Sunati-ne sau scrieti-ne un email:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'spre pagina de contact',
+
+    // Login
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'Prin creerea unui cont in mgazinul nostru, vei fi gidat prin procesul de facturare mai rapid. Ca bonus, poti stoca mai multe adrese de livreare si poti urmari comenzile din contul tau.',
+
+    // Billing address
+    'DD_USER_LABEL_STATE'                                    => 'Judet',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'selectati',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'Adauga adresa de livrare',
+    'DD_DELETE_SHIPPING_ADDRESS'                            => '',
+
+    // List views
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filtru :',
+    'DD_LIST_SHOW_MORE'                                     => 'Vezi produse...',
+
+    // Recommendation list
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'Înapoi la produse',
+
+    // Downloads
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'descarca',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Marime fisier:',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'ceas',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Nume fisier',
+
+    // Details page
+    'BACK_TO_OVERVIEW'                                      => 'spre prezentarea generala',
+    'OF'                                                    => '',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'Parerile clientilor',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
+    'DETAILS_VPE_MESSAGE_1'                                 => "Acest produs poate fi comandat in cutii de",
+    'DETAILS_VPE_MESSAGE_2'                                 => "",
+    'TO_CART_NOVARIANT'                                     => '',
+
+    // Modal basket
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Total',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'continua cumparaturile',
+
+    // Checkout
+    'DD_BASKET_BACK_TO_SHOP'                                => 'Înapoi in shop',
+
+    // E-Mails
+    'DD_FOOTER_FOLLOW_US'                                   => 'Urmăriține :',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Am uitat parola',
+    'DD_INVITE_HEADING'                                     => 'Recomandare articol',
+    'DD_INVITE_LINK'                                        => 'Link',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'Subscriptia ta la Stiri',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Confirmare de livrare - Comanda',
+    'DD_PRICEALARM_HEADING'                                 => 'Alarma de pret',
+    'DD_REGISTER_HEADING'                                   => 'Inregistrarea dumneavoastra',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Linkurile dumneavoastra  de descarcare - comenzi',
+    'DD_WISHLIST_HEADING'                                   => 'Lista de dorinte',
+
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Meniu',
+
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
+    'DD_DELETE_MY_ACCOUNT'                                  => '',
+    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => '',
+    'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'Anulare',
+    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => '',
+    'DD_DELETE_MY_ACCOUNT_ERROR'                            => '',
+
+    // Account -> My product reviews
+    'DD_DELETE_REVIEW_AND_RATING'                           => '',
+    'DD_REVIEWS_NOT_AVAILABLE'                              => '',
+    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => '',
+
+    // Contact page
+    'DD_SELECT_SALUTATION'                                  => '',
+
+    'DD_CATEGORY_RESET_BUTTON'                              => 'Reset',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE"}]
+*/
