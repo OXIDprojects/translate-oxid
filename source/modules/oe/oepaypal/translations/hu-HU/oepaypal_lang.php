23479dc005262ac6530ce931f57e674d3383f76d
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-15 14:15:30 +0200

    New translations oepaypal_lang.php (Hungarian)

---
+++ source/modules/oe/oepaypal/translations/hu-HU/oepaypal_lang.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * This file is part of OXID eSales PayPal module.
+ *
+ * OXID eSales PayPal module is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales PayPal module is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales PayPal module.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2018
+ */
+
+// -------------------------------
+// RESOURCE IDENTIFIER = STRING
+// -------------------------------
+$aLang = array(
+    'charset'                                    => 'ISO-8859-2',
+    'OEPAYPAL_LOCALE'                            => 'hu_HU',
+    'OEPAYPAL_PARTNERBOX_LINK'                   => 'https://www.paypal.com/hu/cgi-bin/webscr?cmd=xpt/cps/popup/OLCWhatIsPayPal-outside',
+    'OEPAYPAL_ERROR_USER_ADDRESS'                => 'Az ',
+    'OEPAYPAL_LOGO_IMG'                          => 'logo-paypal-medium.png',
+    'OEPAYPAL_PAYMENT_HELP_LINK'                 => 'https://www.paypal.com/hu/webapps/mpp/paypal-popup',
+    'OEPAYPAL_PAYMENT_HELP_LINK_TEXT'            => 'Mi a PayPal?',
+    'OEPAYPAL_PAYMENT_NOSHIPPINGFOUND'           => 'Sz',
+    'OEPAYPAL_PAYMENT_NOT_VALID'                 => 'Sajnos ez a term',
+    'OEPAYPAL_ORDER_SUBJECT'                     => 'Az ',
+    'OEPAYPAL_ORDER_CONF_SUBJECT'                => 'Megrendel',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL'          => '\'A kos',
+    'OEPAYPAL_DISPLAY_BASKET_IN_PAYPAL_HELPTEXT' => 'Az akut',
+    'OEPAYPAL_PRICE'                             => '',
+    'OEPAYPAL_REQUIRED_FIELDS_EMPTY'             => 'K',
+    'OEPAYPAL_ORDER_ERROR'                       => 'Hiba a megrendel',
+    'OEPAYPAL_RESPONSE_FROM_PAYPAL'              => 'PayPal hibajelent',
+    'OEPAYPAL_ORDER_TOTAL_HAS_CHANGED'           => 'Az ',
+    'OEPAYPAL_SURCHARGE'                         => 'P',
+    'OEPAYPAL_TYPE_OF_PAYMENT'                   => '\'Fizet',
+    'OEPAYPAL_GIFTWRAPPER'                       => '\'D',
+    'OEPAYPAL_GREETING_CARD'                     => '',
+    'OEPAYPAL_GRAND_TOTAL'                       => '',
+    'OEPAYPAL_INFORMATION'                       => '\'PayPal-Inform',
+    'OEPAYPAL_SAME_ITEM_QUESTION'                => 'Ez a term',
+    'OEPAYPAL_SAME_ITEMS_QUESTION'               => 'Ez a term',
+    'OEPAYPAL_BUTTON_DO_NOT_ADD_ITEM'            => 'Nem ad hozz',
+    'OEPAYPAL_BUTTON_ADD_ITEM'                   => 'Hozz',
+    'OEPAYPAL_MINIBASKET_DISPLAY_BASKET'         => 'Az aktu',
+    'OEPAYPAL_CANCEL'                            => '\'Megszak',
+    'OEPAYPAL_SELECT_ANOTHER_SHIPMENT'           => 'Az ',
+    'OEPAYPAL_OR'                                => 'vagy',
+    'OEPAYPAL_BACK'                              => 'vissza',
+    'OEPAYPAL_PLUS_VAT'                          => 'plusz ',
+    'OEPAYPAL_PAYMENT_INFORMATION'               => "\"Fizet",
+);
