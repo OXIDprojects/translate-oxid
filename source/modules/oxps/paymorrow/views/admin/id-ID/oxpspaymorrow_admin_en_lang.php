02def76511ccdb0d6211f8b9cd6008c1b7191630
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-15 14:11:49 +0200

    New translations oxpspaymorrow_admin_en_lang.php (Indonesian)

---
+++ source/modules/oxps/paymorrow/views/admin/id-ID/oxpspaymorrow_admin_en_lang.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * This file is part of the OXID module for Paymorrow payment.
+ *
+ * The OXID module for Paymorrow payment is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * The OXID eShop module for Paymorrow payment is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * Linking this library statically or dynamically with other modules is making a
+ * combined work based on this library. Thus, the terms and conditions of the GNU
+ * General Public License cover the whole combination.
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules, and
+ * to copy and distribute the resulting executable under terms of your choice,
+ * provided that you also meet, for each linked independent module, the terms and
+ * conditions of the license of that module. An independent module is a module
+ * which is not derived from or based on this library. If you modify this library,
+ * you may extend this exception to your version of the library, but you are not
+ * obliged to do so. If you do not wish to do so, delete this exception statement
+ * from your version.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * the OXID module for Paymorrow payment. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$sLangName = "Indonesia";
+
+$aLang = array(
+    "charset"                                      => "UTF-8",
+    "oxpspaymorrow"                                => "",
+
+    'NAVIGATION_PAYMORROW'                         => '',
+    'SHOP_MODULE_GROUP_oxpsPaymorrowConfiguration' => '',
+    'SHOP_MODULE_GROUP_oxpsPaymorrowProfileUpdate' => '',
+    'OXPSPAYMORROW_PAYMENT_TYPE_INVOICE'           => '',
+    'OXPSPAYMORROW_PAYMENT_TYPE_DIRECT_DEBIT'      => '',
+    'oxpspaymorrow_form_error_log'                 => '',
+    'oxpspaymorrow_paymorrow_info'                 => '',
+    'oxpspaymorrow_payment_map'                    => '',
+
+    // Main Menu Settings
+    'OXPSPAYMORROW_MAIN_MENU_SETTINGS_TITLE'       => '',
+    'SHOP_MODULE_paymorrowSandboxMode'             => '',
+    'SHOP_MODULE_paymorrowMerchantId'              => '',
+    'SHOP_MODULE_paymorrowMerchantIdTest'          => '',
+    'SHOP_MODULE_paymorrowEndpointUrlTest'         => '',
+    'SHOP_MODULE_paymorrowEndpointUrlProd'         => '',
+    'SHOP_MODULE_paymorrowLoggingEnabled'          => '',
+    'SHOP_MODULE_paymorrowResourcePath'            => '',
+    'SHOP_MODULE_paymorrowResourcePathTest'        => '',
+    'SHOP_MODULE_paymorrowOperationMode'           => '',
+    'SHOP_MODULE_paymorrowOperationModeTest'       => '',
+
+    // RSA Keys fields
+    'SHOP_MODULE_paymorrowKeysJson'                => '',
+    'SHOP_MODULE_paymorrowPrivateKey'              => '',
+    'SHOP_MODULE_paymorrowPrivateKeyTest'          => '',
+    'SHOP_MODULE_paymorrowPublicKey'               => '',
+    'SHOP_MODULE_paymorrowPublicKeyTest'           => '',
+    'SHOP_MODULE_paymorrowPaymorrowKey'            => '',
+    'SHOP_MODULE_paymorrowPaymorrowKeyTest'        => '',
+
+    // Profile data normalization settings
+    'SHOP_MODULE_paymorrowUpdateAddresses'         => '',
+    'SHOP_MODULE_paymorrowUpdatePhones'            => '',
+
+    // Help Idents
+    'PM_HELP_ADMIN_PAYMENT_METHODS_ACTIVATE'       => '',
+    'PM_HELP_ADMIN_PAYMENT_METHODS_INVOICE'        => '',
+    'PM_HELP_ADMIN_PAYMENT_METHODS_SDD'            => '',
+);
