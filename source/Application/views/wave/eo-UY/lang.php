7590be6b1f4afa74fbca0110535c1d9084047259
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-15 16:20:09 +0200

    New translations lang.php (Esperanto)

---
+++ source/Application/views/wave/eo-UY/lang.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This file is part of OXID eSales Wave theme.
+ *
+ * OXID eSales Wave theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Wave theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Wave theme.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+$sLangName = "Esperanto";
+
+$aLang = array(
+    'charset'                                               => 'UTF-8',
+
+    // Global
+    'DD_SORT_DESC'                                          => '',
+    'DD_SORT_ASC'                                           => '',
+    'DD_DEMO_ADMIN_TOOL'                                    => '',
+    'DD_DELETE'                                             => '',
+
+    // Form validation
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'Bv enigu validan retpoŝadreson',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => '',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'Bv enigu nombron',
+    'DD_FORM_VALIDATION_INTEGER'                            => '',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'Bv enigu pozitivan nombron',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'Bv enigu negativan nombron',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'Bv enigu valoron',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'Bv elektu minimume unu eblon',
+
+    // Header
+    'SEARCH_TITLE'                                          => '',
+    'SEARCH_SUBMIT'                                         => '',
+    'NAV_MORE'                                              => '',
+
+    // Footer
+    'FOOTER_NEWSLETTER_INFO'                                => '',
+
+    // Home page
+    'MANUFACTURERSLIDER_SUBHEAD'                            => '',
+    'START_BARGAIN_HEADER'                                  => '',
+    'START_NEWEST_HEADER'                                   => '',
+    'START_TOP_PRODUCTS_HEADER'                             => '',
+    'START_BARGAIN_SUBHEADER'                               => '',
+    'START_NEWEST_SUBHEADER'                                => '',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => '',
+
+    // Contact form
+    'DD_CONTACT_PAGE_HEADING'                               => 'Kontaktu nin!',
+    'DD_CONTACT_FORM_HEADING'                               => 'Kontaktformularo',
+
+    // Link list
+    'DD_LINKS_NO_ENTRIES'                                   => '',
+
+    // 404 page
+    'DD_ERR_404_START_TEXT'                                 => '',
+    'DD_ERR_404_START_BUTTON'                               => 'Iru al hejmpaĝo',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'Ĉu ni povas helpi vin?<br>Bv telefonu al ni aŭ sendu retmesaĝon:',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'al la kontaktpaĝo',
+
+    // Login
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => '',
+
+    // Billing address
+    'DD_USER_LABEL_STATE'                                    => '',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'elekti',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'aldoni liveradreson',
+    'DD_DELETE_SHIPPING_ADDRESS'                            => '',
+
+    // List views
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'Filtrilo:',
+    'DD_LIST_SHOW_MORE'                                     => 'Rigardu artiklojn',
+
+    // Recommendation list
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'reen al la superrigardo',
+
+    // Downloads
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'elŝuti',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'Dosiergrando:',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'Horo',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'Dosiernomo:',
+
+    // Details page
+    'BACK_TO_OVERVIEW'                                      => 'Al superrigardo',
+    'OF'                                                    => '',
+    'DD_RATING_CUSTOMERRATINGS'                             => '',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => '',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => '',
+    'DETAILS_VPE_MESSAGE_1'                                 => "",
+    'DETAILS_VPE_MESSAGE_2'                                 => "",
+    'TO_CART_NOVARIANT'                                     => '',
+
+    // Modal basket
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'Entuta sumo',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'Kontinuigi butikumi',
+
+    // Checkout
+    'DD_BASKET_BACK_TO_SHOP'                                => 'reen al la vendejo',
+
+    // E-Mails
+    'DD_FOOTER_FOLLOW_US'                                   => 'Sekvu nin:',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'Mi forgesis la pasvorton',
+    'DD_INVITE_HEADING'                                     => '',
+    'DD_INVITE_LINK'                                        => 'Ligilo',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => '',
+    'DD_ORDERSHIPPED_HEADING'                               => 'Sendkonfirmo - mendo',
+    'DD_PRICEALARM_HEADING'                                 => '',
+    'DD_REGISTER_HEADING'                                   => 'Via regsitrado',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'Viaj elŝutligiloj - mendoj',
+    'DD_WISHLIST_HEADING'                                   => '',
+
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Menuo',
+
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
+    'DD_DELETE_MY_ACCOUNT'                                  => '',
+    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => '',
+    'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'Nuligi',
+    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => '',
+    'DD_DELETE_MY_ACCOUNT_ERROR'                            => '',
+
+    // Account -> My product reviews
+    'DD_DELETE_REVIEW_AND_RATING'                           => '',
+    'DD_REVIEWS_NOT_AVAILABLE'                              => '',
+    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => '',
+
+    // Contact page
+    'DD_SELECT_SALUTATION'                                  => '',
+
+    'DD_CATEGORY_RESET_BUTTON'                              => '',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE"}]
+*/
