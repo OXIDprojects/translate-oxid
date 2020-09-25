96bc0c2f5ced9e531f565848d6ffc4b08705aff9
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-25 11:33:11 +0200

    New translations lang.php (Acholi)

---
+++ source/Application/views/wave/ach-UG/lang.php
@@ -0,0 +1,148 @@
+<?php
+/**
+ * This file is part of OXID eSales Wave theme.
+ *
+ * OXID eSales Wave theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Wave theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Wave theme.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+$sLangName = "crwdns6052:0crwdne6052:0";
+
+$aLang = array(
+    'charset'                                               => 'crwdns6054:0crwdne6054:0',
+
+    // Global
+    'DD_SORT_DESC'                                          => 'crwdns6056:0crwdne6056:0',
+    'DD_SORT_ASC'                                           => 'crwdns6058:0crwdne6058:0',
+    'DD_DEMO_ADMIN_TOOL'                                    => 'crwdns6060:0crwdne6060:0',
+    'DD_DELETE'                                             => 'crwdns6062:0crwdne6062:0',
+
+    // Form validation
+    'DD_FORM_VALIDATION_VALIDEMAIL'                         => 'crwdns6064:0crwdne6064:0',
+    'DD_FORM_VALIDATION_PASSWORDAGAIN'                      => 'crwdns6066:0crwdne6066:0',
+    'DD_FORM_VALIDATION_NUMBER'                             => 'crwdns6068:0crwdne6068:0',
+    'DD_FORM_VALIDATION_INTEGER'                            => 'crwdns6070:0crwdne6070:0',
+    'DD_FORM_VALIDATION_POSITIVENUMBER'                     => 'crwdns6072:0crwdne6072:0',
+    'DD_FORM_VALIDATION_NEGATIVENUMBER'                     => 'crwdns6074:0crwdne6074:0',
+    'DD_FORM_VALIDATION_REQUIRED'                           => 'crwdns6076:0crwdne6076:0',
+    'DD_FORM_VALIDATION_CHECKONE'                           => 'crwdns6078:0crwdne6078:0',
+
+    // Header
+    'SEARCH_TITLE'                                          => 'crwdns6080:0crwdne6080:0',
+    'SEARCH_SUBMIT'                                         => 'crwdns6082:0crwdne6082:0',
+    'NAV_MORE'                                              => 'crwdns6084:0crwdne6084:0',
+
+    // Footer
+    'FOOTER_NEWSLETTER_INFO'                                => 'crwdns6086:0crwdne6086:0',
+
+    // Home page
+    'MANUFACTURERSLIDER_SUBHEAD'                            => 'crwdns6088:0crwdne6088:0',
+    'START_BARGAIN_HEADER'                                  => 'crwdns6090:0crwdne6090:0',
+    'START_NEWEST_HEADER'                                   => 'crwdns6092:0crwdne6092:0',
+    'START_TOP_PRODUCTS_HEADER'                             => 'crwdns6094:0crwdne6094:0',
+    'START_BARGAIN_SUBHEADER'                               => 'crwdns6096:0crwdne6096:0',
+    'START_NEWEST_SUBHEADER'                                => 'crwdns6098:0crwdne6098:0',
+    'START_TOP_PRODUCTS_SUBHEADER'                          => 'crwdns6100:0%scrwdne6100:0',
+
+    // Contact form
+    'DD_CONTACT_PAGE_HEADING'                               => 'crwdns6102:0crwdne6102:0',
+    'DD_CONTACT_FORM_HEADING'                               => 'crwdns6104:0crwdne6104:0',
+
+    // Link list
+    'DD_LINKS_NO_ENTRIES'                                   => 'crwdns6106:0crwdne6106:0',
+
+    // 404 page
+    'DD_ERR_404_START_TEXT'                                 => 'crwdns6108:0crwdne6108:0',
+    'DD_ERR_404_START_BUTTON'                               => 'crwdns6110:0crwdne6110:0',
+    'DD_ERR_404_CONTACT_TEXT'                               => 'crwdns6112:0crwdne6112:0',
+    'DD_ERR_404_CONTACT_BUTTON'                             => 'crwdns6114:0crwdne6114:0',
+
+    // Login
+    'DD_LOGIN_ACCOUNT_PANEL_CREATE_BODY'                    => 'crwdns6116:0crwdne6116:0',
+
+    // Billing address
+    'DD_USER_LABEL_STATE'                                    => 'crwdns6118:0crwdne6118:0',
+    'DD_USER_SHIPPING_SELECT_ADDRESS'                       => 'crwdns6120:0crwdne6120:0',
+    'DD_USER_SHIPPING_ADD_DELIVERY_ADDRESS'                 => 'crwdns6122:0crwdne6122:0',
+    'DD_DELETE_SHIPPING_ADDRESS'                            => 'crwdns6124:0crwdne6124:0',
+
+    // List views
+    'DD_LISTLOCATOR_FILTER_ATTRIBUTES'                      => 'crwdns6126:0crwdne6126:0',
+    'DD_LIST_SHOW_MORE'                                     => 'crwdns6128:0crwdne6128:0',
+
+    // Recommendation list
+    'DD_RECOMMENDATION_EDIT_BACK_TO_LIST'                   => 'crwdns6130:0crwdne6130:0',
+
+    // Downloads
+    'DD_DOWNLOADS_DOWNLOAD_TOOLTIP'                         => 'crwdns6132:0crwdne6132:0',
+    'DD_FILE_ATTRIBUTES_FILESIZE'                           => 'crwdns6134:0crwdne6134:0',
+    'DD_FILE_ATTRIBUTES_OCLOCK'                             => 'crwdns6136:0crwdne6136:0',
+    'DD_FILE_ATTRIBUTES_FILENAME'                           => 'crwdns6138:0crwdne6138:0',
+
+    // Details page
+    'BACK_TO_OVERVIEW'                                      => 'crwdns6140:0crwdne6140:0',
+    'OF'                                                    => 'crwdns6142:0crwdne6142:0',
+    'DD_RATING_CUSTOMERRATINGS'                             => 'crwdns6144:0crwdne6144:0',
+    'PAGE_DETAILS_CUSTOMERS_ALSO_BOUGHT_SUBHEADER'          => 'crwdns6146:0crwdne6146:0',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_ACCESSORIES_SUBHEADER' => 'crwdns6148:0crwdne6148:0',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_SIMILAR_SUBHEADER'     => 'crwdns6150:0crwdne6150:0',
+    'WIDGET_PRODUCT_RELATED_PRODUCTS_CROSSSELING_SUBHEADER' => 'crwdns6152:0crwdne6152:0',
+    'DETAILS_VPE_MESSAGE_1'                                 => "crwdns6154:0crwdne6154:0",
+    'DETAILS_VPE_MESSAGE_2'                                 => "crwdns6156:0crwdne6156:0",
+    'TO_CART_NOVARIANT'                                     => 'crwdns6158:0crwdne6158:0',
+
+    // Modal basket
+    'DD_MINIBASKET_MODAL_TABLE_PRICE'                       => 'crwdns6160:0crwdne6160:0',
+    'DD_MINIBASKET_CONTINUE_SHOPPING'                       => 'crwdns6162:0crwdne6162:0',
+
+    // Checkout
+    'DD_BASKET_BACK_TO_SHOP'                                => 'crwdns6164:0crwdne6164:0',
+
+    // E-Mails
+    'DD_FOOTER_FOLLOW_US'                                   => 'crwdns6166:0crwdne6166:0',
+    'DD_FORGOT_PASSWORD_HEADING'                            => 'crwdns6168:0crwdne6168:0',
+    'DD_INVITE_HEADING'                                     => 'crwdns6170:0crwdne6170:0',
+    'DD_INVITE_LINK'                                        => 'crwdns6172:0crwdne6172:0',
+    'DD_NEWSLETTER_OPTIN_HEADING'                           => 'crwdns6174:0crwdne6174:0',
+    'DD_ORDERSHIPPED_HEADING'                               => 'crwdns6176:0crwdne6176:0',
+    'DD_PRICEALARM_HEADING'                                 => 'crwdns6178:0crwdne6178:0',
+    'DD_REGISTER_HEADING'                                   => 'crwdns6180:0crwdne6180:0',
+    'DD_DOWNLOADLINKS_HEADING'                              => 'crwdns6182:0crwdne6182:0',
+    'DD_WISHLIST_HEADING'                                   => 'crwdns6184:0crwdne6184:0',
+
+    'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'crwdns6186:0crwdne6186:0',
+
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => 'crwdns6188:0crwdne6188:0',
+    'DD_DELETE_MY_ACCOUNT'                                  => 'crwdns6190:0crwdne6190:0',
+    'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => 'crwdns6192:0crwdne6192:0',
+    'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'crwdns6194:0crwdne6194:0',
+    'DD_DELETE_MY_ACCOUNT_SUCCESS'                          => 'crwdns6196:0crwdne6196:0',
+    'DD_DELETE_MY_ACCOUNT_ERROR'                            => 'crwdns6198:0crwdne6198:0',
+
+    // Account -> My product reviews
+    'DD_DELETE_REVIEW_AND_RATING'                           => 'crwdns6200:0crwdne6200:0',
+    'DD_REVIEWS_NOT_AVAILABLE'                              => 'crwdns6202:0crwdne6202:0',
+    'DD_DELETE_REVIEW_CONFIRMATION_QUESTION'                => 'crwdns6204:0crwdne6204:0',
+
+    // Contact page
+    'DD_SELECT_SALUTATION'                                  => 'crwdns6206:0crwdne6206:0',
+
+    'DD_CATEGORY_RESET_BUTTON'                              => 'crwdns6208:0crwdne6208:0',
+);
+
+/*
+[{ oxmultilang ident="GENERAL_YOUWANTTODELETE"}]
+*/
