b0da8b5277eb7aec248ced383f1e23d68911647a
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-25 11:33:16 +0200

    New translations theme_options.php (Acholi)

---
+++ source/Application/views/wave/ach-UG/theme_options.php
@@ -0,0 +1,208 @@
+<?php
+/**
+ * This file is part of OXID eSales Wave theme.
+ *
+ * OXID eSales Wave theme is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OXID eSales Wave theme is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OXID eSales Wave theme.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @link      http://www.oxid-esales.com
+ * @copyright (C) OXID eSales AG 2003-2016
+ */
+
+$sLangName  = 'crwdns7818:0crwdne7818:0';
+
+$aLang = array(
+    'charset'                                       => 'crwdns7820:0crwdne7820:0',
+
+    'SHOP_THEME_GROUP_images'                       => 'crwdns7822:0crwdne7822:0',
+    'SHOP_THEME_GROUP_features'                     => 'crwdns7824:0crwdne7824:0',
+    'SHOP_THEME_GROUP_display'                      => 'crwdns7826:0crwdne7826:0',
+    'SHOP_THEME_GROUP_logo'                         => 'crwdns7828:0crwdne7828:0',
+    'SHOP_THEME_GROUP_favicons'                     => 'crwdns7830:0crwdne7830:0',
+    'SHOP_THEME_GROUP_footer'                       => 'crwdns7832:0crwdne7832:0',
+    'SHOP_THEME_GROUP_header'                       => 'crwdns7834:0crwdne7834:0',
+    'SHOP_THEME_GROUP_contact'                      => 'crwdns7836:0crwdne7836:0',
+    'SHOP_THEME_GROUP_googleanalytics'              => 'crwdns7838:0crwdne7838:0',
+    'SHOP_THEME_GROUP_googlets'                     => 'crwdns7840:0crwdne7840:0',
+    'SHOP_THEME_GROUP_background'                   => 'crwdns7842:0crwdne7842:0',
+    'SHOP_THEME_GROUP_econda'                       => 'crwdns7844:0crwdne7844:0',
+    'SHOP_THEME_GROUP_emails'                       => 'crwdns7846:0crwdne7846:0',
+
+    'SHOP_THEME_sIconsize'                          => 'crwdns7848:0crwdne7848:0', //SHOP_CONFIG_ICONSIZE
+    'HELP_SHOP_THEME_sIconsize'                     => 'crwdns7850:0crwdne7850:0' .
+                                                       'crwdns7852:0crwdne7852:0' .
+                                                       'crwdns7854:0crwdne7854:0' .
+                                                       'crwdns7856:0crwdne7856:0',
+
+    'SHOP_THEME_sThumbnailsize'                     => 'crwdns7858:0crwdne7858:0', //SHOP_CONFIG_THUMBNAILSIZE
+    'HELP_SHOP_THEME_sThumbnailsize'                => 'crwdns7860:0crwdne7860:0' .
+                                                       'crwdns7862:0crwdne7862:0' .
+                                                       'crwdns7864:0crwdne7864:0' .
+                                                       'crwdns7866:0crwdne7866:0',
+
+    'SHOP_THEME_sZoomImageSize'                     => 'crwdns7868:0crwdne7868:0', //SHOP_CONFIG_ZOOMIMAGESIZE
+    'SHOP_THEME_sCatThumbnailsize'                  => 'crwdns7870:0crwdne7870:0', //SHOP_CONFIG_CATEGORYTHUMBNAILSIZE
+    'HELP_SHOP_THEME_sCatThumbnailsize'             => 'crwdns7872:0crwdne7872:0', //SHOP_CONFIG_CATEGORYTHUMBNAILSIZE
+    'SHOP_THEME_aDetailImageSizes'                  => 'crwdns7874:0crwdne7874:0', //SHOP_CONFIG_DETAILIMAGESIZE
+
+    'SHOP_THEME_sManufacturerIconsize'              => 'crwdns7876:0crwdne7876:0', // Check if this is really manufacturer or if it is more like "brand"
+    'HELP_SHOP_THEME_sManufacturerIconsize'         => 'crwdns7878:0crwdne7878:0',
+
+    'SHOP_THEME_sCatIconsize'                       => 'crwdns7880:0crwdne7880:0',
+    'HELP_SHOP_THEME_sCatIconsize'                  => 'crwdns7882:0crwdne7882:0',
+
+    'SHOP_THEME_sCatPromotionsize'                  => 'crwdns7884:0crwdne7884:0',
+    'HELP_SHOP_THEME_sCatPromotionsize'             => 'crwdns7886:0crwdne7886:0',
+
+    'SHOP_THEME_blFooterShowHelp'                   => 'crwdns7888:0crwdne7888:0',
+    'SHOP_THEME_blFooterShowLinks'                  => 'crwdns7890:0crwdne7890:0',
+    'SHOP_THEME_blFooterShowNewsletter'             => 'crwdns7892:0crwdne7892:0',
+    'SHOP_THEME_blFooterShowNewsletterForm'         => 'crwdns7894:0crwdne7894:0',
+    'SHOP_THEME_blFooterShowNews'                   => 'crwdns7896:0crwdne7896:0',
+    'SHOP_THEME_bl_showGiftWrapping'                => 'crwdns7898:0crwdne7898:0', //SHOP_CONFIG_SHOWGIFTWRAPPING
+    'SHOP_THEME_bl_showVouchers'                    => 'crwdns7900:0crwdne7900:0', //SHOP_CONFIG_SHOWVOUCHERS
+    'SHOP_THEME_bl_showWishlist'                    => 'crwdns7902:0crwdne7902:0', //SHOP_CONFIG_SHOWWISHLIST
+    'SHOP_THEME_bl_showCompareList'                 => 'crwdns7904:0crwdne7904:0', //SHOP_CONFIG_SHOWCOMPARELIST
+    'SHOP_THEME_bl_showListmania'                   => 'crwdns7906:0crwdne7906:0', //SHOP_CONFIG_SHOWLISTMANIA
+    'SHOP_THEME_blShowBirthdayFields'               => 'crwdns7908:0crwdne7908:0', //SHOP_CONFIG_SHOWBIRTHDAYFIELDS
+
+    'SHOP_THEME_iTopNaviCatCount'                   => 'crwdns7910:0crwdne7910:0', //SHOP_CONFIG_TOPNAVICATCOUNT
+    'SHOP_THEME_blShowFinalStep'                    => 'crwdns7912:0crwdne7912:0', //SHOP_SYSTEM_SHOWFINALSTEP
+    'SHOP_THEME_iNewBasketItemMessage'              => 'crwdns7914:0crwdne7914:0', //SHOP_SYSTEM_SHOWNEWBASKETITEMMESSAGE
+    'HELP_SHOP_THEME_iNewBasketItemMessage'         => 'crwdns7916:0crwdne7916:0', //SHOP_SYSTEM_SHOWNEWBASKETITEMMESSAGE
+    'SHOP_THEME_iNewBasketItemMessage_0'            => 'crwdns7918:0crwdne7918:0',
+    'SHOP_THEME_iNewBasketItemMessage_1'            => 'crwdns7920:0crwdne7920:0',
+    'SHOP_THEME_iNewBasketItemMessage_2'            => 'crwdns7922:0crwdne7922:0',
+    'SHOP_THEME_iNewBasketItemMessage_3'            => 'crwdns7924:0crwdne7924:0',
+
+    'SHOP_THEME_blShowListDisplayType'              => 'crwdns7926:0crwdne7926:0',
+    'HELP_SHOP_THEME_blShowListDisplayType'         => 'crwdns7928:0crwdne7928:0',
+    'SHOP_THEME_sDefaultListDisplayType'            => 'crwdns7930:0crwdne7930:0',
+    'SHOP_THEME_sDefaultListDisplayType_grid'       => 'crwdns7932:0crwdne7932:0',
+    'SHOP_THEME_sDefaultListDisplayType_line'       => 'crwdns7934:0crwdne7934:0',
+    'SHOP_THEME_sDefaultListDisplayType_infogrid'   => 'crwdns7936:0crwdne7936:0',
+    'SHOP_THEME_sStartPageListDisplayType'          => 'crwdns7938:0crwdne7938:0',
+    'SHOP_THEME_sStartPageListDisplayType_grid'     => 'crwdns7940:0crwdne7940:0',
+    'SHOP_THEME_sStartPageListDisplayType_line'     => 'crwdns7942:0crwdne7942:0',
+    'SHOP_THEME_sStartPageListDisplayType_infogrid' => 'crwdns7944:0crwdne7944:0',
+
+    'SHOP_THEME_aNrofCatArticlesInGrid'             => 'crwdns7946:0crwdne7946:0',
+    'SHOP_THEME_aNrofCatArticles'                   => 'crwdns7948:0crwdne7948:0',
+
+    'SHOP_THEME_sFacebookUrl'                       => 'crwdns7950:0crwdne7950:0',
+    'HELP_SHOP_THEME_sFacebookUrl'                  => 'crwdns7952:0crwdne7952:0',
+    'SHOP_THEME_sTwitterUrl'                        => 'crwdns7954:0crwdne7954:0',
+    'HELP_SHOP_THEME_sTwitterUrl'                   => 'crwdns7956:0crwdne7956:0',
+    'SHOP_THEME_sBlogUrl'                           => 'crwdns7958:0crwdne7958:0',
+    'HELP_SHOP_THEME_sBlogUrl'                      => 'crwdns7960:0crwdne7960:0',
+    'SHOP_THEME_sYouTubeUrl'                        => 'crwdns7962:0crwdne7962:0',
+    'HELP_SHOP_THEME_sYouTubeUrl'                   => 'crwdns7964:0crwdne7964:0',
+    'SHOP_THEME_sGooglePlusUrl'                     => 'crwdns7966:0crwdne7966:0',
+    'HELP_SHOP_THEME_sGooglePlusUrl'                => 'crwdns7968:0crwdne7968:0',
+
+    'SHOP_THEME_sGoogleMapsAddr'                    => 'crwdns7970:0crwdne7970:0',
+    'HELP_SHOP_THEME_sGoogleMapsAddr'               => 'crwdns7972:0crwdne7972:0',
+
+    'SHOP_THEME_bl_showManufacturerSlider'          => 'crwdns7974:0crwdne7974:0',
+
+    'SHOP_THEME_blUseGAPageTracker'                 => 'crwdns7976:0crwdne7976:0',
+    'SHOP_THEME_blUseGAEcommerceTracking'           => 'crwdns7978:0crwdne7978:0',
+    'HELP_SHOP_THEME_blUseGAEcommerceTracking'      => 'crwdns7980:0crwdne7980:0',
+    'SHOP_THEME_sGATrackingId'                      => 'crwdns7982:0crwdne7982:0',
+    'HELP_SHOP_THEME_sGATrackingId'                 => 'crwdns7984:0crwdne7984:0',
+    'SHOP_THEME_blGAAnonymizeIPs'                   => 'crwdns7986:0crwdne7986:0',
+
+    'SHOP_THEME_sLogoFile'                          => 'crwdns7988:0crwdne7988:0',
+    'HELP_SHOP_THEME_sLogoFile'                     => 'crwdns7990:0crwdne7990:0',
+
+    'SHOP_THEME_sLogoWidth'                         => 'crwdns7992:0crwdne7992:0',
+    'HELP_SHOP_THEME_sLogoWidth'                    => 'crwdns7994:0crwdne7994:0',
+    'SHOP_THEME_sLogoHeight'                        => 'crwdns7996:0crwdne7996:0',
+    'HELP_SHOP_THEME_sLogoHeight'                   => 'crwdns7998:0crwdne7998:0',
+
+    'SHOP_THEME_sEmailLogo'                         => 'crwdns8000:0crwdne8000:0',
+    'HELP_SHOP_THEME_sEmailLogo'                    => 'crwdns8002:0crwdne8002:0',
+
+    'SHOP_THEME_sFaviconFile'                       => 'crwdns8004:0crwdne8004:0',
+    'HELP_SHOP_THEME_sFaviconFile'                  => 'crwdns8006:0crwdne8006:0',
+    'SHOP_THEME_sFavicon16File'                     => 'crwdns8008:0crwdne8008:0',
+    'SHOP_THEME_sFavicon32File'                     => 'crwdns8010:0crwdne8010:0',
+    'SHOP_THEME_sFavicon48File'                     => 'crwdns8012:0crwdne8012:0',
+    'SHOP_THEME_sFavicon64File'                     => 'crwdns8014:0crwdne8014:0',
+    'SHOP_THEME_sFavicon128File'                    => 'crwdns8016:0crwdne8016:0',
+    'SHOP_THEME_sFavicon512File'                    => 'crwdns8018:0crwdne8018:0',
+    'SHOP_THEME_sFaviconMSTileColor'                => 'crwdns8020:0crwdne8020:0',
+    'HELP_SHOP_THEME_sFaviconMSTileColor'           => 'crwdns8022:0crwdne8022:0',
+
+    'SHOP_THEME_blUseBackground'                    => 'crwdns8024:0crwdne8024:0',
+    'SHOP_THEME_sBackgroundColor'                   => 'crwdns8026:0crwdne8026:0',
+    'HELP_SHOP_THEME_sBackgroundColor'              => 'crwdns8028:0crwdne8028:0',
+    'SHOP_THEME_sBackgroundPath'                    => 'crwdns8030:0crwdne8030:0',
+    'HELP_SHOP_THEME_sBackgroundPath'               => 'crwdns8032:0crwdne8032:0',
+    'SHOP_THEME_sBackgroundRepeat'                  => 'crwdns8034:0crwdne8034:0',
+    'SHOP_THEME_sBackgroundRepeat_no-repeat'        => 'crwdns8036:0crwdne8036:0',
+    'SHOP_THEME_sBackgroundRepeat_repeat-x'         => 'crwdns8038:0crwdne8038:0',
+    'SHOP_THEME_sBackgroundRepeat_repeat-y'         => 'crwdns8040:0crwdne8040:0',
+    'SHOP_THEME_sBackgroundRepeat_repeat'           => 'crwdns8042:0crwdne8042:0',
+    'SHOP_THEME_sBackgroundPosHorizontal'           => 'crwdns8044:0crwdne8044:0',
+    'SHOP_THEME_sBackgroundPosHorizontal_left'      => 'crwdns8046:0crwdne8046:0',
+    'SHOP_THEME_sBackgroundPosHorizontal_right'     => 'crwdns8048:0crwdne8048:0',
+    'SHOP_THEME_sBackgroundPosHorizontal_center'    => 'crwdns8050:0crwdne8050:0',
+    'SHOP_THEME_sBackgroundPosVertical'             => 'crwdns8052:0crwdne8052:0',
+    'SHOP_THEME_sBackgroundPosVertical_top'         => 'crwdns8054:0crwdne8054:0',
+    'SHOP_THEME_sBackgroundPosVertical_bottom'      => 'crwdns8056:0crwdne8056:0',
+    'SHOP_THEME_sBackgroundPosVertical_center'      => 'crwdns8058:0crwdne8058:0',
+    'SHOP_THEME_sBackgroundSize'                    => 'crwdns8060:0crwdne8060:0',
+    'SHOP_THEME_sBackgroundSize_cover'              => 'crwdns8062:0crwdne8062:0',
+    'SHOP_THEME_sBackgroundSize_contain'            => 'crwdns8064:0crwdne8064:0',
+    'SHOP_THEME_sBackgroundSize_normal'             => 'crwdns8066:0crwdne8066:0',
+    'SHOP_THEME_blBackgroundAttachment'             => 'crwdns8068:0crwdne8068:0',
+    'SHOP_THEME_blHomeLink'                         => 'crwdns8070:0crwdne8070:0',
+    'SHOP_THEME_bl_showPriceAlarm'                  => 'crwdns8072:0crwdne8072:0',
+
+    'SHOP_THEME_blUseGoogleTS'                      => 'crwdns8074:0crwdne8074:0',
+    'SHOP_THEME_sGoogleVendorId'                    => 'crwdns8076:0crwdne8076:0',
+    'HELP_SHOP_THEME_sGoogleVendorId'               => 'crwdns8078:0crwdne8078:0',
+    'SHOP_THEME_sGoogleShoppingAccountId'           => 'crwdns8080:0crwdne8080:0',
+    'HELP_SHOP_THEME_sGoogleShoppingAccountId'      => 'crwdns8082:0crwdne8082:0',
+    'SHOP_THEME_sPageLanguage'                      => 'crwdns8084:0crwdne8084:0',
+    'HELP_SHOP_THEME_sPageLanguage'                 => 'crwdns8086:0crwdne8086:0',
+    'SHOP_THEME_sShoppingCountry'                   => 'crwdns8088:0crwdne8088:0',
+    'HELP_SHOP_THEME_sShoppingCountry'              => 'crwdns8090:0crwdne8090:0',
+    'SHOP_THEME_sShoppingLanguage'                  => 'crwdns8092:0crwdne8092:0',
+    'HELP_SHOP_THEME_sShoppingLanguage'             => 'crwdns8094:0crwdne8094:0',
+    'SHOP_THEME_sShippingDaysOnStock'               => 'crwdns8096:0crwdne8096:0',
+    'HELP_SHOP_THEME_sShippingDaysOnStock'          => 'crwdns8098:0crwdne8098:0',
+    'SHOP_THEME_sShippingDaysNotOnStock'            => 'crwdns8100:0crwdne8100:0',
+    'HELP_SHOP_THEME_sShippingDaysNotOnStock'       => 'crwdns8102:0crwdne8102:0',
+    'SHOP_THEME_sDeliveryDaysOnStock'               => 'crwdns8104:0crwdne8104:0',
+    'HELP_SHOP_THEME_sDeliveryDaysOnStock'          => 'crwdns8106:0crwdne8106:0',
+    'SHOP_THEME_sDeliveryDaysNotOnStock'            => 'crwdns8108:0crwdne8108:0',
+    'HELP_SHOP_THEME_sDeliveryDaysNotOnStock'       => 'crwdns8110:0crwdne8110:0',
+
+    'SHOP_THEME_blSliderShowImageCaption'           => 'crwdns8112:0crwdne8112:0',
+    'HELP_SHOP_THEME_blSliderShowImageCaption'      => 'crwdns8114:0crwdne8114:0',
+
+    'SHOP_THEME_blEcondaRecommendationsStart'       => 'crwdns8116:0crwdne8116:0',
+    'SHOP_THEME_sEcondaWidgetIdStart'               => 'crwdns8118:0crwdne8118:0',
+    'SHOP_THEME_blEcondaRecommendationsList'        => 'crwdns8120:0crwdne8120:0',
+    'SHOP_THEME_sEcondaWidgetIdList'                => 'crwdns8122:0crwdne8122:0',
+    'SHOP_THEME_blEcondaRecommendationsDetails'     => 'crwdns8124:0crwdne8124:0',
+    'SHOP_THEME_sEcondaWidgetIdDetails'             => 'crwdns8126:0crwdne8126:0',
+    'SHOP_THEME_blEcondaRecommendationsBasket'      => 'crwdns8128:0crwdne8128:0',
+    'SHOP_THEME_sEcondaWidgetIdBasket'              => 'crwdns8130:0crwdne8130:0',
+
+    'SHOP_THEME_blFullwidthLayout'                      => 'crwdns8132:0crwdne8132:0',
+
+    'SHOP_THEME_blEmailsShowProductPictures'        => 'crwdns8134:0crwdne8134:0',
+);
