8df6d9c07697233150ee1435300f648ad9cf155f
Marco Steinhaeuser <kermie@users.noreply.github.com>
2020-09-25 11:33:09 +0200

    New translations help_lang.php (Acholi)

---
+++ source/Application/views/admin/ach-UG/help_lang.php
@@ -0,0 +1,457 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * In this file, the content for help popups is stored:
+ *
+ * Syntax for identifier: HELP_TABNAME_INPUTNAME, e.g. HELP_SHOP_CONFIG_BIDIRECTCROSS.
+ * !!!The INPUTNAME is same as in lang.php for avoiding even more different Identifiers.!!!
+ * In some cases, in lang.php GENERAL_ identifiers are used. In this file, always the tab name is used.
+ *
+ *
+ * HTML Tags for markup (same as in online manual):
+ * <span class='navipath_or_inputname'>...</span> for names of input fields, selectlists and Buttons, e.g. <span class='navipath_or_inputname'>Active</span>
+ * <span class='userinput_or_code'>...</span> for input in input fields (also options in selectlists) and code
+ * <span class='filename_filepath_or_italic'>...</span> for filenames, filepaths and other italic stuff
+ * <span class='warning_or_important_hint'>...</span> for warning and important things
+ * <ul> and <li> for lists
+ */
+
+$aLang =  [
+'charset'                                     => 'crwdns5118:0crwdne5118:0',
+
+'HELP_SHOP_SYSTEM_OTHERCOUNTRYORDER'          => 'crwdns8136:0crwdne8136:0',
+
+'HELP_SHOP_SYSTEM_DISABLENAVBARS'             => 'crwdns5122:0crwdne5122:0',
+
+'HELP_SHOP_SYSTEM_DEFAULTIMAGEQUALITY'        => 'crwdns8138:0crwdne8138:0',
+
+'HELP_SHOP_SYSTEM_LDAP'                       => 'crwdns5126:0crwdne5126:0',
+
+'HELP_SHOP_SYSTEM_SHOWVARIANTREVIEWS'         => 'crwdns5128:0crwdne5128:0',
+
+'HELP_SHOP_SYSTEM_VARIANTSSELECTION'          => 'crwdns5130:0crwdne5130:0',
+
+'HELP_SHOP_SYSTEM_VARIANTPARENTBUYABLE'       => 'crwdns8140:0crwdne8140:0',
+
+'HELP_SHOP_SYSTEM_VARIANTINHERITAMOUNTPRICE'  => 'crwdns5134:0crwdne5134:0',
+
+'HELP_SHOP_SYSTEM_ISERVERTIMESHIFT'           => 'crwdns5136:0crwdne5136:0',
+
+'HELP_SHOP_SYSTEM_INLINEIMGEMAIL'             => 'crwdns5138:0crwdne5138:0',
+
+'HELP_SHOP_SYSTEM_UTILMODULE'                 => 'crwdns5140:0crwdne5140:0',
+
+'HELP_SHOP_CACHE_ENABLED'                     => 'crwdns5142:0crwdne5142:0',
+
+'HELP_SHOP_CACHE_LIFETIME'                    => 'crwdns5144:0crwdne5144:0',
+
+'HELP_SHOP_CACHE_CLASSES'                     => 'crwdns5146:0crwdne5146:0',
+
+'HELP_SHOP_CACHE_REVERSE_PROXY_ENABLED'       => 'crwdns5148:0crwdne5148:0',
+
+'HELP_SHOP_CACHE_LAYOUT_CACHE_AGE'            => 'crwdns5150:0crwdne5150:0',
+
+'HELP_SHOP_CONFIG_ORDEROPTINEMAIL'            => 'crwdns8142:0crwdne8142:0',
+
+'HELP_SHOP_CONFIG_BIDIRECTCROSS'              => 'crwdns8144:0crwdne8144:0',
+
+'HELP_SHOP_CONFIG_STOCKONDEFAULTMESSAGE'      => 'crwdns8146:0crwdne8146:0',
+
+'HELP_SHOP_CONFIG_STOCKOFFDEFAULTMESSAGE'     => 'crwdns8148:0crwdne8148:0',
+
+'HELP_SHOP_CONFIG_OVERRIDEZEROABCPRICES'      => 'crwdns8150:0crwdne8150:0',
+
+'HELP_SHOP_CONFIG_SEARCHFIELDS'               => 'crwdns8152:0crwdne8152:0',
+
+'HELP_SHOP_CONFIG_SORTFIELDS'                 => 'crwdns8154:0crwdne8154:0',
+
+'HELP_SHOP_CONFIG_MUSTFILLFIELDS'             => 'crwdns8156:0crwdne8156:0',
+
+'HELP_SHOP_CONFIG_USENEGATIVESTOCK'           => 'crwdns8158:0crwdne8158:0',
+
+'HELP_SHOP_CONFIG_SETORDELETECURRENCY'        => 'crwdns8160:0crwdne8160:0',
+
+'HELP_SHOP_CONFIG_NEWARTBYINSERT'             => 'crwdns5172:0crwdne5172:0',
+
+/** @deprecated since v6.5.4 (2020-04-06); Suggest feature will be removed completely */
+'HELP_SHOP_CONFIG_ALLOW_SUGGEST_ARTICLE'      => 'crwdns5814:0crwdne5814:0',
+/** end deprecated */
+
+'HELP_SHOP_CONFIG_SEND_TECHNICAL_INFORMATION_TO_OXID' => 'crwdns8162:0crwdne8162:0',
+
+'HELP_SHOP_CONFIG_DELETERATINGLOGS'           => 'crwdns5176:0crwdne5176:0',
+
+'HELP_SHOP_CONFIG_DISABLEONLINEVATIDCHECK'    => 'crwdns8164:0crwdne8164:0',
+
+'HELP_SHOP_CONFIG_ALTVATIDCHECKINTERFACEWSDL' => 'crwdns5180:0crwdne5180:0',
+
+'HELP_SHOP_CONFIG_PSLOGIN'                    => 'crwdns8166:0crwdne8166:0',
+
+'HELP_SHOP_CONFIG_BASKETEXCLUDE'              => 'crwdns8168:0crwdne8168:0',
+
+'HELP_SHOP_CONFIG_BASKETRESERVATION'          => 'crwdns8170:0crwdne8170:0',
+
+'HELP_SHOP_CONFIG_BASKETRESERVATIONTIMEOUT'   => 'crwdns5188:0crwdne5188:0',
+
+'HELP_SHOP_CONFIG_INVITATION'                 => 'crwdns8172:0crwdne8172:0',
+
+'HELP_SHOP_CONFIG_POINTSFORINVITATION'        => 'crwdns8174:0crwdne8174:0',
+
+'HELP_SHOP_CONFIG_POINTSFORREGISTRATION'      => 'crwdns8176:0crwdne8176:0',
+
+// @deprecated since v6.5.1 (2019-02-07); credit card payment method will be no longer supported
+'HELP_SHOP_CONFIG_ATTENTION'                  => 'crwdns5876:0crwdne5876:0',
+// END deprecated
+
+'HELP_SHOP_CONFIG_SHOWTSINTERNATIONALFEESMESSAGE' => 'crwdns5196:0crwdne5196:0',
+
+'HELP_SHOP_CONFIG_SHOWTSCODMESSAGE'           => 'crwdns5198:0crwdne5198:0',
+
+'HELP_SHOP_CONFIG_DOWNLOADS'                  => 'crwdns5200:0crwdne5200:0',
+
+'HELP_SHOP_CONFIG_DOWNLOADS_PATH'             => 'crwdns5202:0crwdne5202:0',
+
+'HELP_SHOP_CONFIG_MAX_DOWNLOADS_COUNT'        => 'crwdns8178:0crwdne8178:0',
+
+'HELP_SHOP_CONFIG_LINK_EXPIRATION_TIME_UNREGISTERED' => 'crwdns8180:0crwdne8180:0',
+
+'HELP_SHOP_CONFIG_LINK_EXPIRATION_TIME'       => 'crwdns8182:0crwdne8182:0',
+
+'HELP_SHOP_CONFIG_PARCELSERVICE'              => 'crwdns5210:0crwdne5210:0',
+
+'HELP_SHOP_CONFIG_DOWNLOAD_EXPIRATION_TIME'   => 'crwdns8184:0crwdne8184:0',
+
+'HELP_SHOP_CONFIG_LOADREVIEWS'                  => 'crwdns8186:0crwdne8186:0',
+
+'HELP_SHOP_CONFIG_INCLUDE_PRODUCT_REVIEW_LINKS_IN_ORDER_EMAIL' => 'crwdns5216:0crwdne5216:0',
+
+'HELP_SHOP_MALL_MALLMODE'                     => 'crwdns8188:0crwdne8188:0',
+
+'HELP_SHOP_MALL_PRICEADDITION'                => 'crwdns5220:0crwdne5220:0',
+
+'HELP_SHOP_PERF_NEWESTARTICLES'               => 'crwdns8190:0crwdne8190:0',
+
+'HELP_SHOP_PERF_TOPSELLER'                    => 'crwdns8192:0crwdne8192:0',
+
+'HELP_SHOP_PERF_LOADFULLTREE'                 => 'crwdns5226:0crwdne5226:0',
+
+'HELP_SHOP_PERF_LOADACTION'                   => 'crwdns5228:0crwdne5228:0',
+
+'HELP_SHOP_PERF_USESELECTLISTPRICE'           => 'crwdns5230:0crwdne5230:0',
+
+'HELP_SHOP_PERF_DISBASKETSAVING'              => 'crwdns5232:0crwdne5232:0',
+
+'HELP_SHOP_PERF_LOADDELIVERY'                 => 'crwdns5234:0crwdne5234:0',
+
+'HELP_SHOP_PERF_LOADPRICE'                    => 'crwdns5236:0crwdne5236:0',
+
+'HELP_SHOP_PERF_PARSELONGDESCINSMARTY'        => 'crwdns5238:0crwdne5238:0',
+
+'HELP_SHOP_PERF_LOADSELECTLISTSINALIST'       => 'crwdns5240:0crwdne5240:0',
+
+'HELP_SHOP_PERF_CHECKIFTPLCOMPILE'            => 'crwdns5242:0crwdne5242:0',
+
+'HELP_SHOP_PERF_CLEARCACHEONLOGOUT'           => 'crwdns5244:0crwdne5244:0',
+
+'HELP_SHOP_SEO_TITLEPREFIX'                   => 'crwdns8194:0crwdne8194:0',
+
+'HELP_SHOP_SEO_TITLESUFFIX'                   => 'crwdns8196:0crwdne8196:0',
+
+'HELP_SHOP_SEO_IDSSEPARATOR'                  => 'crwdns8198:0crwdne8198:0',
+
+'HELP_SHOP_SEO_SAFESEOPREF'                   => 'crwdns5252:0crwdne5252:0',
+
+'HELP_SHOP_SEO_RESERVEDWORDS'                 => 'crwdns5254:0crwdne5254:0',
+
+'HELP_SHOP_SEO_SKIPTAGS'                      => 'crwdns5256:0crwdne5256:0',
+
+'HELP_SHOP_SEO_LOWERCASEURLS'                 => 'crwdns5258:0crwdne5258:0',
+
+'HELP_SHOP_SEO_STATICURLS'                    => 'crwdns5260:0crwdne5260:0',
+
+'HELP_SHOP_MAIN_PRODUCTIVE'                   => 'crwdns8200:0crwdne8200:0',
+
+'HELP_SHOP_MAIN_ACTIVE'                       => 'crwdns5264:0crwdne5264:0',
+
+'HELP_SHOP_MAIN_INFOEMAIL'                    => 'crwdns5266:0crwdne5266:0',
+
+'HELP_SHOP_MAIN_ORDEREMAIL'                   => 'crwdns5268:0crwdne5268:0',
+
+'HELP_SHOP_MAIN_OWNEREMAIL'                   => 'crwdns5270:0crwdne5270:0',
+
+'HELP_SHOP_MAIN_SMTPSERVER'                   => 'crwdns5272:0crwdne5272:0',
+
+'HELP_ARTICLE_MAIN_ALDPRICE'                  => 'crwdns5274:0crwdne5274:0',
+
+'HELP_ARTICLE_MAIN_VAT'                       => 'crwdns5276:0crwdne5276:0',
+
+'HELP_ARTICLE_EXTEND_UNITQUANTITY'            => 'crwdns5278:0crwdne5278:0',
+
+'HELP_ARTICLE_EXTEND_EXTURL'                  => 'crwdns5280:0crwdne5280:0',
+
+'HELP_ARTICLE_EXTEND_TPRICE'                  => 'crwdns5282:0crwdne5282:0',
+
+'HELP_ARTICLE_EXTEND_QUESTIONEMAIL'           => 'crwdns5284:0crwdne5284:0',
+
+'HELP_ARTICLE_EXTEND_NONMATERIAL'             => 'crwdns5286:0crwdne5286:0',
+
+'HELP_ARTICLE_EXTEND_FREESHIPPING'            => 'crwdns5288:0crwdne5288:0',
+
+'HELP_ARTICLE_EXTEND_BLFIXEDPRICE'            => 'crwdns5290:0crwdne5290:0',
+
+'HELP_ARTICLE_EXTEND_SKIPDISCOUNTS'           => 'crwdns5292:0crwdne5292:0',
+
+'HELP_ARTICLE_EXTEND_TEMPLATE'                => 'crwdns5294:0crwdne5294:0',
+
+'HELP_ARTICLE_EXTEND_ISCONFIGURABLE'          => 'crwdns8202:0crwdne8202:0',
+
+'HELP_ARTICLE_EXTEND_UPDATEPRICE'             => 'crwdns5298:0crwdne5298:0',
+
+'HELP_ARTICLE_EXTEND_SHOWCUSTOMAGREEMENT'     => 'crwdns5300:0crwdne5300:0',
+
+'HELP_ARTICLE_FILES_MAX_DOWNLOADS_COUNT'      => 'crwdns5302:0crwdne5302:0',
+
+'HELP_ARTICLE_FILES_LINK_EXPIRATION_TIME_UNREGISTERED' => 'crwdns5304:0crwdne5304:0',
+
+'HELP_ARTICLE_FILES_LINK_EXPIRATION_TIME'     => 'crwdns5306:0crwdne5306:0',
+
+'HELP_ARTICLE_FILES_NEW'                      => 'crwdns5308:0crwdne5308:0',
+
+'HELP_ARTICLE_FILES_DOWNLOAD_EXPIRATION_TIME' => 'crwdns5310:0crwdne5310:0',
+
+'HELP_ARTICLE_PICTURES_ICON'                  => 'crwdns8204:0crwdne8204:0',
+
+'HELP_ARTICLE_PICTURES_THUMB'                 => 'crwdns8206:0crwdne8206:0',
+
+'HELP_ARTICLE_PICTURES_PIC1'                  => 'crwdns8208:0crwdne8208:0',
+
+'HELP_ARTICLE_PICTURES_ZOOM1'                 => 'crwdns8210:0crwdne8210:0',
+
+'HELP_ARTICLE_STOCK_REMINDACTIV'              => 'crwdns5320:0crwdne5320:0',
+
+'HELP_ARTICLE_STOCK_STOCKFLAG'                => 'crwdns8212:0crwdne8212:0',
+
+'HELP_ARTICLE_IS_DOWNLOADABLE'                => 'crwdns5324:0crwdne5324:0',
+
+'HELP_ARTICLE_STOCK_REMINDAMAOUNT'            => 'crwdns5326:0crwdne5326:0',
+
+'HELP_ARTICLE_STOCK_DELIVERY'                 => 'crwdns5328:0crwdne5328:0',
+
+'HELP_ARTICLE_SEO_FIXED'                      => 'crwdns5330:0crwdne5330:0',
+
+'HELP_ARTICLE_SEO_KEYWORDS'                   => 'crwdns5332:0crwdne5332:0',
+
+'HELP_ARTICLE_SEO_DESCRIPTION'                => 'crwdns5334:0crwdne5334:0',
+
+'HELP_ARTICLE_SEO_ACTCAT'                     => 'crwdns5336:0crwdne5336:0',
+
+'HELP_ARTICLE_STOCK_STOCKTEXT'                => 'crwdns5338:0crwdne5338:0',
+
+'HELP_ARTICLE_STOCK_NOSTOCKTEXT'              => 'crwdns5340:0crwdne5340:0',
+
+'HELP_ARTICLE_STOCK_AMOUNTPRICE_AMOUNTFROM'   => 'crwdns5342:0crwdne5342:0',
+
+'HELP_ARTICLE_STOCK_AMOUNTPRICE_PRICE'        => 'crwdns8214:0crwdne8214:0',
+
+'HELP_ARTICLE_VARIANT_VARNAME'                => 'crwdns5346:0crwdne5346:0',
+
+'HELP_ATTRIBUTE_MAIN_DISPLAYINBASKET'         => 'crwdns5348:0crwdne5348:0',
+
+'HELP_CATEGORY_MAIN_HIDDEN'                   => 'crwdns5350:0crwdne5350:0',
+
+'HELP_CATEGORY_MAIN_PARENTID'                 => 'crwdns8216:0crwdne8216:0',
+
+'HELP_CATEGORY_MAIN_EXTLINK'                  => 'crwdns5354:0crwdne5354:0',
+
+'HELP_CATEGORY_MAIN_PRICEFROMTILL'            => 'crwdns5356:0crwdne5356:0',
+
+'HELP_CATEGORY_MAIN_DEFSORT'                  => 'crwdns5358:0crwdne5358:0',
+
+'HELP_CATEGORY_MAIN_SORT'                     => 'crwdns5360:0crwdne5360:0',
+
+'HELP_CATEGORY_MAIN_THUMB'                    => 'crwdns5362:0crwdne5362:0',
+
+'HELP_CATEGORY_MAIN_PROMOTION_ICON'           => 'crwdns5364:0crwdne5364:0',
+
+'HELP_CATEGORY_MAIN_SKIPDISCOUNTS'            => 'crwdns5366:0crwdne5366:0',
+
+'HELP_CATEGORY_SEO_FIXED'                     => 'crwdns5368:0crwdne5368:0',
+
+'HELP_CATEGORY_SEO_KEYWORDS'                  => 'crwdns5370:0crwdne5370:0',
+
+'HELP_CATEGORY_SEO_DESCRIPTION'               => 'crwdns5372:0crwdne5372:0',
+
+'HELP_CATEGORY_SEO_SHOWSUFFIX'                => 'crwdns5374:0crwdne5374:0',
+
+'HELP_CONTENT_MAIN_SNIPPET'                   => 'crwdns5376:0crwdne5376:0',
+
+'HELP_CONTENT_MAIN_MAINMENU'                  => 'crwdns5378:0crwdne5378:0',
+
+'HELP_CONTENT_MAIN_CATEGORY'                  => 'crwdns5380:0crwdne5380:0',
+
+'HELP_CONTENT_MAIN_MANUAL'                    => 'crwdns5382:0crwdne5382:0',
+
+'HELP_CONTENT_SEO_FIXED'                      => 'crwdns5384:0crwdne5384:0',
+
+'HELP_CONTENT_SEO_KEYWORDS'                   => 'crwdns5386:0crwdne5386:0',
+
+'HELP_CONTENT_SEO_DESCRIPTION'                => 'crwdns5388:0crwdne5388:0',
+
+'HELP_DELIVERY_MAIN_COUNTRULES'               => 'crwdns8218:0crwdne8218:0',
+
+'HELP_DELIVERY_MAIN_CONDITION'                => 'crwdns8220:0crwdne8220:0',
+
+'HELP_DELIVERY_MAIN_PRICE'                    => 'crwdns8222:0crwdne8222:0',
+
+'HELP_DELIVERY_MAIN_ORDER'                    => 'crwdns5396:0crwdne5396:0',
+
+'HELP_DELIVERY_MAIN_FINALIZE'                 => 'crwdns5398:0crwdne5398:0',
+
+'HELP_DELIVERYSET_MAIN_POS'                   => 'crwdns5400:0crwdne5400:0',
+
+'HELP_DISCOUNT_MAIN_SORT'                     => 'crwdns5402:0crwdne5402:0',
+
+'HELP_DISCOUNT_MAIN_PRICE'                    => 'crwdns5404:0crwdne5404:0',
+
+'HELP_DISCOUNT_MAIN_AMOUNT'                   => 'crwdns5406:0crwdne5406:0',
+
+'HELP_DISCOUNT_MAIN_REBATE'                   => 'crwdns8224:0crwdne8224:0',
+
+'HELP_GENERAL_SEO_ACTCAT'                     => 'crwdns5410:0crwdne5410:0',
+
+'HELP_GENERAL_SEO_FIXED'                      => 'crwdns5412:0crwdne5412:0',
+
+'HELP_GENERAL_SEO_SHOWSUFFIX'                 => 'crwdns5414:0crwdne5414:0',
+
+'HELP_GENERAL_SEO_OXKEYWORDS'                 => 'crwdns5416:0crwdne5416:0',
+
+'HELP_GENERAL_SEO_OXDESCRIPTION'              => 'crwdns5418:0crwdne5418:0',
+
+'HELP_GENIMPORT_FIRSTCOLHEADER'               => 'crwdns5420:0crwdne5420:0',
+
+'HELP_GENIMPORT_REPEATIMPORT'                 => 'crwdns5422:0crwdne5422:0',
+
+'HELP_LANGUAGE_DEFAULT'                       => 'crwdns5424:0crwdne5424:0',
+
+'HELP_LANGUAGE_ACTIVE'                        => 'crwdns5426:0crwdne5426:0',
+
+'HELP_PAYMENT_MAIN_SORT'                      => 'crwdns5428:0crwdne5428:0',
+
+'HELP_PAYMENT_MAIN_FROMBONI'                  => 'crwdns5430:0crwdne5430:0',
+
+'HELP_PAYMENT_MAIN_SELECTED'                  => 'crwdns5432:0crwdne5432:0',
+
+'HELP_PAYMENT_MAIN_AMOUNT'                    => 'crwdns8226:0crwdne8226:0',
+
+'HELP_PAYMENT_MAIN_ADDPRICE'                  => 'crwdns8228:0crwdne8228:0',
+
+'HELP_PAYMENT_MAIN_ADDSUMRULES'               => 'crwdns5438:0crwdne5438:0',
+
+'HELP_SELECTLIST_MAIN_TITLEIDENT'             => 'crwdns5440:0crwdne5440:0',
+
+'HELP_SELECTLIST_MAIN_FIELDS'                 => 'crwdns5442:0crwdne5442:0',
+
+'HELP_USER_MAIN_HASPASSWORD'                  => 'crwdns8230:0crwdne8230:0',
+
+'HELP_USER_PAYMENT_METHODS'                   => 'crwdns8232:0crwdne8232:0',
+
+'HELP_USER_EXTEND_NEWSLETTER'                 => 'crwdns5448:0crwdne5448:0',
+
+'HELP_USER_EXTEND_EMAILFAILED'                => 'crwdns5450:0crwdne5450:0',
+
+'HELP_USER_EXTEND_BONI'                       => 'crwdns5452:0crwdne5452:0',
+
+'HELP_MANUFACTURER_MAIN_ICON'                 => 'crwdns5454:0crwdne5454:0',
+
+'HELP_MANUFACTURER_SEO_FIXED'                 => 'crwdns5456:0crwdne5456:0',
+
+'HELP_MANUFACTURER_SEO_KEYWORDS'              => 'crwdns5458:0crwdne5458:0',
+
+'HELP_MANUFACTURER_SEO_DESCRIPTION'           => 'crwdns5460:0crwdne5460:0',
+
+'HELP_MANUFACTURER_SEO_SHOWSUFFIX'            => 'crwdns5462:0crwdne5462:0',
+
+'HELP_VOUCHERSERIE_MAIN_DISCOUNT'             => 'crwdns8234:0crwdne8234:0',
+
+'HELP_VOUCHERSERIE_MAIN_ALLOWSAMESERIES'      => 'crwdns5466:0crwdne5466:0',
+
+'HELP_VOUCHERSERIE_MAIN_ALLOWOTHERSERIES'     => 'crwdns5468:0crwdne5468:0',
+
+'HELP_VOUCHERSERIE_MAIN_SAMESEROTHERORDER'    => 'crwdns5470:0crwdne5470:0',
+
+'HELP_VOUCHERSERIE_MAIN_RANDOMNUM'            => 'crwdns5472:0crwdne5472:0',
+
+'HELP_VOUCHERSERIE_MAIN_VOUCHERNUM'           => 'crwdns5474:0crwdne5474:0',
+
+'HELP_VOUCHERSERIE_MAIN_CALCULATEONCE'        => 'crwdns5476:0crwdne5476:0',
+
+'HELP_WRAPPING_MAIN_PICTURE'                  => 'crwdns5478:0crwdne5478:0',
+
+'HELP_PROMOTIONS_BANNER_PICTUREANDLINK'       => 'crwdns5480:0crwdne5480:0',
+
+'HELP_SHOP_PERF_SEO_CACHE'                    => 'crwdns5482:0crwdne5482:0',
+
+'HELP_ALTERNATIVE_IMAGE_SERVER_NOTE'          => 'crwdns5484:0crwdne5484:0',
+
+'HELP_SHOP_RDFA_CONTENT_OFFERER'              => 'crwdns5486:0crwdne5486:0',
+
+'HELP_SHOP_RDFA_CONTENT_PAYMENT'              => 'crwdns5488:0crwdne5488:0',
+
+'HELP_SHOP_RDFA_CONTENT_DELIVERY'             => 'crwdns5490:0crwdne5490:0',
+
+'HELP_SHOP_RDFA_VAT'                          => 'crwdns5492:0crwdne5492:0',
+
+'HELP_SHOP_RDFA_DURATION_PRICES'              => 'crwdns5494:0crwdne5494:0',
+
+'HELP_SHOP_RDFA_LOGO_URL'                     => 'crwdns5496:0crwdne5496:0',
+
+'HELP_SHOP_RDFA_GEO_LONGITUDE'                => 'crwdns5498:0crwdne5498:0',
+
+'HELP_SHOP_RDFA_GEO_LATITUDE'                 => 'crwdns5500:0crwdne5500:0',
+
+'HELP_SHOP_RDFA_GLN'                          => 'crwdns5502:0crwdne5502:0',
+
+'HELP_SHOP_RDFA_NAICS'                        => 'crwdns5504:0crwdne5504:0',
+
+'HELP_SHOP_RDFA_ISIC'                         => 'crwdns5506:0crwdne5506:0',
+
+'HELP_SHOP_RDFA_DUNS'                         => 'crwdns5508:0crwdne5508:0',
+
+'HELP_SHOP_RDFA_SHOW_PRODUCTSTOCK'            => 'crwdns5510:0crwdne5510:0',
+
+'HELP_SHOP_RDFA_RATING_MIN'                   => 'crwdns5512:0crwdne5512:0',
+
+'HELP_SHOP_RDFA_RATING_MAX'                   => 'crwdns5514:0crwdne5514:0',
+
+'HELP_SHOP_RDFA_COND'                         => 'crwdns5516:0crwdne5516:0',
+
+'HELP_SHOP_RDFA_FNC'                          => 'crwdns5518:0crwdne5518:0',
+
+'HELP_SHOP_RDFA_COSTUMER'                     => 'crwdns5520:0crwdne5520:0',
+
+'HELP_SHOP_RDFA_DURATION_OFFERINGS'           => 'crwdns5522:0crwdne5522:0',
+
+'HELP_SHOP_PERF_SYSREQ_CHECK'                 => 'crwdns5524:0crwdne5524:0',
+
+'HELP_SHOP_CONFIG_ADDITIONAL_SERVICE_VAT_CALCULATION_BIGGEST_NET'  => 'crwdns5526:0crwdne5526:0',
+
+'HELP_SHOP_CONFIG_ADDITIONAL_SERVICE_VAT_CALCULATION_PROPORTIONAL' => 'crwdns5528:0crwdne5528:0',
+
+'HELP_SHOP_CONFIG_VIEWNETPRICE'               => 'crwdns5530:0crwdne5530:0',
+
+'HELP_SHOP_CONFIG_ENTERNETPRICE'              => 'crwdns5532:0crwdne5532:0',
+
+'HELP_REVERSE_PROXY_GET_FRONTEND'             => 'crwdns5534:0crwdne5534:0',
+
+'HELP_REVERSE_PROXY_GET_BACKEND'              => 'crwdns5536:0crwdne5536:0',
+
+'HELP_SHOP_CONFIG_DEBIT_OLD_BANK_INFORMATION_NOT_ALLOWED' => 'crwdns5538:0crwdne5538:0',
+
+'HELP_SHOP_CONFIG_ENABLE_INTANGIBLE_PRODUCTS_AGREEMENT'   => 'crwdns5540:0crwdne5540:0',
+
+'HELP_SHOP_CONFIG_FIELDS_CONTACTFORM'         => 'crwdns5542:0crwdne5542:0',
+];
