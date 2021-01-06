1644cda06919b2f93f71fa3972ee53075edfb95d
Marco Steinhaeuser <kermie@users.noreply.github.com>
2021-01-06 10:25:55 +0100

    New translations lang.php (Czech)

--- source/Application/views/wave/cs-CZ/lang.php
+++ source/Application/views/wave/cs-CZ/lang.php
@@ -125,7 +125,7 @@ $aLang = array(
 
     'DD_ROLES_BEMAIN_UIROOTHEADER'                          => 'Nabídka',
 
-    'DD_DELETE_MY_ACCOUNT_WARNING'                          => '',
+    'DD_DELETE_MY_ACCOUNT_WARNING'                          => 'Tuto akci nelze vrátit zpět. Tímto trvale smažete vaše osobní údaje.',
     'DD_DELETE_MY_ACCOUNT'                                  => 'Smazat účet',
     'DD_DELETE_MY_ACCOUNT_CONFIRMATION_QUESTION'            => 'Jste si jisti, že chcete smazat Váš účet?',
     'DD_DELETE_MY_ACCOUNT_CANCEL'                           => 'Zrušit',
