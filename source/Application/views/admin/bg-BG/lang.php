2699179e916e775e83376dca1389cdfff0f3931a
Marco Steinhaeuser <kermie@users.noreply.github.com>
2021-06-16 00:50:26 +0200

    New translations lang.php (Bulgarian)

--- source/Application/views/admin/bg-BG/lang.php
+++ source/Application/views/admin/bg-BG/lang.php
@@ -1250,7 +1250,7 @@ $aLang = [
     'SYSREQ_ALLOW_URL_FOPEN'                  => 'allow_url_fopen or fsockopen to port 80',
     'SYSREQ_PHP4_COMPAT'                      => 'Зенд режим за съвместимост трябва да бъде изключен',
     // @deprecated since v.6.5.1 (2020-02-12);
-    'SYSREQ_PHP_VERSION'                      => '',
+    'SYSREQ_PHP_VERSION'                      => 'PHP версия от 7.3 до 8.0',
     // END deprecated
     'SYSREQ_REQUEST_URI'                      => 'REQUEST_URI set',
     'SYSREQ_LIB_XML2'                         => 'LIB XML2',
